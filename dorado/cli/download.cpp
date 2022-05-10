2f28f88fdf760c9ba0053ca0266ddecbad4dc9a3
Chris Seymour <christopher.seymour@nanoporetech.com>
2022-05-11 00:30:11 +0100

    download directory + archive cleanup

--- dorado/cli/download.cpp
+++ dorado/cli/download.cpp
@@ -10,10 +10,19 @@
 #include "httplib.h"
 #include "elzip/elzip.hpp"
 
+namespace fs = std::filesystem;
+
 
 int download(int argc, char *argv[]) {
 
     argparse::ArgumentParser parser("dorado", DORADO_VERSION);
+
+    parser.add_argument("model")
+            .help("the model to download");
+
+    parser.add_argument("--directory")
+           .default_value(std::string("."))
+           .help("the directory to download the models into");
 
     parser.add_argument("--list")
             .default_value(false)
@@ -29,33 +38,37 @@ int download(int argc, char *argv[]) {
     }
 
     auto list = parser.get<bool>("--list");
+    auto directory = fs::path(parser.get<std::string>("--directory"));
 
     if (list) {
         std::cerr << "> basecaller models" << std::endl;
         for (const auto& [model, _] : basecaller::models) {
-	    std::cerr << " - " << model << std::endl;
-	}
-	
-	return 0;
+            std::cerr << " - " << model << std::endl;
+        }
+
+        return 0;
     }
 
     httplib::Client http(basecaller::URL_ROOT);
     http.set_follow_location(true);
 
     std::cerr << "> basecaller models" << std::endl;
-    for (const auto& [model, path] : basecaller::models) {
-      
-      std::cerr << " - downloading " << model << " " << path;
-      auto res = http.Get(path.c_str());
-      std::cout << " [" << res->status << "]" << std::endl;
 
-      std::ofstream file(model + ".zip");
-      file << res->body;
-      file.close();
+    for (const auto& [model, url] : basecaller::models) {
 
-      elz::extractZip(model + ".zip", model);
-
-      return 0;
+        if (list) {
+            std::cerr << " - " << model << std::endl;
+        } else {
+            std::cerr << " - downloading " << model;
+            auto res = http.Get(url.c_str());
+            std::cout << " [" << res->status << "]" << std::endl;
+            fs::path archive(directory / (model + ".zip"));
+            std::ofstream ofs(archive.string());
+            ofs << res->body;
+            ofs.close();
+            elz::extractZip(archive.string(), directory.string());
+            fs::remove(archive);
+      }
 
     }
 
