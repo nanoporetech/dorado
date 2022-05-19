e221116caad0ee89f4813cd5766187689b7ff6ee
Mike Vella <Mike.Vella@nanoporetech.com>
2022-05-19 14:59:13 +0100

    Inline docs update

--- dorado/read_pipeline/BasecallerNode.h
+++ dorado/read_pipeline/BasecallerNode.h
@@ -10,28 +10,40 @@ public:
     ~BasecallerNode();
 
 private:
+    // Consume reads from input queue
     void input_worker_thread();
+    // Basecall reads
     void basecall_worker_thread(int worker_id);
+    // Basecall batch of chunks
     void basecall_current_batch(int worker_id);
 
     ReadSink& m_sink;
     std::unique_ptr<std::thread> m_input_worker; // Chunks up incoming reads and sticks them in the pending list
     std::vector<std::unique_ptr<std::thread>> m_basecall_workers; // Basecalls chunks from the queue and puts read on the sink.
     std::string m_model_path;
+    // Vector of model runners (each with their own GPU access etc)
     std::vector<Runner> m_model_runners;
+    // Number of chunks in a batch
     size_t m_batch_size;
+    // Chunk length
     size_t m_chunk_size;
+    // Minimum overlap between two adjacent chunks in a read. Overlap is used to reduce edge effects and improve accuracy.
     size_t m_overlap;
 
+    // Model runners which have not terminated.
     std::atomic<int> m_num_active_model_runners = 0;
 
+    // Time when Basecaller Node is initialised. Used for benchmarking and debugging
     std::chrono::time_point<std::chrono::system_clock> initialization_time;
+    // Time when Basecaller Node terminates. Used for benchmarking and debugging
     std::chrono::time_point<std::chrono::system_clock> termination_time;
     // Global chunk input list
     std::mutex m_chunks_in_mutex;
-    std::deque<std::shared_ptr<Chunk>> m_chunks_in; // Gets filled with chunks from the input reads
+    // Gets filled with chunks from the input reads
+    std::deque<std::shared_ptr<Chunk>> m_chunks_in;
 
     std::mutex m_working_reads_mutex;
+    // Reads removed from input queue and being basecalled.
     std::deque<std::shared_ptr<Read>> m_working_reads;
 
     // If we go multi-threaded, there will be one of these batches per thread
--- dorado/read_pipeline/ScalerNode.h
+++ dorado/read_pipeline/ScalerNode.h
@@ -5,11 +5,11 @@ class ScalerNode : public ReadSink {
 public: 
     ScalerNode(ReadSink& sink, size_t max_reads=1000);
     ~ScalerNode();
-    //Read Trimming function (removes some initial part of the raw read).
+    // Read Trimming method (removes some initial part of the raw read).
     int trim(torch::Tensor signal, int window_size=40, float threshold_factor=2.4, int min_elements=3);
 
 private:
-    void worker_thread(); //Worker thread performs scaling and trimming asynchronously.
-    ReadSink& m_sink; //ReadSink to consume scaled reads. Typically this will be a Basecaller Node.
+    void worker_thread(); // Worker thread performs scaling and trimming asynchronously.
+    ReadSink& m_sink; // ReadSink to consume scaled reads. Typically this will be a Basecaller Node.
     std::unique_ptr<std::thread> m_worker;
 };
