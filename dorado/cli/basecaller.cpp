39afd3c6c9043ec1d0976e0275bd6a433d842a8e
Chris Seymour <christopher.seymour@nanoporetech.com>
2022-09-12 19:06:54 +0000

    increase default chunksize to 10000

--- dorado/cli/basecaller.cpp
+++ dorado/cli/basecaller.cpp
@@ -119,7 +119,7 @@ int basecaller(int argc, char* argv[]) {
 
     parser.add_argument("-b", "--batchsize").default_value(1024).scan<'i', int>();
 
-    parser.add_argument("-c", "--chunksize").default_value(8000).scan<'i', int>();
+    parser.add_argument("-c", "--chunksize").default_value(10000).scan<'i', int>();
 
     parser.add_argument("-o", "--overlap").default_value(500).scan<'i', int>();
 
