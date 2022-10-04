25ec22688afe2f8512571efd8acf0d75525d3e2b
Chris Seymour <christopher.seymour@nanoporetech.com>
2022-10-04 20:41:47 +0000

    docs and tests

--- dorado/utils/tensor_utils.h
+++ dorado/utils/tensor_utils.h
@@ -14,7 +14,14 @@ void serialise_tensor(torch::Tensor t, const std::string& path);
 std::vector<torch::Tensor> load_tensors(const std::filesystem::path& dir,
                                         const std::vector<std::string>& tensors);
 
+// Computes the q-th quantiles of each row of the input tensor `t`
+// using a partial sort as opposed a full sort per torch::quantiles
+// Only `interpolation='lower'` is currently implemented.
 torch::Tensor quantile(const torch::Tensor t, const torch::Tensor q);
+
+// Computes the q-th quantiles of each row of the input tensor `t`
+// using a counting sort which is extremely fast for low range integers.
+// Only `interpolation='lower'` is currently implemented.
 torch::Tensor quantile_counting(const torch::Tensor t, const torch::Tensor q);
 
 }  // namespace utils
