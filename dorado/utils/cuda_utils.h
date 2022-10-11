213db6aa69742b1538dd98ef127cd5623b2f641d
Chris Seymour <christopher.seymour@nanoporetech.com>
2022-10-11 10:52:04 +0000

    reduced startup cost on mGPU system with threading cudaMemGetInfo

--- dorado/utils/cuda_utils.h
+++ dorado/utils/cuda_utils.h
@@ -7,7 +7,9 @@
 // each device (e.g ["cuda:0", "cuda:2", ..., "cuda:7"]
 std::vector<std::string> parse_cuda_device_string(std::string device_string);
 
+// Reports the amount of available memory (in bytes) for a given device.
+size_t available_memory(std::string device);
 // Given a vector of devices in the format as returned by parse_cuda_device_string this function
-// will report the largest amount of available memory (in bytes) common to all devices.
-size_t available_memory(std::vector<std::string> devices);
+// will report the amount of available memory (in bytes) for all devices.
+std::vector<size_t> available_memory(std::vector<std::string> devices);
 int auto_gpu_batch_size(std::string model_path, std::vector<std::string> devices);
