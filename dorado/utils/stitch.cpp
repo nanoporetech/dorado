56b694f08313b9767d6392b3cf863a834040c479
Chris Seymour <christopher.seymour@nanoporetech.com>
2022-11-04 12:25:40 +0000

    namespace fix

--- dorado/utils/stitch.cpp
+++ dorado/utils/stitch.cpp
@@ -5,8 +5,8 @@ namespace dorado::utils {
 
 void stitch_chunks(std::shared_ptr<Read> read) {
     // Calculate the chunk down sampling, round to closest int.
-    read->model_stride = ::utils::div_round_closest(read->called_chunks[0]->raw_chunk_size,
-                                                    read->called_chunks[0]->moves.size());
+    read->model_stride = dorado::utils::div_round_closest(read->called_chunks[0]->raw_chunk_size,
+                                                          read->called_chunks[0]->moves.size());
 
     int start_pos = 0;
     int mid_point_front = 0;
