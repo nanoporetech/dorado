0d9c979776dcb72b81297b2598b6962d44443ff2
Mark Bicknell <mark.bicknell@nanoporetech.com>
2022-11-08 20:01:23 +0000

    Clang format change for DuplexCallerNode.cpp

--- dorado/read_pipeline/DuplexCallerNode.cpp
+++ dorado/read_pipeline/DuplexCallerNode.cpp
@@ -199,8 +199,9 @@ void DuplexCallerNode::worker_thread() {
             int end_alignment_position = alignment_start_end.second;
 
             int min_trimmed_alignment_length = 200;
-            bool consensus_possible = (start_alignment_position < end_alignment_position)  &&
-                                      ((end_alignment_position - start_alignment_position) > min_trimmed_alignment_length);
+            bool consensus_possible = (start_alignment_position < end_alignment_position) &&
+                                      ((end_alignment_position - start_alignment_position) >
+                                       min_trimmed_alignment_length);
 
             if (consensus_possible) {
                 auto [consensus, quality_scores_phred] = compute_basespace_consensus(
