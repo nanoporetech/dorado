928050f37a3ba5cf63495e64f489bd67cb4097a3
Chris Seymour <christopher.seymour@nanoporetech.com>
2022-11-15 22:10:35 +0000

    cli model read change

--- dorado/cli/duplex.cpp
+++ dorado/cli/duplex.cpp
@@ -16,8 +16,9 @@ int duplex(int argc, char* argv[]) {
     utils::InitLogging();
 
     argparse::ArgumentParser parser("dorado", DORADO_VERSION);
-    parser.add_argument("bam_file").help("Basecalled reads in BAM/SAM format.");
-    parser.add_argument("pairs_file").help("Space-delimited csv containing read ID pairs.");
+    parser.add_argument("model").help("Model");
+    parser.add_argument("reads").help("Reads in Pod5 format or BAM/SAM format for basespace.");
+    parser.add_argument("--pairs").help("Space-delimited csv containing read ID pairs.");
     parser.add_argument("--emit-fastq").default_value(false).implicit_value(true);
     parser.add_argument("-t", "--threads").default_value(0).scan<'i', int>();
 
@@ -28,23 +29,28 @@ int duplex(int argc, char* argv[]) {
         std::exit(1);
     }
 
-    std::string bam_file = parser.get<std::string>("bam_file");
-    std::string pairs_file = parser.get<std::string>("pairs_file");
+    std::string model = parser.get<std::string>("model");
+    std::string reads = parser.get<std::string>("reads");
+    std::string pairs_file = parser.get<std::string>("--pairs");
     size_t threads = static_cast<size_t>(parser.get<int>("--threads"));
     bool emit_fastq = parser.get<bool>("--emit-fastq");
     std::vector<std::string> args(argv, argv + argc);
+
+    if (model.compare("basespace") != 0) {
+        spdlog::error("> Unsupported model {}", model);
+        return 1;
+    }
 
     spdlog::info("> Loading pairs file");
     std::map<std::string, std::string> template_complement_map = utils::load_pairs_file(pairs_file);
 
     spdlog::info("> Loading reads");
-    std::map<std::string, std::shared_ptr<Read>> reads = utils::read_bam(bam_file);
+    std::map<std::string, std::shared_ptr<Read>> read_map = utils::read_bam(reads);
 
     torch::set_num_threads(1);
     threads = threads == 0 ? std::thread::hardware_concurrency() : threads;
-
     WriterNode writer_node(std::move(args), emit_fastq, false, true, 4);
-    DuplexCallerNode duplex_caller_node(writer_node, template_complement_map, reads, threads);
+    DuplexCallerNode duplex_caller_node(writer_node, template_complement_map, read_map, threads);
 
     return 0;
 }
