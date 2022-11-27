47b800b3161dac2fcf9e8f8350faa2827d1f5da9
Mike Vella <mike.vella@nanoporetech.com>
2022-11-27 13:59:39 +0000

    Removed diagnostics code

--- dorado/read_pipeline/StereoDuplexEncoderNode.cpp
+++ dorado/read_pipeline/StereoDuplexEncoderNode.cpp
@@ -32,12 +32,6 @@ std::shared_ptr<dorado::Read> stereo_encode(std::shared_ptr<dorado::Read> templa
                        complement_sequence_reverse_complement.data(),
                        complement_sequence_reverse_complement.size(), align_config);
 
-    if (template_read->read_id == "025c2ee5-6775-43b8-85cb-0397fef88d5b") {
-        std::cerr << "Edit distance is " << result.editDistance << std::endl;
-        std::cerr << "Template sequence is " << template_read->seq << std::endl;
-        std::cerr << "Complement sequence is " << complement_read->seq << std::endl;
-    }
-
     int query_cursor = 0;
     int target_cursor = result.startLocations[0];
 
@@ -50,13 +44,6 @@ std::shared_ptr<dorado::Read> stereo_encode(std::shared_ptr<dorado::Read> templa
     int start_alignment_position = alignment_start_end.first;
     int end_alignment_position = alignment_start_end.second;
 
-    if (template_read->read_id == "025c2ee5-6775-43b8-85cb-0397fef88d5b") {
-        std::cerr << "start_alignment_position " << start_alignment_position << std::endl;
-        std::cerr << "end_alignment_position " << end_alignment_position << std::endl;
-        std::cerr << "Target cursor " << target_cursor << std::endl;
-        std::cerr << "Query cursor " << query_cursor << std::endl;
-    }
-
     // TODO: perhaps its overkill having this function make this decision...
     int min_trimmed_alignment_length = 200;
     bool consensus_possible =
@@ -68,9 +55,6 @@ std::shared_ptr<dorado::Read> stereo_encode(std::shared_ptr<dorado::Read> templa
     std::shared_ptr<dorado::Read> read = std::make_shared<dorado::Read>();
 
     if (consensus_possible) {
-        //dorado::utils::preprocess_quality_scores(template_q_scores);
-        //dorado::utils::preprocess_quality_scores(complemnet_q_scores_reversed);
-
         // Step 3 - Move along the alignment, filling out the stereo-encoded tensor
         // Prepare the encoding tensor
         int max_size = template_read->raw_data.size(0) + template_read->raw_data.size(0);
@@ -83,16 +67,6 @@ std::shared_ptr<dorado::Read> stereo_encode(std::shared_ptr<dorado::Read> templa
         for (int i = 0; i < template_read->moves.size(); i++) {
             num_moves += template_read->moves[i];
         }
-        std::cerr << "number of moves is: " << num_moves << std::endl;
-        std::cerr << "sequence length is: " << template_read->seq.size() << std::endl;
-
-        // Step one - we need three cursors (besides the sequence ones)
-        // 1. TSignal cursor
-        // 2. CSignal Cursor
-        // 3. Encoding cursor
-
-        // Step 1 - let's get the tsignal cursor.
-        // We have the target_query, let's loop over the move table until we have got our index into the signal.
 
         int template_signal_cursor = 0;
         int complement_signal_cursor = 0;
@@ -103,14 +77,11 @@ std::shared_ptr<dorado::Read> stereo_encode(std::shared_ptr<dorado::Read> templa
             for (int j = 0; j < stride - 1; j++) {
                 template_moves_expanded.push_back(0);
             }
-        }  // TODO add the last elements
+        }
         int extra_moves = template_moves_expanded.size() - template_read->raw_data.size(0);
         for (int i = 0; i < extra_moves; i++) {
             template_moves_expanded.pop_back();
         }
-
-        std::cerr << "Raw data size: " << template_read->raw_data.size(0) << std::endl;
-        std::cerr << "Expanded move table size: " << template_moves_expanded.size() << std::endl;
 
         int template_moves_seen = template_moves_expanded[template_signal_cursor];
         while (template_moves_seen < target_cursor + 1) {  // TODO: is this +1 correct?
@@ -130,9 +101,6 @@ std::shared_ptr<dorado::Read> stereo_encode(std::shared_ptr<dorado::Read> templa
         for (int i = 0; i < extra_moves; i++) {
             complement_moves_expanded.pop_back();
         }
-        std::cerr << "Raw data size: " << complement_read->raw_data.size(0) << std::endl;
-        std::cerr << "Expanded move table size: " << complement_moves_expanded.size() << std::endl;
-
         complement_moves_expanded.push_back(1);
         std::reverse(complement_moves_expanded.begin(), complement_moves_expanded.end());
         complement_moves_expanded.pop_back();
@@ -145,8 +113,6 @@ std::shared_ptr<dorado::Read> stereo_encode(std::shared_ptr<dorado::Read> templa
             complement_signal_cursor++;
             complement_moves_seen += complement_moves_expanded[complement_signal_cursor];
         }
-
-        std::cerr << complement_moves_expanded << std::endl;
 
         float pad_value = 0.8 * std::min(torch::min(complement_signal).item<float>(),
                                          torch::min(template_read->raw_data).item<float>());
@@ -237,17 +203,12 @@ std::shared_ptr<dorado::Read> stereo_encode(std::shared_ptr<dorado::Read> templa
             }
         }
 
-        std::cerr << template_read->read_id << std::endl;
-        std::cerr << complement_read->read_id << std::endl;
-
         tmp = tmp.index(
                 {torch::indexing::Slice(None),
                  torch::indexing::Slice(
                          None,
                          stereo_global_cursor)});  // TODO check if stereo_global_cursor is the correct limit here
         //torch::save(tmp, "/home/OXFORDNANOLABS/mvella/stereo_tensor3.pt");
-
-        //std::cerr << tmp << std::endl;
 
         read->read_id = template_read->read_id + ";" + complement_read->read_id;
         read->raw_data = tmp; // use the encoded signal
@@ -298,11 +259,6 @@ void StereoDuplexEncoderNode::worker_thread() {
         }
 
         if (partner_found) {
-            i++;
-            if (i % 100 == 0) {
-                std::cerr << i << std::endl;
-            }
-
             if (read_cache.find(partner_id) == read_cache.end()) {
                 // Partner is not in the read cache
                 read_cache[read->read_id] = read;
@@ -324,11 +280,8 @@ void StereoDuplexEncoderNode::worker_thread() {
 
                 std::shared_ptr<Read> stereo_encoded_read =
                         stereo_encode(template_read, complement_read);
-                std::cerr << "ndims : " << stereo_encoded_read->raw_data.ndimension() << std::endl;
 
                 if (stereo_encoded_read->raw_data.ndimension() == 2) { // 2 dims for stereo encoding, 1 for simpex
-                    //m_sink.push_read(stereo_encoded_read);  // Found a partner, so process it.
-                    std::cerr << "sizes is: " << stereo_encoded_read->raw_data.sizes() << std::endl;
                     m_sink.push_read(stereo_encoded_read);  // Found a partner, so process it.
                 }
             }
