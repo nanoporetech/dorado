bbbb7ca60362e8f8d47109a45453d0210cfa7697
Mike Vella <mike.vella@nanoporetech.com>
2022-12-01 10:06:12 +0000

    Fixed typo

--- dorado/read_pipeline/StereoDuplexEncoderNode.cpp
+++ dorado/read_pipeline/StereoDuplexEncoderNode.cpp
@@ -14,7 +14,7 @@ std::shared_ptr<dorado::Read> stereo_encode(std::shared_ptr<dorado::Read> templa
     std::shared_ptr<dorado::Read> read = std::make_shared<dorado::Read>();  // Return read
 
     float template_len = template_read->seq.size();
-    float complement_len = template_read->seq.size();
+    float complement_len = complement_len->seq.size();
 
     float delta = std::max(template_len, complement_len) - std::min(template_len, complement_len);
     if ((delta / std::max(template_len, complement_len)) > 0.05) {
