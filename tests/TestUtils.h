0253550a883294522b1dbae7ef1fffa2171e80b3
Chris Seymour <christopher.seymour@nanoporetech.com>
2023-02-08 12:15:41 +0000

    get_stereo_data_dir

--- tests/TestUtils.h
+++ tests/TestUtils.h
@@ -28,3 +28,16 @@ static std::string get_pod5_data_dir() {
     }
     return data_path;
 }
+
+static std::string get_stereo_data_dir() {
+    const std::string data_path = "./tests/data/stereo";
+
+    if (!std::filesystem::exists(data_path)) {
+        std::cerr << "Datapath " << data_path
+                  << " does not exist, exiting.\n"
+                     "Unit tests must be run from the build working directory inside dorado"
+                  << std::endl;
+        exit(1);
+    }
+    return data_path;
+}
