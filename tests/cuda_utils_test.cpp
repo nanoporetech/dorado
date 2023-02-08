7654c6a7c25dee390091885c49b76ba6fee6b30e
Hugh Pendry <Hugh.Pendry@nanoporetech.com>
2023-02-08 15:45:59 +0000

    remove cuda_uils_test from apple builds plus updates following review

--- tests/cuda_utils_test.cpp
+++ tests/cuda_utils_test.cpp
@@ -3,6 +3,7 @@
 #include <catch2/catch.hpp>
 
 #include <limits>
+#include <tuple>
 
 #define CUT_TAG "[cuda_utils]"
 
@@ -68,41 +69,36 @@ const std::vector<int> breakpoints{2, 4, 6};
 const std::array<int, 3> batch_sizes_low{0, 1, 2};
 const std::array<int, 3> batch_sizes_mid{3, 4, 5};
 const std::array<int, 3> batch_sizes_high{6, 7, 8};
+const std::array<int, 3> batch_sizes_none{0, 0, 0};
 
 const std::vector<std::array<int, 3>> batch_sizes = {batch_sizes_low, batch_sizes_mid,
                                                      batch_sizes_high};
 
-struct BatchSelecterTestCase {
-    int available_memory;
-    bool success;
-    std::array<int, 3> result;
-};
-
-std::vector<BatchSelecterTestCase> test_cases{
-        {0, false, {}},
-        {1, false, {}},
-        {2, true, batch_sizes_low},
-        {3, true, batch_sizes_low},
-        {4, true, batch_sizes_mid},
-        {5, true, batch_sizes_mid},
-        {6, true, batch_sizes_high},
-        {7, true, batch_sizes_high},
-        {-1, false, {}},
-        {std::numeric_limits<int>::max(), true, batch_sizes_high},
-        {std::numeric_limits<int>::min(), false, {}},
-};
-
 TEST_CASE(CUT_TAG ": try_select_max_batch_sizes parameterised", CUT_TAG) {
-    auto index = GENERATE(range(0, static_cast<int>(test_cases.size())));
-    auto const& test_case = test_cases[index];
-    INFO("available memory: " + std::to_string(test_case.available_memory));
+    int available_memory;
+    bool expected_success;
+    std::array<int, 3> expected_result;
+    std::tie(available_memory, expected_success, expected_result) =
+            GENERATE(table<int, bool, std::array<int, 3>>(
+                    {std::make_tuple(0, false, batch_sizes_none),
+                     std::make_tuple(1, false, batch_sizes_none),
+                     std::make_tuple(2, true, batch_sizes_low),
+                     std::make_tuple(3, true, batch_sizes_low),
+                     std::make_tuple(4, true, batch_sizes_mid),
+                     std::make_tuple(5, true, batch_sizes_mid),
+                     std::make_tuple(6, true, batch_sizes_high),
+                     std::make_tuple(7, true, batch_sizes_high),
+                     std::make_tuple(-1, false, batch_sizes_none),
+                     std::make_tuple(std::numeric_limits<int>::max(), true, batch_sizes_high),
+                     std::make_tuple(std::numeric_limits<int>::min(), false, batch_sizes_none)}));
+    INFO("available memory: " + std::to_string(available_memory));
 
     auto result = dorado::utils::details::try_select_max_batch_sizes(breakpoints, batch_sizes,
-                                                                     test_case.available_memory);
+                                                                     available_memory);
 
-    REQUIRE(result.has_value() == test_case.success);
+    REQUIRE(result.has_value() == expected_success);
     if (result) {
-        REQUIRE(*result == test_case.result);
+        REQUIRE(*result == expected_result);
     }
 }
 
