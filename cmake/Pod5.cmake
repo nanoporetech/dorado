62327aee979cb28875b106e5a584e83df20be6ff
Mike Vella <Mike.Vella@nanoporetech.com>
2023-02-14 15:59:15 +0000

    upgrade to POD5 0.1.5

--- cmake/Pod5.cmake
+++ cmake/Pod5.cmake
@@ -1,13 +1,13 @@
-set(POD5_VERSION 0.0.41)
+set(POD5_VERSION 0.1.5)
 set(POD5_DIR pod5-${POD5_VERSION}-${CMAKE_SYSTEM_NAME})
 set(POD5_REPO "https://github.com/nanoporetech/pod5-file-format")
 set(POD5_INCLUDE dorado/3rdparty/${POD5_DIR}/include)
 
 if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
     if(CMAKE_SYSTEM_PROCESSOR MATCHES "^aarch64*|^arm*")
-      set(POD5_URL "${POD5_REPO}/releases/download/${POD5_VERSION}/pod5-file-format-${POD5_VERSION}-linux-arm64.tar.gz")
+      set(POD5_URL "${POD5_REPO}/releases/download/${POD5_VERSION}/lib_pod5-${POD5_VERSION}-linux-arm64.tar.gz")
     else()
-      set(POD5_URL "${POD5_REPO}/releases/download/${POD5_VERSION}/pod5-file-format-${POD5_VERSION}-linux-x64.tar.gz")
+      set(POD5_URL "${POD5_REPO}/releases/download/${POD5_VERSION}/lib_pod5-${POD5_VERSION}-linux-x64.tar.gz")
     endif()
     set(POD5_LIBRARIES
       ${DORADO_3RD_PARTY}/${POD5_DIR}/lib64/libpod5_format.a
@@ -15,13 +15,13 @@ if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
       ${DORADO_3RD_PARTY}/${POD5_DIR}/lib64/libjemalloc_pic.a
     )
 elseif(APPLE)
-    set(POD5_URL "${POD5_REPO}/releases/download/${POD5_VERSION}/pod5-file-format-${POD5_VERSION}-osx-11.0-arm64.tar.gz")
+    set(POD5_URL "${POD5_REPO}/releases/download/${POD5_VERSION}/lib_pod5-${POD5_VERSION}-osx-11.0-arm64.tar.gz")
     set(POD5_LIBRARIES
       ${DORADO_3RD_PARTY}/${POD5_DIR}/lib/libpod5_format.a
       ${DORADO_3RD_PARTY}/${POD5_DIR}/lib/libarrow.a
     )
 elseif(WIN32)
-    set(POD5_URL "${POD5_REPO}/releases/download/${POD5_VERSION}/pod5-file-format-${POD5_VERSION}-win-x64.tar.gz")
+    set(POD5_URL "${POD5_REPO}/releases/download/${POD5_VERSION}/lib_pod5-${POD5_VERSION}-win-x64.tar.gz")
     set(POD5_LIBRARIES
       ${DORADO_3RD_PARTY}/${POD5_DIR}/lib/pod5_format.lib
       ${DORADO_3RD_PARTY}/${POD5_DIR}/lib/arrow_static.lib
