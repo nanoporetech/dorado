3f7a6d24100e54cf0c0deb234f87436fbcc44cc5
Steve Malton <stephen.malton@nanoporetech.com>
2023-02-24 12:01:30 +0000

    Fix lib directory

--- cmake/Pod5.cmake
+++ cmake/Pod5.cmake
@@ -1,12 +1,14 @@
 set(POD5_VERSION 0.1.8)
 set(POD5_DIR pod5-${POD5_VERSION}-${CMAKE_SYSTEM_NAME})
 set(POD5_REPO "https://github.com/nanoporetech/pod5-file-format")
-set(POD5_INCLUDE dorado/3rdparty/${POD5_DIR}/include)
+set(POD5_INCLUDE ${DORADO_3RD_PARTY}/${POD5_DIR}/include)
 
 if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
     if(CMAKE_SYSTEM_PROCESSOR MATCHES "^aarch64*|^arm*")
+      set(LIB_DIR "lib64")
       if((CMAKE_CXX_COMPILER_ID MATCHES "GNU") AND (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 9.0))
         set(POD5_URL "${POD5_REPO}/releases/download/${POD5_VERSION}/lib_pod5-${POD5_VERSION}-linux-gcc7-arm64.tar.gz")
+        set(LIB_DIR "lib")
       else()
         set(POD5_URL "${POD5_REPO}/releases/download/${POD5_VERSION}/lib_pod5-${POD5_VERSION}-linux-arm64.tar.gz")
       endif()
@@ -14,9 +16,9 @@ if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
       set(POD5_URL "${POD5_REPO}/releases/download/${POD5_VERSION}/lib_pod5-${POD5_VERSION}-linux-x64.tar.gz")
     endif()
     set(POD5_LIBRARIES
-      ${DORADO_3RD_PARTY}/${POD5_DIR}/lib64/libpod5_format.a
-      ${DORADO_3RD_PARTY}/${POD5_DIR}/lib64/libarrow.a
-      ${DORADO_3RD_PARTY}/${POD5_DIR}/lib64/libjemalloc_pic.a
+      ${DORADO_3RD_PARTY}/${POD5_DIR}/${LIB_DIR}/libpod5_format.a
+      ${DORADO_3RD_PARTY}/${POD5_DIR}/${LIB_DIR}/libarrow.a
+      ${DORADO_3RD_PARTY}/${POD5_DIR}/${LIB_DIR}/libjemalloc_pic.a
     )
 elseif(APPLE)
     set(POD5_URL "${POD5_REPO}/releases/download/${POD5_VERSION}/lib_pod5-${POD5_VERSION}-osx-11.0-arm64.tar.gz")
