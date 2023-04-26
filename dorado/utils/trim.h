2e437563d28e4d7e667afb48ab4d05f32b015e24
Stuart Abercrombie <stuart.abercrombie@nanoporetech.com>
2023-04-26 14:45:52 +0100

    Formatting again

--- dorado/utils/trim.h
+++ dorado/utils/trim.h
@@ -4,7 +4,9 @@
 namespace dorado::utils {
 
 // Read Trimming method (removes some initial part of the raw read).
-int trim(const torch::Tensor &signal, float threshold = 2.4,
-         int window_size = 40, int min_elements = 3);
+int trim(const torch::Tensor &signal,
+         float threshold = 2.4,
+         int window_size = 40,
+         int min_elements = 3);
 
-} // namespace dorado::utils
+}  // namespace dorado::utils
