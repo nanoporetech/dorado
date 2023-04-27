7c8be1672e52b35284f334941aa05bda38fb74e3
David Newman <david.newman@nanoporetech.com>
2023-04-27 14:32:59 +0100

    models: added stereo v1.0

--- dorado/utils/models.h
+++ dorado/utils/models.h
@@ -52,6 +52,9 @@ static const std::vector<std::string> models = {
         "dna_r10.4.1_e8.2_400bps_hac@v4.2.0",
         "dna_r10.4.1_e8.2_400bps_sup@v4.2.0",
 
+        // Stereo
+        "dna_r10.4.1_e8.2_5khz_stereo@v1.0",
+
         // RNA003
         "rna003_120bps_sup@v3",
 };
