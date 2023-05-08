6204ca52191ea6e00a78f02173da34d659901d15
Joyjit Daw <joyjit.daw@nanoporetech.com>
2023-05-08 21:43:42 +0100

    [tests] add windows dorado exe test

---
+++ tests/test_simple_basecaller_execution.bat
@@ -0,0 +1,11 @@
+set dorado_bin=%1
+set model=dna_r9.4.1_e8_hac@v3.3
+set batch=384
+
+echo dorado basecaller test stage
+%dorado_bin% download --model %model%
+%dorado_bin% basecaller %model% tests/data/pod5 -b %batch% --emit-fastq > ref.fq
+%dorado_bin% basecaller %model% tests/data/pod5 -b %batch% --modified-bases 5mCG --emit-moves --reference ref.fq --emit-sam > calls.bam
+
+echo dorado aligner test stage
+%dorado_bin% aligner ref.fq calls.bam > aligned-calls.bam
