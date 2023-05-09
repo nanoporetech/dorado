f0eaa673d5c83ab50aae7e88d4c2c4aad98a954f
Chris Seymour <christopher.seymour@nanoporetech.com>
2023-05-09 18:35:30 +0100

    aligned cols

--- dorado/cli/summary.cpp
+++ dorado/cli/summary.cpp
@@ -5,6 +5,7 @@
 #include <argparse.hpp>
 #include <spdlog/spdlog.h>
 
+#include <cctype>
 #include <csignal>
 #include <filesystem>
 
@@ -22,13 +23,21 @@ std::vector<std::string> header = {
         "mux",
         "start_time",
         "duration",
-        //"template_start",
+        "template_start",
         "template_duration",
         "sequence_length_template",
         "mean_qscore_template",
 };
 
-int summary(int argc, char* argv[]) {
+std::vector<std::string> aligned_header = {
+        "alignment_genome",         "alignment_genome_start",    "alignment_genome_end",
+        "alignment_strand_start",   "alignment_strand_end",      "alignment_direction",
+        "alignment_length",         "alignment_num_aligned",     "alignment_num_correct",
+        "alignment_num_insertions", "alignment_num_deletions",   "alignment_num_substitutions",
+        "alignment_mapq",           "alignment_strand_coverage", "alignment_identity",
+        "alignment_accuracy"};
+
+int summary(int argc, char *argv[]) {
     utils::InitLogging();
 
     argparse::ArgumentParser parser("dorado", DORADO_VERSION, argparse::default_arguments::help);
@@ -36,9 +45,11 @@ int summary(int argc, char* argv[]) {
     parser.add_argument("-s", "--separator").default_value(std::string("\t"));
     parser.add_argument("-v", "--verbose").default_value(false).implicit_value(true);
 
+    // --primary-only
+
     try {
         parser.parse_args(argc, argv);
-    } catch (const std::exception& e) {
+    } catch (const std::exception &e) {
         std::ostringstream parser_stream;
         parser_stream << parser;
         spdlog::error("{}\n{}", e.what(), parser_stream.str());
@@ -59,10 +70,20 @@ int summary(int argc, char* argv[]) {
 #endif
     std::signal(SIGINT, [](int signum) { interupt = 1; });
 
-    for (int col = 0; col < header.size(); col++) {
+    // HEADER
+    for (int col = 0; col < header.size() - 1; col++) {
         std::cout << header[col] << separator;
     }
-    std::cout << header[header.size()] << '\n';
+    std::cout << header[header.size() - 1];
+
+    if (reader.is_aligned) {
+        for (int col = 0; col < aligned_header.size() - 1; col++) {
+            std::cout << separator << aligned_header[col];
+        }
+        std::cout << separator << aligned_header[aligned_header.size() - 1];
+    }
+
+    std::cout << '\n';
 
     while (reader.read() && !interupt) {
         auto rg_value = reader.get_tag<std::string>("RG");
@@ -80,13 +101,118 @@ int summary(int argc, char* argv[]) {
 
         auto num_samples = reader.get_tag<int>("ns");
         auto trim_samples = reader.get_tag<int>("ts");
-        // todo: sample_rate
+
+        // todo: sample_rate and template_start_time
         float template_duration = (num_samples - trim_samples) / 4000.0f;
+        auto template_start_time = start_time;
 
         std::cout << filename << separator << read_id << separator << rg_value.substr(0, 36)
                   << separator << channel << separator << mux << separator << start_time
-                  << separator << duration << separator << template_duration << separator << seqlen
-                  << separator << mean_qscore << separator << '\n';
+                  << separator << duration << separator << template_start_time << separator
+                  << template_duration << separator << seqlen << separator << mean_qscore;
+
+        if (reader.is_aligned &&
+            !(reader.record->core.flag & (BAM_FSECONDARY | BAM_FSUPPLEMENTARY | BAM_FUNMAP))) {
+            //  ['*', -1, -1, -1, -1, '*', 0, 0, 0, 0, 0, 0, 0, 0.0, 0.0, 0.0]
+
+            std::string alignment_genome = reader.header->target_name[reader.record->core.tid];
+
+            int32_t alignment_genome_start = reader.record->core.pos;
+            int32_t alignment_genome_end = bam_endpos(reader.record.get());
+
+            // Depends on whether the read is aligned to the forward or reverse strand.
+            int32_t alignment_strand_start =
+                    bam_is_rev(reader.record) ? alignment_genome_end : alignment_genome_start;
+            int32_t alignment_strand_end =
+                    bam_is_rev(reader.record) ? alignment_genome_start : alignment_genome_end;
+
+            std::string alignment_direction = bam_is_rev(reader.record) ? "-" : "+";
+            int32_t alignment_length = reader.record->core.l_qseq;
+
+            uint32_t *cigar = bam_get_cigar(reader.record);
+            int n_cigar = reader.record->core.n_cigar;
+
+            // Initialize counts
+            int alignment_num_aligned = 0;
+            int alignment_num_correct = 0;  // Requires MD tag
+            int alignment_num_insertions = 0;
+            int alignment_num_deletions = 0;
+            int alignment_num_substitutions = 0;  // Requires MD tag
+
+            int32_t query_start =
+                    reader.record->core
+                            .pos;  // 0-based leftmost position of the alignment in the reference
+            int32_t query_end = bam_endpos(reader.record.get());
+
+            for (int i = 0; i < n_cigar; ++i) {
+                int op = bam_cigar_op(cigar[i]);
+                int op_len = bam_cigar_oplen(cigar[i]);
+
+                switch (op) {
+                case BAM_CMATCH:
+                    alignment_num_aligned += op_len;
+                    break;
+                case BAM_CINS:
+                    alignment_num_insertions += op_len;
+                    break;
+                case BAM_CDEL:
+                    alignment_num_deletions += op_len;
+                    break;
+                default:
+                    break;
+                }
+            }
+
+            uint8_t *md_ptr = bam_aux_get(reader.record.get(), "MD");
+
+            if (md_ptr) {
+                char *md = bam_aux2Z(md_ptr);
+
+                int md_length = 0;
+                int i = 0;
+                while (md[i]) {
+                    if (std::isdigit(md[i])) {
+                        md_length = md_length * 10 + (md[i] - '0');
+                    } else {
+                        if (md[i] == '^') {
+                            // Skip deletions
+                            i++;
+                            while (md[i] && !std::isdigit(md[i])) {
+                                i++;
+                            }
+                        } else {
+                            // Substitution found
+                            alignment_num_substitutions++;
+                            md_length++;
+                        }
+                    }
+                    i++;
+                }
+            }
+            alignment_num_correct = alignment_num_aligned - alignment_num_substitutions;
+
+            float alignment_idn = alignment_num_correct / static_cast<float>(alignment_num_aligned);
+            float alignment_acc = alignment_num_correct /
+                                  static_cast<float>(alignment_genome_end - alignment_genome_start);
+            float strand_coverage = (query_start - query_end) / static_cast<float>(seqlen);
+
+            std::cout << separator << alignment_genome << separator << alignment_genome_start
+                      << separator << alignment_genome_end << separator << alignment_strand_start
+                      << separator << alignment_strand_end << separator << alignment_direction
+                      << separator
+
+                      << alignment_genome_end - alignment_genome_start << separator  // length
+                      << alignment_num_aligned << separator                          // matches
+                      << alignment_num_correct << separator                          // correct
+
+                      << alignment_num_insertions << separator << alignment_num_deletions
+                      << separator << alignment_num_substitutions << separator
+
+                      << static_cast<int>(reader.record->core.qual) << separator << strand_coverage
+                      << separator << alignment_idn << separator << alignment_acc;
+        }
+
+        std::cout << '\n';
     }
 
     return 0;
