a9a908a47f0fc5b57e85623d84c6dbb2ae21331b
Joyjit Daw <joyjit.daw@nanoporetech.com>
2023-05-11 03:23:53 +0100

    make time_utils functions header only
    this is to prevent introducing a utils library
    dependency on the dataloader library right now.

--- dorado/utils/time_utils.h
+++ dorado/utils/time_utils.h
@@ -1,16 +1,54 @@
 #pragma once
-#include "../utils/compat_utils.h"
+#include "compat_utils.h"
 
 #include <ctime>
 #include <string>
 
 namespace dorado::utils {
 
-std::string get_string_timestamp_from_unix_time(time_t time_stamp_ms);
+inline std::string get_string_timestamp_from_unix_time(time_t time_stamp_ms) {
+    static std::mutex timestamp_mtx;
+    std::unique_lock lock(timestamp_mtx);
+    //Convert a time_t (seconds from UNIX epoch) to a timestamp in ISO 8601 (YYYY-MM-DDTHH:MM:SS.sss+00:00) format
+    auto time_stamp_s = time_stamp_ms / 1000;
+    int num_ms = time_stamp_ms % 1000;
+    char buf[32];
+    struct tm ts;
+    ts = *gmtime(&time_stamp_s);
 
-time_t get_unix_time_from_string_timestamp(const std::string& time_stamp);
+    std::stringstream ss;
+    ss << std::put_time(&ts, "%Y-%m-%dT%H:%M:%S.");
+    ss << std::setfill('0') << std::setw(3) << num_ms;  // add ms
+    ss << "+00:00";                                     //add zero timezone
+    return ss.str();
+}
 
-std::string adjust_time_ms(const std::string& time_stamp, uint64_t offset_ms);
+// Expects the time to be encoded like "2017-09-12T9:50:12.456+00:00".
+inline time_t get_unix_time_from_string_timestamp(const std::string& time_stamp) {
+    std::stringstream ss(time_stamp);
+    std::tm base_time = {};
+    ss >> std::get_time(&base_time, "%Y-%m-%dT%H:%M:%S.");
 
-std::string adjust_time(const std::string& time_stamp, uint32_t offset);
+    auto num_ms = std::stoi(time_stamp.substr(20, time_stamp.size() - 26));
+    return mktime(&base_time) * 1000 + num_ms;
+}
+
+inline std::string adjust_time_ms(const std::string& time_stamp, uint64_t offset_ms) {
+    return get_string_timestamp_from_unix_time(get_unix_time_from_string_timestamp(time_stamp) +
+                                               offset_ms);
+}
+
+inline std::string adjust_time(const std::string& time_stamp, uint32_t offset) {
+    // Expects the time to be encoded like "2017-09-12T9:50:12Z".
+    // Adds the offset (in seconds) to the timeStamp.
+    std::tm base_time = {};
+    strptime(time_stamp.c_str(), "%Y-%m-%dT%H:%M:%SZ", &base_time);
+    time_t timeObj = mktime(&base_time);
+    timeObj += offset;
+    std::tm* new_time = gmtime(&timeObj);
+    char buff[32];
+    strftime(buff, 32, "%FT%TZ", new_time);
+    return std::string(buff);
+}
+
 }  // namespace dorado::utils
