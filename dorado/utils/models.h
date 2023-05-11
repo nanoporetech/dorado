99c25f781713d48c701bd7fe5ece0f069723fce5
Charles Pugh <charles.pugh@nanoporetech.com>
2023-05-11 11:29:24 +0100

    Add v4.2 remora all context 5mC, 6mA models

--- dorado/utils/models.h
+++ dorado/utils/models.h
@@ -73,6 +73,8 @@ namespace modified {
 static const std::vector<std::string> mods = {
         "5mCG",
         "5mCG_5hmCG",
+        "5mC",
+        "6mA",
 };
 
 static const std::vector<std::string> models = {
@@ -113,6 +115,8 @@ static const std::vector<std::string> models = {
         "dna_r10.4.1_e8.2_400bps_fast@v4.2.0_5mCG_5hmCG@v0",
         "dna_r10.4.1_e8.2_400bps_hac@v4.2.0_5mCG_5hmCG@v0",
         "dna_r10.4.1_e8.2_400bps_sup@v4.2.0_5mCG_5hmCG@v0",
+        "dna_r10.4.1_e8.2_400bps_sup@v4.2.0_5mC@v0",
+        "dna_r10.4.1_e8.2_400bps_sup@v4.2.0_6mA@v0",
 
 };
 
