9153cf40d644d2e67138c6c90e6039b399c987b7
Chris Seymour <christopher.seymour@nanoporetech.com>
2023-05-16 09:04:40 +0100

    use model and batch job vars

--- tests/test_expected_logging.sh
+++ tests/test_expected_logging.sh
@@ -5,8 +5,10 @@
 set -ex
 set -o pipefail
 
-dorado_bin=$(cd "$(dirname $1)"; pwd -P)/$(basename $1)
 test_dir=$(dirname $0)
+dorado_bin=$(cd "$(dirname $1)"; pwd -P)/$(basename $1)
+model=${2:-dna_r9.4.1_e8_hac@v3.3}
+batch=${3:-384}
 data_dir=$test_dir/data
 output_dir=${test_dir}/test_output
 mkdir -p $output_dir
@@ -14,10 +16,10 @@ mkdir -p $output_dir
 test_output_file=$test_dir/output.log
 
 # Setup
-$dorado_bin download --model dna_r9.4.1_e8_hac@v3.3 > /dev/null
+$dorado_bin download --model ${model} > /dev/null
 
 # Test commands
-$dorado_bin basecaller dna_r9.4.1_e8_hac@v3.3 $data_dir/pod5 -b 384 > $output_dir/output.bam 2>$test_output_file
+$dorado_bin basecaller dna_r9.4.1_e8_hac@v3.3 $data_dir/pod5 -b ${batch} > $output_dir/output.bam 2>$test_output_file
 grep "Reads basecalled: 1" $test_output_file
 ret_val=$?
 if [ $ret_val -ne 0 ]; then
