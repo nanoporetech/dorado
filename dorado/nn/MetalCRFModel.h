0a7b4e23705fc14769c6f38e2d4e6d49249d9ceb
Steve Malton <stephen.malton@nanoporetech.com>
2023-05-25 15:23:14 +0100

    Fix metal model

--- dorado/nn/MetalCRFModel.h
+++ dorado/nn/MetalCRFModel.h
@@ -24,6 +24,7 @@ public:
     size_t model_stride() const final;
     size_t chunk_size() const final;
     size_t batch_size() const final;
+    void terminate() final;
 
 private:
     std::shared_ptr<MetalCaller> m_caller;
