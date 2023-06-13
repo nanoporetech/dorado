bf4be3f96b081b3e583a4b8d559ef8f392a06d63
Joyjit Daw <joyjit.daw@nanoporetech.com>
2023-06-13 08:27:05 -0400

    [tests] re-enable tests mistakenly commented out

--- tests/Pod5DataLoaderTest.cpp
+++ tests/Pod5DataLoaderTest.cpp
@@ -25,114 +25,114 @@ size_t MockSink::get_read_count() {
 
 }  // namespace
 
-//TEST_CASE(TEST_GROUP "Test loading single-read POD5 files") {
-//    // Create a mock sink for testing output of reads
-//    MockSink mock_sink;
-//
-//    std::string data_path(get_pod5_data_dir());
-//    dorado::DataLoader loader(mock_sink, "cpu", 1);
-//    loader.load_reads(data_path, false);
-//
-//    REQUIRE(mock_sink.get_read_count() == 1);
-//}
-//
-//TEST_CASE(TEST_GROUP "Test loading single-read POD5 file, empty read list") {
-//    // Create a mock sink for testing output of reads
-//    MockSink mock_sink;
-//
-//    auto read_list = std::unordered_set<std::string>();
-//    std::string data_path(get_pod5_data_dir());
-//    dorado::DataLoader loader(mock_sink, "cpu", 1, 0, read_list);
-//    loader.load_reads(data_path, false);
-//
-//    REQUIRE(mock_sink.get_read_count() == 0);
-//}
-//
-//TEST_CASE(TEST_GROUP "Test loading single-read POD5 file, no read list") {
-//    // Create a mock sink for testing output of reads
-//    MockSink mock_sink;
-//
-//    std::string data_path(get_pod5_data_dir());
-//    dorado::DataLoader loader(mock_sink, "cpu", 1, 0, std::nullopt);
-//    loader.load_reads(data_path, false);
-//
-//    REQUIRE(mock_sink.get_read_count() == 1);
-//}
-//
-//TEST_CASE(TEST_GROUP "Test loading single-read POD5 file, mismatched read list") {
-//    // Create a mock sink for testing output of reads
-//    MockSink mock_sink;
-//
-//    auto read_list = std::unordered_set<std::string>();
-//    read_list.insert("read_1");
-//    std::string data_path(get_pod5_data_dir());
-//    dorado::DataLoader loader(mock_sink, "cpu", 1, 0, read_list);
-//    loader.load_reads(data_path, false);
-//
-//    REQUIRE(mock_sink.get_read_count() == 0);
-//}
-//
-//TEST_CASE(TEST_GROUP "Test loading single-read POD5 file, matched read list") {
-//    // Create a mock sink for testing output of reads
-//    MockSink mock_sink;
-//
-//    auto read_list = std::unordered_set<std::string>();
-//    read_list.insert("002bd127-db82-436f-b828-28567c3d505d");  // read present in POD5
-//    std::string data_path(get_pod5_data_dir());
-//    dorado::DataLoader loader(mock_sink, "cpu", 1, 0, read_list);
-//    loader.load_reads(data_path, false);
-//
-//    REQUIRE(mock_sink.get_read_count() == 1);
-//}
-//
-//TEST_CASE(TEST_GROUP "Test calculating number of reads from pod5, read ids list.") {
-//    // Create a mock sink for testing output of reads
-//    std::string data_path(get_pod5_data_dir());
-//
-//    SECTION("pod5 file only, no read ids list") {
-//        CHECK(dorado::DataLoader::get_num_reads(data_path, std::nullopt) == 1);
-//    }
-//
-//    SECTION("pod5 file and read ids with 0 reads") {
-//        auto read_list = std::unordered_set<std::string>();
-//        CHECK(dorado::DataLoader::get_num_reads(data_path, read_list) == 0);
-//    }
-//    SECTION("pod5 file and read ids with 2 reads") {
-//        auto read_list = std::unordered_set<std::string>();
-//        read_list.insert("1");
-//        read_list.insert("2");
-//        CHECK(dorado::DataLoader::get_num_reads(data_path, read_list) == 1);
-//    }
-//}
-//
-//TEST_CASE(TEST_GROUP "Find sample rate from pod5.") {
-//    // Create a mock sink for testing output of reads
-//    std::string data_path(get_pod5_data_dir());
-//
-//    CHECK(dorado::DataLoader::get_sample_rate(data_path) == 4000);
-//}
-//
-//TEST_CASE(TEST_GROUP "Find sample rate from nested pod5.") {
-//    // Create a mock sink for testing output of reads
-//    std::string data_path(get_nested_pod5_data_dir());
-//
-//    CHECK(dorado::DataLoader::get_sample_rate(data_path, true) == 4000);
-//}
-//
-//TEST_CASE(TEST_GROUP "Load data sorted by channel id.") {
-//    std::string data_path(get_data_dir("multi_read_pod5"));
-//
-//    MessageSinkToVector<std::shared_ptr<dorado::Read>> sink(100);
-//    dorado::DataLoader loader(sink, "cpu", 1, 0);
-//    loader.load_reads(data_path, true, dorado::DataLoader::ReadOrder::BY_CHANNEL);
-//
-//    auto reads = sink.get_messages();
-//    int start_channel_id = -1;
-//    for (auto &i : reads) {
-//        CHECK(i->attributes.channel_number >= start_channel_id);
-//        start_channel_id = i->attributes.channel_number;
-//    }
-//}
+TEST_CASE(TEST_GROUP "Test loading single-read POD5 files") {
+    // Create a mock sink for testing output of reads
+    MockSink mock_sink;
+
+    std::string data_path(get_pod5_data_dir());
+    dorado::DataLoader loader(mock_sink, "cpu", 1);
+    loader.load_reads(data_path, false);
+
+    REQUIRE(mock_sink.get_read_count() == 1);
+}
+
+TEST_CASE(TEST_GROUP "Test loading single-read POD5 file, empty read list") {
+    // Create a mock sink for testing output of reads
+    MockSink mock_sink;
+
+    auto read_list = std::unordered_set<std::string>();
+    std::string data_path(get_pod5_data_dir());
+    dorado::DataLoader loader(mock_sink, "cpu", 1, 0, read_list);
+    loader.load_reads(data_path, false);
+
+    REQUIRE(mock_sink.get_read_count() == 0);
+}
+
+TEST_CASE(TEST_GROUP "Test loading single-read POD5 file, no read list") {
+    // Create a mock sink for testing output of reads
+    MockSink mock_sink;
+
+    std::string data_path(get_pod5_data_dir());
+    dorado::DataLoader loader(mock_sink, "cpu", 1, 0, std::nullopt);
+    loader.load_reads(data_path, false);
+
+    REQUIRE(mock_sink.get_read_count() == 1);
+}
+
+TEST_CASE(TEST_GROUP "Test loading single-read POD5 file, mismatched read list") {
+    // Create a mock sink for testing output of reads
+    MockSink mock_sink;
+
+    auto read_list = std::unordered_set<std::string>();
+    read_list.insert("read_1");
+    std::string data_path(get_pod5_data_dir());
+    dorado::DataLoader loader(mock_sink, "cpu", 1, 0, read_list);
+    loader.load_reads(data_path, false);
+
+    REQUIRE(mock_sink.get_read_count() == 0);
+}
+
+TEST_CASE(TEST_GROUP "Test loading single-read POD5 file, matched read list") {
+    // Create a mock sink for testing output of reads
+    MockSink mock_sink;
+
+    auto read_list = std::unordered_set<std::string>();
+    read_list.insert("002bd127-db82-436f-b828-28567c3d505d");  // read present in POD5
+    std::string data_path(get_pod5_data_dir());
+    dorado::DataLoader loader(mock_sink, "cpu", 1, 0, read_list);
+    loader.load_reads(data_path, false);
+
+    REQUIRE(mock_sink.get_read_count() == 1);
+}
+
+TEST_CASE(TEST_GROUP "Test calculating number of reads from pod5, read ids list.") {
+    // Create a mock sink for testing output of reads
+    std::string data_path(get_pod5_data_dir());
+
+    SECTION("pod5 file only, no read ids list") {
+        CHECK(dorado::DataLoader::get_num_reads(data_path, std::nullopt) == 1);
+    }
+
+    SECTION("pod5 file and read ids with 0 reads") {
+        auto read_list = std::unordered_set<std::string>();
+        CHECK(dorado::DataLoader::get_num_reads(data_path, read_list) == 0);
+    }
+    SECTION("pod5 file and read ids with 2 reads") {
+        auto read_list = std::unordered_set<std::string>();
+        read_list.insert("1");
+        read_list.insert("2");
+        CHECK(dorado::DataLoader::get_num_reads(data_path, read_list) == 1);
+    }
+}
+
+TEST_CASE(TEST_GROUP "Find sample rate from pod5.") {
+    // Create a mock sink for testing output of reads
+    std::string data_path(get_pod5_data_dir());
+
+    CHECK(dorado::DataLoader::get_sample_rate(data_path) == 4000);
+}
+
+TEST_CASE(TEST_GROUP "Find sample rate from nested pod5.") {
+    // Create a mock sink for testing output of reads
+    std::string data_path(get_nested_pod5_data_dir());
+
+    CHECK(dorado::DataLoader::get_sample_rate(data_path, true) == 4000);
+}
+
+TEST_CASE(TEST_GROUP "Load data sorted by channel id.") {
+    std::string data_path(get_data_dir("multi_read_pod5"));
+
+    MessageSinkToVector<std::shared_ptr<dorado::Read>> sink(100);
+    dorado::DataLoader loader(sink, "cpu", 1, 0);
+    loader.load_reads(data_path, true, dorado::DataLoader::ReadOrder::BY_CHANNEL);
+
+    auto reads = sink.get_messages();
+    int start_channel_id = -1;
+    for (auto &i : reads) {
+        CHECK(i->attributes.channel_number >= start_channel_id);
+        start_channel_id = i->attributes.channel_number;
+    }
+}
 
 TEST_CASE(TEST_GROUP "Test loading POD5 file with read ignore list") {
     std::string data_path(get_data_dir("multi_read_pod5"));
