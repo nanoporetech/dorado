2a1f5595c4dcf6219d133dae128bd7f38d6546fd
Chris Seymour <christopher.seymour@nanoporetech.com>
2023-07-03 18:10:27 +0100

    switch back to medmad scaling

--- dorado/read_pipeline/ScalerNode.cpp
+++ dorado/read_pipeline/ScalerNode.cpp
@@ -23,13 +23,23 @@ std::pair<float, float> ScalerNode::normalisation(torch::Tensor& x) {
     return {shift, scale};
 }
 
+#define EPS 1e-9f
+
+std::pair<float, float> calculate_med_mad(torch::Tensor& x, float factor = 1.4826) {
+    //Calculate signal median and median absolute deviation
+    auto med = x.median();
+    auto mad = torch::median(torch::abs(x - med)) * factor + EPS;
+
+    return {med.item<float>(), mad.item<float>()};
+}
+
 void ScalerNode::worker_thread() {
     Message message;
     while (m_work_queue.try_pop(message)) {
         // If this message isn't a read, we'll get a bad_variant_access exception.
         auto read = std::get<std::shared_ptr<Read>>(message);
 
-        const auto [shift, scale] = normalisation(read->raw_data);
+        const auto [shift, scale] = calculate_med_mad(read->raw_data);
         // raw_data comes from DataLoader with dtype int16.  We send it on as float16 after
         // shifting/scaling in float32 form.
         read->raw_data = ((read->raw_data.to(torch::kFloat) - shift) / scale).to(torch::kFloat16);
