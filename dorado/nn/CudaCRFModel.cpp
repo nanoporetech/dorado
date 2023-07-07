cbd67d52338168c0b43c04b472df836d15057737
Steve Malton <stephen.malton@nanoporetech.com>
2023-07-07 16:34:38 +0100

    Fix need for signalling under lock by using shared_ptr instead of local stack variables

--- dorado/nn/CudaCRFModel.cpp
+++ dorado/nn/CudaCRFModel.cpp
@@ -92,16 +92,16 @@ public:
         if (num_chunks == 0) {
             return std::vector<DecodedChunk>();
         }
-        NNTask task(input.to(m_options.device()), output, num_chunks);
+        auto task = std::make_shared<NNTask>(input.to(m_options.device()), output, num_chunks);
         {
             std::lock_guard<std::mutex> lock(m_input_lock);
-            m_input_queue.push_front(&task);
+            m_input_queue.push_front(task);
         }
         m_input_cv.notify_one();
 
-        std::unique_lock lock(task.mut);
-        while (!task.done) {
-            task.cv.wait(lock);
+        std::unique_lock lock(task->mut);
+        while (!task->done) {
+            task->cv.wait(lock);
         }
 
         return m_decoder->cpu_part(output);
@@ -131,7 +131,7 @@ public:
                 return;
             }
 
-            NNTask *task = m_input_queue.back();
+            auto task = m_input_queue.back();
             m_input_queue.pop_back();
             input_lock.unlock();
 
@@ -151,8 +151,8 @@ public:
             m_model_ms += forward_ms;
             m_decode_ms += forward_plus_decode_ms - forward_ms;
             task->done = true;
-            task->cv.notify_one();
             task_lock.unlock();
+            task->cv.notify_one();
         }
     }
     void terminate() { m_terminate.store(true); }
@@ -174,7 +174,7 @@ public:
     torch::nn::ModuleHolder<torch::nn::AnyModule> m_module{nullptr};
     size_t m_model_stride;
     std::atomic<bool> m_terminate{false};
-    std::deque<NNTask *> m_input_queue;
+    std::deque<std::shared_ptr<NNTask>> m_input_queue;
     std::mutex m_input_lock;
     std::condition_variable m_input_cv;
     std::unique_ptr<std::thread> m_cuda_thread;
