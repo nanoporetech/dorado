82cc8bf986978fadbcc3b072d582ed9462608def
Steve Malton <stephen.malton@nanoporetech.com>
2023-07-26 12:19:07 +0100

    Temp: Fix windows build

--- dorado/read_pipeline/Pipelines.h
+++ dorado/read_pipeline/Pipelines.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ReadPipeline.h"
+#include "nn/ModelRunner.h"
 #include "utils/types.h"
 
 #include <map>
@@ -15,7 +16,7 @@ class CRFModelConfig;
 class ModelRunnerBase;
 class ModBaseRunner;
 
-using Runner = class std::shared_ptr<dorado::ModelRunnerBase>;
+using Runner = std::shared_ptr<dorado::ModelRunnerBase>;
 using PairingParameters = std::variant<ReadOrder, std::map<std::string, std::string>>;
 
 namespace pipelines {
