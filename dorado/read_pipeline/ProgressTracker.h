79b3f723311b223b97640ebcb8cf0418dafa2484
George Nikitin <133176081+GeorgeNikitinNV@users.noreply.github.com>
2023-08-15 15:00:07 +1200

    fix formatting

--- dorado/read_pipeline/ProgressTracker.h
+++ dorado/read_pipeline/ProgressTracker.h
@@ -98,10 +98,10 @@ public:
         m_num_duplex_bases_filtered = fetch_stat("ReadFilterNode.duplex_bases_filtered");
 
 
-            // don't output progress bar if stderr is not a tty
-            if (!utils::is_fd_tty(stderr) & !m_enable_progress_bar) {
-                return;
-            }
+        // don't output progress bar if stderr is not a tty
+        if (!utils::is_fd_tty(stderr) & !m_enable_progress_bar) {
+            return;
+        }
 
 
         if (m_num_reads_expected != 0) {
