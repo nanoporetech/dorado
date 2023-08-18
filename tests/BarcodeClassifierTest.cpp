010944be0dfc789fb88cffde762400a715b6b5a9
Joyjit Daw <1127155+tijyojwad@users.noreply.github.com>
2023-08-18 19:28:31 +0100

    [tests] add tests for BarcodeClassifier class

---
+++ tests/BarcodeClassifierTest.cpp
@@ -0,0 +1,116 @@
+#include "read_pipeline/BarcodeClassifier.h"
+
+#include "MessageSinkUtils.h"
+#include "TestUtils.h"
+#include "htslib/sam.h"
+#include "read_pipeline/HtsReader.h"
+#include "utils/bam_utils.h"
+#include "utils/sequence_utils.h"
+
+#include <catch2/catch.hpp>
+
+#include <cstdint>
+#include <filesystem>
+#include <string>
+#include <vector>
+
+#define TEST_GROUP "[barcode_demux]"
+
+namespace fs = std::filesystem;
+
+using namespace dorado;
+
+TEST_CASE("BarcodeClassifier: check instantiation for all kits", TEST_GROUP) {
+    using Catch::Matchers::Contains;
+
+    auto barcoding_kits = demux::barcode_kits_list_str();
+
+    std::string s;
+    std::stringstream ss(barcoding_kits);
+
+    std::vector<std::string> kit_names;
+    while (std::getline(ss, s, ' ')) {
+        kit_names.push_back(s);
+    }
+
+    CHECK(kit_names.size() > 0);
+
+    for (auto& kit_name : kit_names) {
+        CHECK_NOTHROW(demux::BarcodeClassifier({kit_name}));
+    }
+
+    CHECK_NOTHROW(demux::BarcodeClassifier(kit_names));
+}
+
+TEST_CASE("BarcodeClassifier: instantiate barcode with unknown kit", TEST_GROUP) {
+    CHECK_THROWS(demux::BarcodeClassifier({"MY_RANDOM_KIT"}));
+}
+
+TEST_CASE("BarcodeCLassifier: test single ended barcode", TEST_GROUP) {
+    fs::path data_dir = fs::path(get_data_dir("barcode_demux/single_end"));
+
+    demux::BarcodeClassifier classifier({"SQK-RBK114-96"});
+
+    for (std::string bc :
+         {"SQK-RBK114-96_BC01", "SQK-RBK114-96_RBK39", "SQK-RBK114-96_BC92", "unclassified"}) {
+        auto bc_file = data_dir / (bc + ".fastq");
+        HtsReader reader(bc_file.string());
+        while (reader.read()) {
+            auto seqlen = reader.record->core.l_qseq;
+            auto bseq = bam_get_seq(reader.record);
+            std::string seq = utils::convert_nt16_to_str(bseq, seqlen);
+            auto res = classifier.barcode(seq);
+            if (res.adapter_name == "unclassified") {
+                CHECK(bc == res.adapter_name);
+            } else {
+                CHECK(bc == (res.kit + "_" + res.adapter_name));
+            }
+        }
+    }
+}
+
+TEST_CASE("BarcodeCLassifier: test double ended barcode", TEST_GROUP) {
+    fs::path data_dir = fs::path(get_data_dir("barcode_demux/double_end"));
+
+    demux::BarcodeClassifier classifier({"SQK-RPB004"});
+
+    for (std::string bc :
+         {"SQK-RPB004_BC01", "SQK-RPB004_BC05", "SQK-RPB004_BC11", "unclassified"}) {
+        auto bc_file = data_dir / (bc + ".fastq");
+        HtsReader reader(bc_file.string());
+        while (reader.read()) {
+            auto seqlen = reader.record->core.l_qseq;
+            auto bseq = bam_get_seq(reader.record);
+            std::string seq = utils::convert_nt16_to_str(bseq, seqlen);
+            auto res = classifier.barcode(seq);
+            if (res.adapter_name == "unclassified") {
+                CHECK(bc == res.adapter_name);
+            } else {
+                CHECK(bc == (res.kit + "_" + res.adapter_name));
+            }
+        }
+    }
+}
+
+TEST_CASE("BarcodeCLassifier: test double ended barcode with different variants", TEST_GROUP) {
+    fs::path data_dir = fs::path(get_data_dir("barcode_demux/double_end_variant"));
+
+    demux::BarcodeClassifier classifier({"EXP-PBC096"});
+
+    for (std::string bc :
+         {"EXP-PBC096_BC04", "EXP-PBC096_BC37", "EXP-PBC096_BC83", "unclassified"}) {
+        auto bc_file = data_dir / (bc + ".fastq");
+        HtsReader reader(bc_file.string());
+        while (reader.read()) {
+            auto seqlen = reader.record->core.l_qseq;
+            auto bseq = bam_get_seq(reader.record);
+            std::string seq = utils::convert_nt16_to_str(bseq, seqlen);
+            auto res = classifier.barcode(seq);
+            if (res.adapter_name == "unclassified") {
+                CHECK(bc == res.adapter_name);
+            } else {
+                CHECK(bc == (res.kit + "_" + res.adapter_name));
+            }
+        }
+    }
+}
--- tests/CliUtilsTest.cpp
+++ tests/CliUtilsTest.cpp
@@ -10,19 +10,19 @@ TEST_CASE("CliUtils: Check thread allocation", TEST_GROUP) {
     int aligner_threads, writer_threads;
 
     SECTION("fraction > 0") {
-        std::tie(aligner_threads, writer_threads) = aligner_writer_thread_allocation(10, 0.25f);
+        std::tie(aligner_threads, writer_threads) = worker_vs_writer_thread_allocation(10, 0.25f);
         REQUIRE(aligner_threads == 8);
         REQUIRE(writer_threads == 2);
     }
 
     SECTION("fraction == 0") {
-        std::tie(aligner_threads, writer_threads) = aligner_writer_thread_allocation(10, 0.0f);
+        std::tie(aligner_threads, writer_threads) = worker_vs_writer_thread_allocation(10, 0.0f);
         REQUIRE(aligner_threads == 9);
         REQUIRE(writer_threads == 1);
     }
 
     SECTION("fraction 100%") {
-        std::tie(aligner_threads, writer_threads) = aligner_writer_thread_allocation(10, 1.0f);
+        std::tie(aligner_threads, writer_threads) = worker_vs_writer_thread_allocation(10, 1.0f);
         REQUIRE(aligner_threads == 1);
         REQUIRE(writer_threads == 9);
     }
