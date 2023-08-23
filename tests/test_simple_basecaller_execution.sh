fe8b55ec0c2afbe76c7f009b0cb57f7d7e245ef3
Joyjit Daw <joyjit.daw@nanoporetech.com>
2023-08-22 22:53:22 -0400

    skip duplex tests on tegra

--- tests/test_simple_basecaller_execution.sh
+++ tests/test_simple_basecaller_execution.sh
@@ -44,28 +44,30 @@ $dorado_bin basecaller ${model} $data_dir/pod5 -b ${batch} --modified-bases 5mCG
 samtools quickcheck -u $output_dir/calls.bam
 samtools view -h $output_dir/calls.bam > $output_dir/calls.sam
 
-echo dorado duplex basespace test stage
-$dorado_bin duplex basespace $data_dir/basespace/pairs.bam --threads 1 --pairs $data_dir/basespace/pairs.txt > $output_dir/calls.bam
+if ! uname -r | grep -q tegra; then
+    echo dorado duplex basespace test stage
+    $dorado_bin duplex basespace $data_dir/basespace/pairs.bam --threads 1 --pairs $data_dir/basespace/pairs.txt > $output_dir/calls.bam
 
-echo dorado in-line duplex test stage
-duplex_model_name="dna_r10.4.1_e8.2_400bps_hac@v4.2.0"
-$dorado_bin download --model ${duplex_model_name} --directory ${output_dir}
-duplex_model=${output_dir}/${duplex_model_name}
-$dorado_bin duplex $duplex_model $data_dir/duplex/pod5 > $output_dir/duplex_calls.bam
-samtools quickcheck -u $output_dir/duplex_calls.bam
-num_duplex_reads=$(samtools view -c -d "dx:1" $output_dir/duplex_calls.bam)
-if [[ $num_duplex_reads -ne "2" ]]; then
-    echo "Duplex basecalling missing reads."
-    exit 1
-fi
+    echo dorado in-line duplex test stage
+    duplex_model_name="dna_r10.4.1_e8.2_400bps_hac@v4.2.0"
+    $dorado_bin download --model ${duplex_model_name} --directory ${output_dir}
+    duplex_model=${output_dir}/${duplex_model_name}
+    $dorado_bin duplex $duplex_model $data_dir/duplex/pod5 > $output_dir/duplex_calls.bam
+    samtools quickcheck -u $output_dir/duplex_calls.bam
+    num_duplex_reads=$(samtools view -c -d "dx:1" $output_dir/duplex_calls.bam)
+    if [[ $num_duplex_reads -ne "2" ]]; then
+        echo "Duplex basecalling missing reads."
+        exit 1
+    fi
 
-echo dorado pairs file based duplex test stage
-$dorado_bin duplex $duplex_model $data_dir/duplex/pod5 --pairs $data_dir/duplex/pairs.txt > $output_dir/duplex_calls.bam
-samtools quickcheck -u $output_dir/duplex_calls.bam
-num_duplex_reads=$(samtools view -c -d "dx:1" $output_dir/duplex_calls.bam)
-if [[ $num_duplex_reads -ne "2" ]]; then
-    echo "Duplex basecalling missing reads."
-    exit 1
+    echo dorado pairs file based duplex test stage
+    $dorado_bin duplex $duplex_model $data_dir/duplex/pod5 --pairs $data_dir/duplex/pairs.txt > $output_dir/duplex_calls.bam
+    samtools quickcheck -u $output_dir/duplex_calls.bam
+    num_duplex_reads=$(samtools view -c -d "dx:1" $output_dir/duplex_calls.bam)
+    if [[ $num_duplex_reads -ne "2" ]]; then
+        echo "Duplex basecalling missing reads."
+        exit 1
+    fi
 fi
 
 if command -v truncate > /dev/null
