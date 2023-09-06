55f027f726b70b6af63e1d89c24306c8c68b3328
Ben Lawrence <ben.lawrence@nanoporetech.com>
2023-09-06 11:48:43 +0100

    [DOR-315] Skip code signing on x64 to prevent crashes
    See DOR-315.

--- cmake/DoradoCodesigning.cmake
+++ cmake/DoradoCodesigning.cmake
@@ -3,6 +3,14 @@
 # Note that this script is intended to run after CPack install phase but before the build.
 #
 
+# x64 builds can't be notarized on our CI machines and codesigning by itself doesn't
+# affect the end user, but if we enable the hardened runtime then we get crashes when
+# basecalling due to torch jitting code so skip signing entirely.
+if (CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")
+    message(STATUS "Skipping code signing on x64 due to torch jitting")
+    return()
+endif()
+
 # Grab the identity from the environment
 set(DORADO_CODESIGN_IDENTITY "$ENV{APPLE_CODESIGN_IDENTITY}")
 if ("${DORADO_CODESIGN_IDENTITY}" STREQUAL "")
