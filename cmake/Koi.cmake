5bb54cf5a1ae396a43515266c8fee20cd0ccdb71
Georg Kolling <georg.kolling@nanoporetech.com>
2023-09-14 15:31:12 +0100

    Bump Koi version to 0.3.8

--- cmake/Koi.cmake
+++ cmake/Koi.cmake
@@ -20,7 +20,7 @@ endfunction()
 
 if(CMAKE_SYSTEM_NAME STREQUAL "Linux" OR WIN32)
 
-    set(KOI_VERSION 0.3.7)
+    set(KOI_VERSION 0.3.8)
     if(BUILD_KOI_FROM_SOURCE)
         message(STATUS "Building Koi from source")
         set(KOI_DIR "${DORADO_3RD_PARTY}/koi")
