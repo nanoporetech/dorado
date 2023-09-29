096f2394020c6994175c21f6e8d05bedf07e1e6d
Ben Lawrence <ben.lawrence@nanoporetech.com>
2023-09-29 12:28:11 +0100

    Separate 3rdparty into source and download locations
    _SOURCE is for any resources that are part of the repo or submodules,
    and _DOWNLOAD is for anything that needs to be downloaded.
    
    This way you can share a single checkout between multiple machines or
    docker images by setting DORADO_3RD_PARTY_DOWNLOAD to something
    specific per-machine.

--- cmake/HDF5.cmake
+++ cmake/HDF5.cmake
@@ -8,33 +8,33 @@ if((CMAKE_SYSTEM_NAME STREQUAL "Linux") AND (CMAKE_SYSTEM_PROCESSOR STREQUAL "aa
 
     set(DYNAMIC_HDF ON)
     set(HDF_VER hdf5-1.10.0-aarch64)
-    if(EXISTS ${DORADO_3RD_PARTY}/${HDF_VER})
+    if(EXISTS ${DORADO_3RD_PARTY_DOWNLOAD}/${HDF_VER})
         message(STATUS "Found ${HDF_VER}")
     else()    
         download_and_extract(https://cdn.oxfordnanoportal.com/software/analysis/${HDF_VER}.zip ${HDF_VER})
     endif()
-    list(PREPEND CMAKE_PREFIX_PATH ${DORADO_3RD_PARTY}/${HDF_VER}/${HDF_VER})
+    list(PREPEND CMAKE_PREFIX_PATH ${DORADO_3RD_PARTY_DOWNLOAD}/${HDF_VER}/${HDF_VER})
 
 elseif(WIN32)
     # On windows, we need to build HDF5
 
-    if(EXISTS ${DORADO_3RD_PARTY}/${HDF_VER})
+    if(EXISTS ${DORADO_3RD_PARTY_DOWNLOAD}/${HDF_VER})
         message(STATUS "Found ${HDF_VER}")
     else()
         # Need a zlib build for HDF to use
         download_and_extract(https://github.com/madler/zlib/archive/refs/tags/v${ZLIB_VER}.tar.gz zlib-${ZLIB_VER})
-        execute_process(COMMAND cmake -S ${DORADO_3RD_PARTY}/zlib-${ZLIB_VER}/zlib-${ZLIB_VER} -B ${DORADO_3RD_PARTY}/zlib-${ZLIB_VER}/zlib-${ZLIB_VER}/cmake-build -A x64
-            -DCMAKE_INSTALL_PREFIX=${DORADO_3RD_PARTY}/zlib-${ZLIB_VER}/install)
-        execute_process(COMMAND cmake --build ${DORADO_3RD_PARTY}/zlib-${ZLIB_VER}/zlib-${ZLIB_VER}/cmake-build --config Release --target install)
+        execute_process(COMMAND cmake -S ${DORADO_3RD_PARTY_DOWNLOAD}/zlib-${ZLIB_VER}/zlib-${ZLIB_VER} -B ${DORADO_3RD_PARTY_DOWNLOAD}/zlib-${ZLIB_VER}/zlib-${ZLIB_VER}/cmake-build -A x64
+            -DCMAKE_INSTALL_PREFIX=${DORADO_3RD_PARTY_DOWNLOAD}/zlib-${ZLIB_VER}/install)
+        execute_process(COMMAND cmake --build ${DORADO_3RD_PARTY_DOWNLOAD}/zlib-${ZLIB_VER}/zlib-${ZLIB_VER}/cmake-build --config Release --target install)
 
         # HDF5 itself
         download_and_extract(https://nanoporetech.box.com/shared/static/h5u267duw3sa4l814yirmxamx3hgouwp.zip ${HDF_VER})
     endif()
 
-    list(APPEND CMAKE_PREFIX_PATH ${DORADO_3RD_PARTY}/zlib-${ZLIB_VER}/install)
+    list(APPEND CMAKE_PREFIX_PATH ${DORADO_3RD_PARTY_DOWNLOAD}/zlib-${ZLIB_VER}/install)
 
-    install(FILES ${DORADO_3RD_PARTY}/zlib-${ZLIB_VER}/install/bin/zlib.dll DESTINATION bin)
-    list(APPEND CMAKE_PREFIX_PATH ${DORADO_3RD_PARTY}/${HDF_VER}/${HDF_VER})
+    install(FILES ${DORADO_3RD_PARTY_DOWNLOAD}/zlib-${ZLIB_VER}/install/bin/zlib.dll DESTINATION bin)
+    list(APPEND CMAKE_PREFIX_PATH ${DORADO_3RD_PARTY_DOWNLOAD}/${HDF_VER}/${HDF_VER})
 
 endif()
 
--- cmake/VbzPlugin.cmake
+++ cmake/VbzPlugin.cmake
@@ -9,12 +9,12 @@ function(add_hdf_vbz_plugin)
     if(WIN32)
         # On windows we need to build a static lib for zstd as there's no prebuilt distro
         set(ZSTD_BUILD_DIR ${CMAKE_BINARY_DIR}/cmake-build-zstd)
-        execute_process(COMMAND cmake -S ${DORADO_3RD_PARTY}/zstd/build/cmake -B ${ZSTD_BUILD_DIR} -A x64)
+        execute_process(COMMAND cmake -S ${DORADO_3RD_PARTY_SOURCE}/zstd/build/cmake -B ${ZSTD_BUILD_DIR} -A x64)
         execute_process(COMMAND cmake --build ${ZSTD_BUILD_DIR} --config Release)
 
         # On windows we need to tell hdf_plugins where we put the built zstd lib
-        set(CONAN_INCLUDE_DIRS_RELEASE ${DORADO_3RD_PARTY}/zstd/lib)
-        set(CONAN_INCLUDE_DIRS_DEBUG ${DORADO_3RD_PARTY}/zstd/lib)
+        set(CONAN_INCLUDE_DIRS_RELEASE ${DORADO_3RD_PARTY_SOURCE}/zstd/lib)
+        set(CONAN_INCLUDE_DIRS_DEBUG ${DORADO_3RD_PARTY_SOURCE}/zstd/lib)
         set(CONAN_LIB_DIRS_RELEASE ${ZSTD_BUILD_DIR}/lib/Release)
         set(CONAN_LIB_DIRS_DEBUG ${ZSTD_BUILD_DIR}/lib/Debug)
 
