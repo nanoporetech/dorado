3fd0607b45a192f9897d591ae5440e04ec9a506c
Hugh Pendry <hugh.pendry@nanoporetech.com>
2023-10-11 10:45:10 +0100

    code tidying

--- dorado/read_pipeline/BarcodeClassifierSelector.cpp
+++ dorado/read_pipeline/BarcodeClassifierSelector.cpp
@@ -3,11 +3,11 @@
 namespace dorado::demux {
 
 BarcodeClassifier* BarcodeClassifierSelector::get_barcoder(const std::string& barcode_kit) {
-    std::lock_guard<std::mutex> lock(m_mutex);
     assert(!barcode_kit.empty());
+    std::lock_guard<std::mutex> lock(m_mutex);
     if (!m_barcoder_lut.count(barcode_kit)) {
-        std::vector<std::string> kits{barcode_kit};
-        m_barcoder_lut.emplace(barcode_kit, BarcodeClassifier{kits});
+        m_barcoder_lut.emplace(barcode_kit,
+                               BarcodeClassifier{std::vector<std::string>{barcode_kit}});
     }
     return &m_barcoder_lut.at(barcode_kit);
 }
