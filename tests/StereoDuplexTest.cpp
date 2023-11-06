02882ebd21a63ff2f0f283ce1c23f11f8da77c3d
Stuart Abercrombie <sabercrombie@shadow-moses6.oxfordnanolabs.local>
2023-11-06 22:27:22 +0000

    Fix read time stamps problem for debug builds.

--- tests/StereoDuplexTest.cpp
+++ tests/StereoDuplexTest.cpp
@@ -29,6 +29,7 @@ TEST_CASE(TEST_GROUP "Encoder") {
                     DataPath("template_raw_data.tensor").string());
         template_read.read_common.raw_data = template_read.read_common.raw_data.to(torch::kFloat16);
         template_read.read_common.run_id = "test_run";
+        template_read.read_common.start_time_ms = static_cast<uint64_t>(0);
         template_read.seq_start = 0;
         template_read.seq_end = template_read.read_common.seq.length();
     }
@@ -42,6 +43,7 @@ TEST_CASE(TEST_GROUP "Encoder") {
                     DataPath("complement_raw_data.tensor").string());
         complement_read.read_common.raw_data =
                 complement_read.read_common.raw_data.to(torch::kFloat16);
+        complement_read.read_common.start_time_ms = static_cast<uint64_t>(100);
         complement_read.seq_start = 0;
         complement_read.seq_end = complement_read.read_common.seq.length();
     }
@@ -63,8 +65,10 @@ TEST_CASE(TEST_GROUP "Encoder") {
     REQUIRE(stereo_read->read_common.run_id == template_read.read_common.run_id);
 
     // Encode with swapped template and complement reads
-    read_pair.template_read = complement_read;
-    read_pair.complement_read = template_read;
+    std::swap(read_pair.template_read, read_pair.complement_read);
+    std::swap(read_pair.template_read.read_common.start_time_ms,
+              read_pair.complement_read.read_common.start_time_ms);
+
     const auto swapped_stereo_read = stereo_node.stereo_encode(read_pair);
     // Check if the encoded signal is NOT equal to the expected stereo_raw_data
     REQUIRE(!torch::equal(stereo_raw_data, swapped_stereo_read->read_common.raw_data));
