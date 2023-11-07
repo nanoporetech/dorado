d5db2fa2e38198ec53c44d1b6c72fedce42b53cb
Steve Malton <stephen.malton@nanoporetech.com>
2023-11-07 11:19:25 +0000

    Improvements for subread tagger (ported from minknow_5.8 branch) - Store read groups by read tag - Only process the read tags that we know have been updated
    This enormously reduces the amount of work the subread taggger has to do

--- dorado/read_pipeline/SubreadTaggerNode.h
+++ dorado/read_pipeline/SubreadTaggerNode.h
@@ -8,6 +8,7 @@
 #include <mutex>
 #include <thread>
 #include <unordered_map>
+#include <unordered_set>
 #include <utility>
 #include <vector>
 
@@ -26,17 +27,25 @@ private:
     void start_threads();
     void terminate_impl();
     void worker_thread();
+    void check_duplex_thread();
 
     int m_num_worker_threads = 0;
     std::vector<std::unique_ptr<std::thread>> m_worker_threads;
+    std::unique_ptr<std::thread> m_duplex_thread;
 
     std::mutex m_subread_groups_mutex;
     std::unordered_map<uint64_t, std::vector<SimplexReadPtr>> m_subread_groups;
 
+    std::mutex m_updated_read_tags_mutex;
+
     std::mutex m_duplex_reads_mutex;
-    std::list<DuplexReadPtr> m_duplex_reads;
-    std::list<std::pair<std::vector<SimplexReadPtr>, std::vector<DuplexReadPtr>>>
+    std::condition_variable m_check_duplex_cv;
+    std::unordered_set<uint64_t> m_updated_read_tags;
+    std::unordered_map<uint64_t, std::list<DuplexReadPtr>> m_duplex_reads;
+    std::unordered_map<uint64_t, std::pair<std::vector<SimplexReadPtr>, std::vector<DuplexReadPtr>>>
             m_full_subread_groups;
+
+    std::atomic_bool m_terminate{false};
 };
 
 }  // namespace dorado
