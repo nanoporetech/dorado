8ee144cad6ed5399ac293f2b5b86ced668666607
Stuart Abercrombie <sabercrombie@shadow-moses6.oxfordnanolabs.local>
2023-11-08 23:59:23 +0000

    Move encoding generation out of StereoDuplexEncoderNode

---
+++ dorado/read_pipeline/stereo_features.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "ReadPipeline.h"
+#include <ATen/ATen.h>
+
+namespace dorado {
+
+at::Tensor GenerateStereoFeatures(const DuplexRead::StereoFeatureInputs& feature_inputs);
+
+}  // namespace dorado
