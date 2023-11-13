32d3a1467e5fe82de12341c735d42885e87caf1c
Ben Lawrence <ben.lawrence@nanoporetech.com>
2023-11-13 11:46:01 +0000

    Replace a reference with a copy
    mach_port_t is trivial and IOServiceGetMatchingService() doesn't take
    it by reference so it's safe to copy it.
    
    This was suggested in !703.

--- dorado/utils/metal_utils.cpp
+++ dorado/utils/metal_utils.cpp
@@ -65,7 +65,7 @@ std::optional<int64_t> retrieve_ioreg_prop(const std::string &service_class,
 
 #if MAC_OS_X_VERSION_MIN_REQUIRED < 120000 /* MAC_OS_VERSION_12_0 */
     // These are the same variable, just renamed in macOS 12+.
-    const auto &kIOMainPortDefault = kIOMasterPortDefault;
+    const mach_port_t kIOMainPortDefault = kIOMasterPortDefault;
 #endif
     // IOServiceGetMatchingService consumes a reference to matching_dict, so we don't need
     // to release it ourselves.
