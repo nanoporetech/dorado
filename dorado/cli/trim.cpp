0c1d10e614a0228b7c5ce418b1bc89781fcd218b
Kevin Dolan <Kevin.Dolan@nanoporetech.com>
2023-11-24 14:45:38 +0000

    Added trim cli file.

---
+++ dorado/cli/trim.cpp
@@ -0,0 +1,158 @@
+#include "Version.h"
+#include "cli/cli_utils.h"
+#include "read_pipeline/AdapterDetectorNode.h"
+#include "read_pipeline/HtsReader.h"
+#include "read_pipeline/ProgressTracker.h"
+#include "utils/basecaller_utils.h"
+#include "utils/log_utils.h"
+#include "utils/stats.h"
+
+#include <spdlog/spdlog.h>
+
+#include <chrono>
+#include <memory>
+#include <optional>
+#include <string>
+#include <thread>
+#include <vector>
+using namespace std::chrono_literals;
+
+#ifndef _WIN32
+#include <unistd.h>
+#endif
+
+namespace dorado {
+
+namespace {
+
+void add_pg_hdr(sam_hdr_t* hdr) {
+    sam_hdr_add_line(hdr, "PG", "ID", "trim", "PN", "dorado", "VN", DORADO_VERSION, NULL);
+}
+
+}  // anonymous namespace
+
+int trimmer(int argc, char* argv[]) {
+    utils::InitLogging();
+
+    argparse::ArgumentParser parser("dorado", DORADO_VERSION, argparse::default_arguments::help);
+    parser.add_description("Adapter/primer trimming tool.");
+    parser.add_argument("reads")
+            .help("Path to a file with reads to trim. Can be in any HTS format.")
+            .nargs(argparse::nargs_pattern::any);
+    parser.add_argument("--output-dir").help("Output folder for trimmed reads.").required();
+    parser.add_argument("-t", "--threads")
+            .help("Combined number of threads for barcoding and output generation. Default uses "
+                  "all available threads.")
+            .default_value(0)
+            .scan<'i', int>();
+    parser.add_argument("-n", "--max-reads")
+            .help("Maximum number of reads to process. Mainly for debugging. Process all reads by "
+                  "default.")
+            .default_value(0)
+            .scan<'i', int>();
+    parser.add_argument("-l", "--read-ids")
+            .help("A file with a newline-delimited list of reads to trim.")
+            .default_value(std::string(""));
+    int verbosity = 0;
+    parser.add_argument("-v", "--verbose")
+            .default_value(false)
+            .implicit_value(true)
+            .nargs(0)
+            .action([&](const auto&) { ++verbosity; })
+            .append();
+    parser.add_argument("--emit-fastq")
+            .help("Output in fastq format. Default is BAM.")
+            .default_value(false)
+            .implicit_value(true);
+    parser.add_argument("--no-trim-primers")
+            .help("Skip primer detection and trimming trimming. Only adapters will be detected and "
+                  "trimmed.")
+            .default_value(false)
+            .implicit_value(true);
+
+    try {
+        parser.parse_args(argc, argv);
+    } catch (const std::exception& e) {
+        std::ostringstream parser_stream;
+        parser_stream << parser;
+        spdlog::error("{}\n{}", e.what(), parser_stream.str());
+        std::exit(1);
+    }
+
+    if (parser.get<bool>("--verbose")) {
+        utils::SetVerboseLogging(static_cast<dorado::utils::VerboseLogLevel>(verbosity));
+    }
+
+    auto reads(parser.get<std::vector<std::string>>("reads"));
+    auto output_dir(parser.get<std::string>("output-dir"));
+    auto threads(parser.get<int>("threads"));
+    auto max_reads(parser.get<int>("max-reads"));
+
+    threads = threads == 0 ? std::thread::hardware_concurrency() : threads;
+
+    auto read_list = utils::load_read_list(parser.get<std::string>("--read-ids"));
+
+    if (reads.empty()) {
+#ifndef _WIN32
+        if (isatty(fileno(stdin))) {
+            std::cout << parser << std::endl;
+            return 1;
+        }
+#endif
+        reads.push_back("-");
+    } else if (reads.size() > 1) {
+        spdlog::error("> multi file input not yet handled");
+        return 1;
+    }
+
+    HtsReader reader(reads[0], read_list);
+    auto header = SamHdrPtr(sam_hdr_dup(reader.header));
+    add_pg_hdr(header.get());
+
+    PipelineDescriptor pipeline_desc;
+
+    pipeline_desc.add_node<AdapterDetectorNode>({}, threads, true,
+                                                parser.get<bool>("--no-trim-primers"));
+
+    // Create the Pipeline from our description.
+    std::vector<dorado::stats::StatsReporter> stats_reporters;
+    auto pipeline = Pipeline::create(std::move(pipeline_desc), &stats_reporters);
+    if (pipeline == nullptr) {
+        spdlog::error("Failed to create pipeline");
+        std::exit(EXIT_FAILURE);
+    }
+
+    // At present, header output file header writing relies on direct node method calls
+    // rather than the pipeline framework.
+    auto& demux_writer_ref =
+            dynamic_cast<BarcodeDemuxerNode&>(pipeline->get_node_ref(demux_writer));
+    demux_writer_ref.set_header(header.get());
+
+    // Set up stats counting
+    std::vector<dorado::stats::StatsCallable> stats_callables;
+    ProgressTracker tracker(0, false);
+    stats_callables.push_back(
+            [&tracker](const stats::NamedStats& stats) { tracker.update_progress_bar(stats); });
+    constexpr auto kStatsPeriod = 100ms;
+    auto stats_sampler = std::make_unique<dorado::stats::StatsSampler>(
+            kStatsPeriod, stats_reporters, stats_callables, static_cast<size_t>(0));
+    // End stats counting setup.
+
+    spdlog::info("> starting adapter/primer trimming");
+    reader.read(*pipeline, max_reads);
+
+    // Wait for the pipeline to complete.  When it does, we collect
+    // final stats to allow accurate summarisation.
+    auto final_stats = pipeline->terminate(DefaultFlushOptions());
+
+    stats_sampler->terminate();
+
+    tracker.update_progress_bar(final_stats);
+    tracker.summarize();
+
+    spdlog::info("> finished barcode demuxing");
+
+    return 0;
+}
+
+}  // namespace dorado
