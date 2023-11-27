70a69ea14043c02f1350393c1ed03e5ae783bc12
Ben Lawrence <ben.lawrence@nanoporetech.com>
2023-11-27 11:54:05 +0000

    [DOR-365] Remove default arg from GPUDecoder
    This is always set by the caller.

--- dorado/decode/GPUDecoder.h
+++ dorado/decode/GPUDecoder.h
@@ -8,7 +8,7 @@ namespace dorado {
 
 class GPUDecoder : Decoder {
 public:
-    explicit GPUDecoder(float score_clamp_val = 0) : m_score_clamp_val(score_clamp_val) {}
+    explicit GPUDecoder(float score_clamp_val) : m_score_clamp_val(score_clamp_val) {}
 
     std::vector<DecodedChunk> beam_search(const at::Tensor& scores,
                                           int num_chunks,
