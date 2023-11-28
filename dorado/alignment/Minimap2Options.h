81f7d281efa500e9440e25838b4dd724ef349689
Hugh Pendry <hugh.pendry@nanoporetech.com>
2023-11-28 14:56:01 +0000

    Added comparison functions for Minimap2MappingOptions

--- dorado/alignment/Minimap2Options.h
+++ dorado/alignment/Minimap2Options.h
@@ -46,10 +46,48 @@ struct Minimap2MappingOptions {
     bool print_secondary;
 };
 
+inline bool operator<(const Minimap2MappingOptions& l, const Minimap2MappingOptions& r) {
+    return std::tie(l.best_n_secondary, l.bandwidth, l.bandwidth_long, l.soft_clipping,
+                    l.secondary_seq, l.print_secondary) <
+           std::tie(r.best_n_secondary, r.bandwidth, r.bandwidth_long, r.soft_clipping,
+                    r.secondary_seq, r.print_secondary);
+}
+
+inline bool operator>(const Minimap2MappingOptions& l, const Minimap2MappingOptions& r) {
+    return r < l;
+}
+
+inline bool operator<=(const Minimap2MappingOptions& l, const Minimap2MappingOptions& r) {
+    return !(l > r);
+}
+
+inline bool operator>=(const Minimap2MappingOptions& l, const Minimap2MappingOptions& r) {
+    return !(l < r);
+}
+
+inline bool operator==(const Minimap2MappingOptions& l, const Minimap2MappingOptions& r) {
+    return std::tie(l.best_n_secondary, l.bandwidth, l.bandwidth_long, l.soft_clipping,
+                    l.secondary_seq, l.print_secondary) ==
+           std::tie(r.best_n_secondary, r.bandwidth, r.bandwidth_long, r.soft_clipping,
+                    r.secondary_seq, r.print_secondary);
+}
+
+inline bool operator!=(const Minimap2MappingOptions& l, const Minimap2MappingOptions& r) {
+    return !(l == r);
+}
+
 struct Minimap2Options : public Minimap2IndexOptions, public Minimap2MappingOptions {
     bool print_aln_seq;
 };
+
+inline bool operator==(const Minimap2Options& l, const Minimap2Options& r) {
+    return static_cast<Minimap2IndexOptions>(l) == static_cast<Minimap2IndexOptions>(r) &&
+           static_cast<Minimap2MappingOptions>(l) == static_cast<Minimap2MappingOptions>(r);
+}
+
+inline bool operator!=(const Minimap2Options& l, const Minimap2Options& r) { return !(l == r); }
+
 static constexpr Minimap2Options dflt_options{{15, 10, 16000000000ull},
-                                              {true, 5, 500, 20000, false, false},
+                                              {5, 500, 20000, false, false, true},
                                               false};
 }  // namespace dorado::alignment
