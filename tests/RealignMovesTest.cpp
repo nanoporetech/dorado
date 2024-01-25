3cf09afa95ac75a822ef0634a5e612929c360ae2
Ben Lawrence <ben.lawrence@nanoporetech.com>
2024-01-25 10:21:53 +0000

    Revert "Remove test which is causing CI failure due to Minimap bug (https://github.com/lh3/minimap2/issues/1147)"
    This reverts commit d2bb114a5825ef972f417b38d3e3af3a84fde0e9.

--- tests/RealignMovesTest.cpp
+++ tests/RealignMovesTest.cpp
@@ -19,3 +19,23 @@ TEST_CASE("Realign Moves No Error", TEST_GROUP) {
     // Test that calling realign_moves does not throw any exceptions
     CHECK_NOTHROW(utils::realign_moves(query_sequence, target_sequence, moves));
 }
+
+TEST_CASE("No alignment doesn't produce an error", TEST_GROUP) {
+    std::string query_sequence = "ACGT";                    // Example query sequence
+    std::string target_sequence = "TGAC";                   // Example target sequence
+    std::vector<uint8_t> moves = {1, 0, 1, 0, 1, 0, 0, 1};  // Original moves vector
+
+    // Check that the function does not throw an exception
+    REQUIRE_NOTHROW(utils::realign_moves(query_sequence, target_sequence, moves));
+
+    // Call the function and store the result
+    int move_offset, target_start;
+    std::vector<uint8_t> new_moves;
+
+    CHECK_NOTHROW(std::tie(move_offset, target_start, new_moves) =
+                          utils::realign_moves(query_sequence, target_sequence, moves));
+
+    CHECK(move_offset == -1);
+    CHECK(target_start == -1);
+    CHECK(new_moves.empty());
+}
