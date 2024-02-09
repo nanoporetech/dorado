81d236ba77cb52e438d324f4cd0b4308d265fb3f
Ben Lawrence <ben.lawrence@nanoporetech.com>
2024-02-09 12:12:58 +0000

    [DOR-564] Move the setup/teardown logic out of the cmakes
    Different machines may have different devices available so we can't
    hardcode a specific device to use. Also test on an iPad since that's
    going to be our use case.

--- tests/run_ios_test.py
+++ tests/run_ios_test.py
@@ -5,6 +5,68 @@
 #
 import subprocess
 from argparse import ArgumentParser
+import typing as tp
+import json
+
+
+def get_devices() -> list[tuple[str, str]]:
+    print("Getting device list")
+    exe_args = [
+        "xcrun",
+        "simctl",
+        "list",
+        "-j",
+    ]
+    output = json.loads(subprocess.check_output(exe_args))
+    devices = []
+    for device_list in output["devices"].values():
+        for device in device_list:
+            state = device["state"]
+            if device["isAvailable"] and (state == "Booted" or state == "Shutdown"):
+                devices.append((device["name"], state))
+    return devices
+
+
+def setup_device(device: tp.Optional[str]) -> bool:
+    needs_booting = True
+
+    # Pick a device if one isn't provided.
+    if device is None:
+        devices = get_devices()
+        for name, state in devices:
+            if "iPad" in name and "Pro" in name:
+                device = name
+                needs_booting = state != "Booted"
+                break
+        if device is None:
+            raise RuntimeError(f"Couldn't find appropriate device in: {devices}")
+        print(f"Chose device '{device}'")
+
+    if needs_booting:
+        print(f"Booting up device: '{device}'")
+        exe_args = [
+            "xcrun",
+            "simctl",
+            "boot",
+            device,
+        ]
+        return subprocess.call(exe_args) == 0
+    else:
+        print("Device is already booted")
+        return True
+
+
+def teardown_device(device: tp.Optional[str]) -> bool:
+    if device is None:
+        device = "all"
+    print(f"Shutting down device: '{device}'")
+    exe_args = [
+        "xcrun",
+        "simctl",
+        "shutdown",
+        device,
+    ]
+    return subprocess.call(exe_args) == 0
 
 
 def install_app(device: str, app_path: str) -> bool:
@@ -57,18 +119,33 @@ def run_test(device: str, identifier: str) -> bool:
 
 
 if __name__ == "__main__":
-    # Parse args.
     parser = ArgumentParser()
-    parser.add_argument("--app_path", required=True, type=str)
-    parser.add_argument("--identifier", required=True, type=str)
-    parser.add_argument("--device", default="booted", type=str)
+    # Main operation.
+    options = parser.add_mutually_exclusive_group(required=True)
+    options.add_argument("--setup", action="store_true")
+    options.add_argument("--teardown", action="store_true")
+    options.add_argument("--run", action="store_true")
+    # Extra args.
+    parser.add_argument("--app_path", type=str)
+    parser.add_argument("--identifier", type=str)
+    parser.add_argument("--device", type=str)
     args = parser.parse_args()
 
-    # Uninstall the app first so that we can't accidentally run an old build.
-    uninstall_app(args.device, args.identifier)
-    install_app(args.device, args.app_path)
-    success = run_test(args.device, args.identifier)
-    uninstall_app(args.device, args.identifier)
+    # Handle the args.
+    success = False
+    if args.setup:
+        success = setup_device(args.device)
+    elif args.teardown:
+        success = teardown_device(args.device)
+    elif args.run:
+        # Assume a device is already running.
+        if not args.device:
+            args.device = "booted"
+        # Uninstall the app first so that we can't accidentally run an old build.
+        uninstall_app(args.device, args.identifier)
+        install_app(args.device, args.app_path)
+        success = run_test(args.device, args.identifier)
+        uninstall_app(args.device, args.identifier)
 
     # Return the result to the ctest runner.
     if not success:
