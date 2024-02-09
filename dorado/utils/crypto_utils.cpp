f50d6c37deb914ebb550742d886ecd22e262f5db
Ben Lawrence <ben.lawrence@nanoporetech.com>
2024-02-09 17:45:23 +0000

    [DOR-564] Simplify SHA256 size checks

--- dorado/utils/crypto_utils.cpp
+++ dorado/utils/crypto_utils.cpp
@@ -2,24 +2,20 @@
 
 #if defined(__APPLE__)
 #include <CommonCrypto/CommonDigest.h>
-static constexpr auto kDigestLength = CC_SHA256_DIGEST_LENGTH;
 #else
 #include <openssl/sha.h>
-static constexpr auto kDigestLength = SHA256_DIGEST_LENGTH;
 #endif
 
 namespace dorado::utils::crypto {
 
 SHA256Digest sha256(std::string_view data) {
-    // Make sure we match the size of the API we're using.
-    static_assert(std::size(SHA256Digest{}) == kDigestLength);
-    static_assert(sizeof(SHA256Digest) == kDigestLength);
-
     // Digest the data.
     SHA256Digest hash{};
 #if defined(__APPLE__)
+    static_assert(std::size(SHA256Digest{}) == CC_SHA256_DIGEST_LENGTH);
     ::CC_SHA256(data.data(), static_cast<CC_LONG>(data.size()), hash.data());
 #else
+    static_assert(std::size(SHA256Digest{}) == SHA256_DIGEST_LENGTH);
     ::SHA256(reinterpret_cast<const unsigned char *>(data.data()), data.size(), hash.data());
 #endif
     return hash;
