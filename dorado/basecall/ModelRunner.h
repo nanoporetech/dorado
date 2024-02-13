3238e9ca36c23ea4bdc6857c4aa7c9332ccc4581
Georg Kolling <georg.kolling@nanoporetech.com>
2024-02-13 15:12:08 +0000

    Remove superfluous ModelRunner::batch_timeout_ms()

--- dorado/basecall/ModelRunner.h
+++ dorado/basecall/ModelRunner.h
@@ -24,8 +24,6 @@ public:
     size_t model_stride() const final { return m_config.stride; }
     size_t chunk_size() const final { return m_input.size(2); }
     size_t batch_size() const final { return m_input.size(0); }
-    // Timeout is short for simplex, longer for duplex which gets a subset of reads
-    int batch_timeout_ms() const final { return is_duplex_model(m_config) ? 100 : 5000; }
     void terminate() final {}
     void restart() final {}
     std::string get_name() const final { return "ModelRunner"; }
