0f1c8bd82a5377c41fbdc411394fa1d344431d3a
Stuart Abercrombie <stuart.abercrombie@nanoporetech.com>
2024-02-14 16:39:54 +0000

    Add ReadForwarderNode

---
+++ tests/ReadForwarderNodeTest.cpp
@@ -0,0 +1,28 @@
+#include "read_pipeline/ReadForwarderNode.h"
+
+#include "read_pipeline/ReadPipeline.h"
+
+#include <catch2/catch.hpp>
+
+#define TEST_GROUP "[ReadForwarderNodeTest]"
+
+TEST_CASE("Minimal", TEST_GROUP) {
+    std::vector<dorado::Message> messages;
+    auto add_to_vec_callback = [&messages](dorado::Message&& message) {
+        messages.push_back(std::move(message));
+    };
+
+    dorado::PipelineDescriptor pipeline_desc;
+    pipeline_desc.add_node<dorado::ReadForwarderNode>({}, 10, add_to_vec_callback);
+    auto pipeline = dorado::Pipeline::create(std::move(pipeline_desc), nullptr);
+
+    // Test that only simplex and duplex reads are passed out of ReadForwarderNode.
+    pipeline->push_message(std::make_unique<dorado::SimplexRead>());
+    pipeline->push_message(dorado::BamPtr());
+    pipeline->push_message(dorado::ReadPair());
+    pipeline->push_message(dorado::CacheFlushMessage());
+    pipeline->push_message(std::make_unique<dorado::DuplexRead>());
+    pipeline.reset();
+
+    CHECK(messages.size() == 2);
+}
