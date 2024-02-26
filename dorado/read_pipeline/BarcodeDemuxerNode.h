1b00cce11e1d95d3d0c5d1654e31595130b1198d
Steve Malton <stephen.malton@nanoporetech.com>
2024-02-26 14:14:22 +0000

    [DOR-584] Switch demuxer node to HtsFile and smart ptrs

--- dorado/read_pipeline/BarcodeDemuxerNode.h
+++ dorado/read_pipeline/BarcodeDemuxerNode.h
@@ -1,15 +1,17 @@
 #pragma once
 
 #include "read_pipeline/MessageSink.h"
+#include "utils/hts_file.h"
 #include "utils/stats.h"
-
-#include <htslib/sam.h>
+#include "utils/types.h"
 
 #include <atomic>
 #include <filesystem>
 #include <memory>
 #include <string>
 #include <unordered_map>
+
+struct bam1_t;
 
 namespace dorado {
 
@@ -25,7 +27,7 @@ public:
     ~BarcodeDemuxerNode();
     std::string get_name() const override { return "BarcodeDemuxerNode"; }
     stats::NamedStats sample_stats() const override;
-    void terminate(const FlushOptions&) override { stop_input_processing(); }
+    void terminate(const FlushOptions&) override;
     void restart() override { start_input_processing(&BarcodeDemuxerNode::input_thread_fn, this); }
 
     void set_header(const sam_hdr_t* header);
@@ -33,10 +35,10 @@ public:
 private:
     std::filesystem::path m_output_dir;
     int m_htslib_threads;
-    sam_hdr_t* m_header{nullptr};
+    SamHdrPtr m_header;
     std::atomic<int> m_processed_reads{0};
 
-    std::unordered_map<std::string, htsFile*> m_files;
+    std::unordered_map<std::string, std::unique_ptr<utils::HtsFile>> m_files;
     std::unique_ptr<std::thread> m_worker;
     void input_thread_fn();
     int write(bam1_t* record);
