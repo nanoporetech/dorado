a9b4b1b42735d8beabfe86f0ef5590ac7b3a59d8
Joyjit Daw <joyjit.daw@nanoporetech.com>
2024-03-20 16:43:40 -0400

    Skip supp/secondary alignments only if trimming
    Otherwise continue to classify them as before

--- dorado/read_pipeline/BarcodeClassifierNode.cpp
+++ dorado/read_pipeline/BarcodeClassifierNode.cpp
@@ -66,8 +66,10 @@ void BarcodeClassifierNode::input_thread_fn() {
     while (get_input_message(message)) {
         if (std::holds_alternative<BamPtr>(message)) {
             auto read = std::get<BamPtr>(std::move(message));
-            // If the read is a secondary or supplementary read, ignore it.
-            if (read->core.flag & (BAM_FSUPPLEMENTARY | BAM_FSECONDARY)) {
+            // If the read is a secondary or supplementary read, ignore it if
+            // we're setup to trim reads.
+            if (m_default_barcoding_info->trim &&
+                (read->core.flag & (BAM_FSUPPLEMENTARY | BAM_FSECONDARY))) {
                 continue;
             }
             barcode(read);
