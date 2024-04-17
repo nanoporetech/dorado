75c221ad1f558579425f41d4dd01a93ffa125ffb
Richard Harris <richard.harris@nanoporetech.com>
2024-04-17 12:06:42 +0100

    fix SDPA test

--- tests/ScaledDotProductAttention.cpp
+++ tests/ScaledDotProductAttention.cpp
@@ -2,11 +2,12 @@
 #include <c10/core/TensorOptions.h>
 #include <catch2/catch.hpp>
 #include <torch/nn.h>
-#if TORCH_VERSION_MAJOR >= 2
-#include <ATen/ops/scaled_dot_product_attention.h>
-#endif
 
 #include <vector>
+
+#if TORCH_VERSION_MAJOR >= 2
+#include <ATen/ops/scaled_dot_product_attention.h>
+#endif  // #if TORCH_VERSION_MAJOR >= 2
 
 #define TEST_TAG "[SDPA]"
 
@@ -15,8 +16,7 @@ using namespace dorado::basecall::nn;
 TEST_CASE(TEST_TAG " Test Scaled Dot Product Attention", TEST_TAG) {
 #if TORCH_VERSION_MAJOR < 2
     SECTION("SDPA tests invalid if TORCH_VERSION_MAJOR < 2") { CHECK(false); }
-#endif
-
+#else
     auto options = at::TensorOptions().dtype(torch::kFloat64).device(c10::kCUDA);
 
     SECTION("No Mask") {
@@ -50,4 +50,6 @@ TEST_CASE(TEST_TAG " Test Scaled Dot Product Attention", TEST_TAG) {
         const auto torch_res = at::scaled_dot_product_attention(qkv[0], qkv[1], qkv[2], mask);
         CHECK(at::allclose(naive_res, torch_res));
     }
+
+#endif  // #if TORCH_VERSION_MAJOR < 2
 }
