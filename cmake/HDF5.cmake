12509cfb2a05f7ee79a92f4babe24b5d8c1395b0
Ben Lawrence <ben.lawrence@nanoporetech.com>
2024-04-22 17:01:57 +0100

    [DOR-670] Simplify download_and_extract() call
    It already does that check internally.

--- cmake/HDF5.cmake
+++ cmake/HDF5.cmake
@@ -4,11 +4,7 @@ if((CMAKE_SYSTEM_NAME STREQUAL "Linux") AND (CMAKE_SYSTEM_PROCESSOR STREQUAL "aa
     # download the pacakge for arm, we want to package this due to hdf5's dependencies
     set(DYNAMIC_HDF ON)
     set(HDF_VER hdf5-1.10.0-aarch64)
-    if(EXISTS ${DORADO_3RD_PARTY_DOWNLOAD}/${HDF_VER})
-        message(STATUS "Found ${HDF_VER}")
-    else()    
-        download_and_extract(https://cdn.oxfordnanoportal.com/software/analysis/${HDF_VER}.zip ${HDF_VER})
-    endif()
+    download_and_extract(https://cdn.oxfordnanoportal.com/software/analysis/${HDF_VER}.zip ${HDF_VER})
     list(PREPEND CMAKE_PREFIX_PATH ${DORADO_3RD_PARTY_DOWNLOAD}/${HDF_VER}/${HDF_VER})
 
 elseif(WIN32)
