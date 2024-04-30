11057d78136884cd9b48af0f38d2569baddcb674
Ben Lawrence <ben.lawrence@nanoporetech.com>
2024-04-30 17:00:52 +0100

    [INSTX-5003] Search for metal kernels in a second place on iOS
    I can't find a nice way to put them where we'd usually expect them, but
    this path isn't much better either.

--- dorado/utils/metal_utils.cpp
+++ dorado/utils/metal_utils.cpp
@@ -62,19 +62,35 @@ void report_error(const NS::Error *error, const char *function) {
         result;                                            \
     })
 
-auto get_library_location() {
+auto load_kernels(MTL::Device *const device) {
     char ns_path[PATH_MAX + 1];
     uint32_t size = sizeof(ns_path);
     if (_NSGetExecutablePath(ns_path, &size) < 0) {
         throw std::runtime_error("Failed to build path to kernels");
     }
     ns_path[size] = '\0';
-
     fs::path exepth{ns_path};
-    fs::path mtllib{"../lib/default.metallib"};
-    fs::path fspath = exepth.parent_path() / mtllib;
 
-    return NS::String::string(fspath.c_str(), NS::ASCIIStringEncoding);
+    // Check in the lib folder.
+    fs::path fspath = exepth.parent_path() / "../lib/default.metallib";
+    auto lib_path = NS::String::string(fspath.c_str(), NS::ASCIIStringEncoding);
+    auto kernels = NS::TransferPtr(wrap_func_with_err(device->newLibrary, lib_path));
+    if (kernels) {
+        return kernels;
+    }
+
+#if TARGET_OS_IPHONE
+    // If we're running inside MinKNOW then the lib will be in a different place.
+    // TODO: is there a nicer way to do this without resorting to envvars?
+    fspath = exepth.parent_path() / "Frameworks/minknow_ios_framework.framework/default.metallib";
+    lib_path = NS::String::string(fspath.c_str(), NS::ASCIIStringEncoding);
+    kernels = NS::TransferPtr(wrap_func_with_err(device->newLibrary, lib_path));
+    if (kernels) {
+        return kernels;
+    }
+#endif  // TARGET_OS_IPHONE
+
+    throw std::runtime_error("Failed to load metallib library");
 }
 
 #if !TARGET_OS_IPHONE
@@ -137,15 +153,7 @@ NS::SharedPtr<MTL::ComputePipelineState> make_cps(
         const std::string &name,
         const std::vector<std::tuple<std::string, MetalConstant>> &named_constants,
         const std::optional<int> max_total_threads_per_tg) {
-    auto default_library = NS::TransferPtr(device->newDefaultLibrary());
-
-    if (!default_library) {
-        auto lib_path = get_library_location();
-        default_library = NS::TransferPtr(wrap_func_with_err(device->newLibrary, lib_path));
-        if (!default_library) {
-            throw std::runtime_error("Failed to load metallib library.");
-        }
-    }
+    auto metal_kernels = load_kernels(device);
 
     auto constant_vals = NS::TransferPtr(FunctionConstantValues::alloc()->init());
     for (auto &[cname, constant] : named_constants) {
@@ -164,7 +172,7 @@ NS::SharedPtr<MTL::ComputePipelineState> make_cps(
 
     auto kernel_name = NS::String::string(name.c_str(), NS::ASCIIStringEncoding);
     auto kernel = NS::TransferPtr(
-            wrap_func_with_err(default_library->newFunction, kernel_name, constant_vals.get()));
+            wrap_func_with_err(metal_kernels->newFunction, kernel_name, constant_vals.get()));
     if (!kernel) {
         throw std::runtime_error("Failed to find the kernel: " + name);
     }
