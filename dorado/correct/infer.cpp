62e673660624098786820621fd51f837a5bbf7a9
Joyjit Daw <joyjit.daw@nanoporetech.com>
2024-05-16 23:54:05 -0700

    clarify hardcoded model sizes

--- dorado/correct/infer.cpp
+++ dorado/correct/infer.cpp
@@ -18,8 +18,9 @@
 namespace dorado::correction {
 
 int calculate_batch_size(const std::string& device, float memory_fraction) {
-    const float model_mem = 1.f;
-    const float per_sample_mem = 0.9f;
+    // These sizes are currently hard coded for version 1 model.
+    const float model_mem = 1.f;        // GB
+    const float per_sample_mem = 0.9f;  // GB
     float usable_memory = 0.f;
     if (device == "cpu") {
 #ifdef __APPLE__
