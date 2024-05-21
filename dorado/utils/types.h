074d83949e4ca1fffe1a87d8e44f7fd17edc50ce
Steve Malton <stephen.malton@nanoporetech.com>
2024-05-21 16:15:50 +0100

    [INSTX-5255] Bring sam line parsing from ont_core into dorado

--- dorado/utils/types.h
+++ dorado/utils/types.h
@@ -16,6 +16,78 @@ struct kstring_t;
 
 namespace dorado {
 
+struct AlignmentResult {
+    std::string name;    ///< Record name, if applicable
+    std::string genome;  ///< Name of the reference genome against which the strand has been aligned
+    char direction;  ///< '+' if the alignment matches to the forward strand, '-' if alignment matches the reverse
+    int genome_start;  ///< ref[genome_start:genome_end] successfully aligned against strand[strand_start:strand_end]
+    int genome_end;  ///< ref[genome_start:genome_end] successfully aligned against strand[strand_start:strand_end]
+    int strand_start;  ///< ref[genome_start:genome_end] successfully aligned against strand[strand_start:strand_end]
+    int strand_end;  ///< ref[genome_start:genome_end] successfully aligned against strand[strand_start:strand_end]
+    int num_events;      ///< Length of input strand
+    int num_insertions;  ///< Number of positions in strand that have no counterpart in the reference
+    int num_deletions;   ///< Number of positions in genome that have no counterpart in the strand
+    int num_aligned;  ///< Equals genome_end - genome_start - num_deletions = strand_end - strand_start - num_insertions
+    int num_correct;   ///< Number of aligned positions where bases agree
+    float coverage;    ///< num_aligned / min(strand length, reference length)
+    float identity;    ///< Equals num_correct / num_aligned
+    float accuracy;    ///< Equals num_correct / (num_aligned + num_insertions + num_deletions)
+    int strand_score;  ///< Score of the alignment (scale is implementation-specific)
+    std::string sam_string;  ///< SAM file contents
+    int bed_hits;  ///< How many times this alignment intersects regions of interest in the bed file.
+    std::string bed_lines;     ///< list of lines from the bed file which this alignment intersects
+    std::string sequence;      ///< The aligned sequence.
+    std::string qstring;       ///< The quality string for the aligned sequence.
+    bool secondary_alignment;  ///< This is a secondary alignment.
+    bool supplementary_alignment;  ///< This is a supplementary alignment.
+    int mapping_quality;           ///< MAPQ score
+
+    /// Constructor.
+    AlignmentResult()
+            : name("unknown"),
+              genome("*"),
+              direction('*'),
+              genome_start(0),
+              genome_end(0),
+              strand_start(0),
+              strand_end(0),
+              num_events(-1),
+              num_insertions(-1),
+              num_deletions(-1),
+              num_aligned(-1),
+              num_correct(-1),
+              coverage(-1.f),
+              identity(-1.f),
+              accuracy(-1.f),
+              strand_score(-1),
+              sam_string("*"),
+              bed_hits(0),
+              bed_lines(""),
+              sequence(""),
+              qstring(""),
+              secondary_alignment(false),
+              supplementary_alignment(false),
+              mapping_quality(255) {}
+};
+
+struct BarcodingInfo {
+    using FilterSet = std::optional<std::unordered_set<std::string>>;
+    std::string kit_name{};
+    bool barcode_both_ends{false};
+    bool trim{false};
+    FilterSet allowed_barcodes;
+    std::optional<std::string> custom_kit = std::nullopt;
+    std::optional<std::string> custom_seqs = std::nullopt;
+};
+
+std::shared_ptr<const BarcodingInfo> create_barcoding_info(
+        const std::vector<std::string> &kit_names,
+        bool barcode_both_ends,
+        bool trim_barcode,
+        BarcodingInfo::FilterSet allowed_barcodes,
+        const std::optional<std::string> &custom_kit,
+        const std::optional<std::string> &custom_seqs);
+
 struct BarcodeScoreResult {
     int penalty = -1;
     int top_penalty = -1;
