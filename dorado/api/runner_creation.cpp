5665cf8404becfce44b5c1547ec7e6519859abbc
Kevin Dolan <Kevin.Dolan@nanoporetech.com>
2024-05-22 15:48:38 +0100

    Added methof for getting the number of batch dims.

--- dorado/api/runner_creation.cpp
+++ dorado/api/runner_creation.cpp
@@ -169,6 +169,9 @@ std::vector<modbase::RunnerPtr> create_modbase_runners(
 }
 
 #if DORADO_CUDA_BUILD
+size_t get_num_batch_dims(std::shared_ptr<basecall::CudaCaller> caller) {
+    return caller->num_batch_dims();
+}
 basecall::RunnerPtr create_basecall_runner(std::shared_ptr<basecall::CudaCaller> caller,
                                            size_t batch_dims_idx) {
     return std::make_unique<basecall::CudaModelRunner>(std::move(caller), batch_dims_idx);
