f079ca667b21589b5f10751e28aeb63ab73a9f2e
Hugh Pendry <hugh.pendry@nanoporetech.com>
2024-06-04 10:49:07 +0100

    Added validation check for results of read splitting. Added further stats for read splitting.

--- dorado/read_pipeline/ReadSplitNode.h
+++ dorado/read_pipeline/ReadSplitNode.h
@@ -3,8 +3,10 @@
 #include "read_pipeline/MessageSink.h"
 #include "utils/stats.h"
 
+#include <atomic>
 #include <memory>
 #include <string>
+#include <vector>
 
 namespace dorado {
 
@@ -26,7 +28,24 @@ public:
 private:
     void input_thread_fn();  // Worker thread performs splitting asynchronously.
 
+    void update_read_counters(std::size_t num_split_reads);
+
     std::unique_ptr<const splitter::ReadSplitter> m_splitter;
+
+#ifdef _MSC_VER
+    // Disable warning C4324: 'dorado::ReadSplitNode': structure was padded due to alignment specifier
+    // as we do not care about the increased size of the ReadSplitNode.
+#pragma warning(push)
+#pragma warning(disable : 4324)
+#endif
+    alignas(std::hardware_destructive_interference_size) std::atomic_size_t
+            m_num_input_reads_pushed{};
+    alignas(std::hardware_destructive_interference_size) std::atomic_size_t m_num_reads_split{};
+    alignas(std::hardware_destructive_interference_size) std::atomic_size_t
+            m_total_num_reads_pushed{};
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
 };
 
 }  // namespace dorado
