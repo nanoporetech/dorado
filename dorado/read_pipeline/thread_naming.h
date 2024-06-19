d9f4c4aaad474fa69885deea0b5b760356471c8c
Hugh Pendry <hugh.pendry@nanoporetech.com>
2024-06-19 15:05:21 +0100

    Added comment for set_thread_name

--- dorado/read_pipeline/thread_naming.h
+++ dorado/read_pipeline/thread_naming.h
@@ -2,9 +2,13 @@
 
 namespace dorado {
 
+// Declaration of the thread naming interface.
 class ThreadNaming {
 public:
     virtual ~ThreadNaming() = default;
+
+    // Call to set the name of the current thread.
+    // N.B. the name will be truncated to 15 characters on some platforms.
     virtual void set_thread_name(const std::string& name) = 0;
 };
 
