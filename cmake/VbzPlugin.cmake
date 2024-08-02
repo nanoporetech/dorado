23f94532adc72093b33c2e7866d5de31bfa4c79d
Ben Lawrence <ben.lawrence@nanoporetech.com>
2024-08-02 11:36:05 +0100

    [DOR-547] Fix the GCC8 build

--- cmake/VbzPlugin.cmake
+++ cmake/VbzPlugin.cmake
@@ -25,12 +25,32 @@ function(add_hdf_vbz_plugin)
         set(CONAN_LIB_DIRS_DEBUG ${ZSTD_BUILD_DIR}/lib/Debug)
 
         install(FILES ${ZSTD_BUILD_DIR}/lib/Release/zstd.dll DESTINATION bin)
+
+    elseif (CMAKE_SYSTEM_PROCESSOR MATCHES "^aarch64*|^arm*" AND CMAKE_CXX_COMPILER_ID MATCHES "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS 9.0)
+        # The GCC8 CI build needs a newer version of zstd than provided by the docker image.
+        set(ZSTD_BUILD_DIR ${CMAKE_BINARY_DIR}/cmake-build-zstd)
+        set(ZSTD_INSTALL_DIR ${CMAKE_BINARY_DIR}/cmake-install-zstd)
+        execute_process(COMMAND
+            cmake
+                -S ${DORADO_3RD_PARTY_SOURCE}/zstd/build/cmake
+                -B ${ZSTD_BUILD_DIR}
+                -D CMAKE_INSTALL_PREFIX=${ZSTD_INSTALL_DIR}
+        )
+        execute_process(COMMAND cmake --build ${ZSTD_BUILD_DIR} --target install)
+
+        list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/dorado/3rdparty/hdf_plugins/cmake")
+        # Findzstd.cmake uses conan variables to determine where things are.
+        set(CONAN_INCLUDE_DIRS_RELEASE ${ZSTD_INSTALL_DIR}/include)
+        set(CONAN_LIB_DIRS_RELEASE ${ZSTD_INSTALL_DIR}/lib)
+        find_package(zstd 1.3.6 REQUIRED)
+
     else()
         # Some platforms need the Findzstd.cmake from hdf_plugins, but append it in case we already
         # have one in the path.
         list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/dorado/3rdparty/hdf_plugins/cmake")
         # hdf_plugins looks for 1.3.1 but the libarrow in POD5 requires 1.3.6 minimum.
         find_package(zstd 1.3.6 REQUIRED)
+
     endif()
 
     add_subdirectory(dorado/3rdparty/hdf_plugins EXCLUDE_FROM_ALL)
