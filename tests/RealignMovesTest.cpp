90486de3493a1c0cf66960f6802d3d31294de4a3
Mike Vella <Mike.Vella@nanoporetech.com>
2024-08-08 23:39:41 +0200

    Fixes and improvements to move table realignment algorithm

--- tests/RealignMovesTest.cpp
+++ tests/RealignMovesTest.cpp
@@ -8,7 +8,7 @@ using namespace dorado;
 
 #define TEST_GROUP "[utils][realign_moves]"
 
-/*TEST_CASE("Realign Moves No Error", TEST_GROUP) {
+TEST_CASE("Realign Moves No Error", TEST_GROUP) {
     std::string query_sequence = "ACGTACGTACGTACGTACGTACGTACGTACGT";   // Example query sequence
     std::string target_sequence = "ACGTACGTACGTACGTACGTACGTACGTACGT";  // Example target sequence
     std::vector<uint8_t> moves = {
@@ -38,10 +38,10 @@ TEST_CASE("No alignment doesn't produce an error", TEST_GROUP) {
     CHECK(move_offset == -1);
     CHECK(target_start == -1);
     CHECK(new_moves.empty());
-}*/
+}
 
 TEST_CASE("Test realign_moves - output moves correct", TEST_GROUP) {
-    /*    SECTION("Test move table realignemnt of long identical sequences") {
+    SECTION("Test move table realignemnt of long identical sequences") {
         auto query =
                 std::string("ACGACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTT");
         auto target =
@@ -62,7 +62,7 @@ TEST_CASE("Test realign_moves - output moves correct", TEST_GROUP) {
         CHECK(query_start == 0);
         CHECK(new_moves.size() == input_moves.size());
         CHECK(new_moves == input_moves);
-    }*/
+    }
 
     SECTION("Test move table realignemnt where target is suffix of query") {
         auto query = std::string(
@@ -84,6 +84,30 @@ TEST_CASE("Test realign_moves - output moves correct", TEST_GROUP) {
         CHECK(old_moves_offset == 8);
         CHECK(target_start == 0);
         CHECK(new_moves.size() == input_moves.size() - 4 * 2);
-        CHECK(std::equal(new_moves.begin(), new_moves.end(), input_moves.begin() + 4));
+        CHECK(std::equal(new_moves.begin(), new_moves.end(), input_moves.begin() + 4 * 2));
+    }
+
+    SECTION("Test 2  -  move table realignemnt where target is suffix of query") {
+        auto query = std::string(
+                "TTTTACGACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTT");
+        auto target =
+                std::string("ACGACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTACGTT");
+
+        std::vector<uint8_t> input_moves;
+
+        for (char nucleotide : query) {
+            (void)nucleotide;
+            input_moves.push_back(1);
+            input_moves.push_back(0);
+            input_moves.push_back(0);
+        }
+
+        auto [old_moves_offset, target_start, new_moves] =
+                utils::realign_moves(query, target, input_moves);  // simplex, duplex, moves
+
+        CHECK(old_moves_offset == 4 * 3);
+        CHECK(target_start == 0);
+        CHECK(new_moves.size() == input_moves.size() - 4 * 3);
+        CHECK(std::equal(new_moves.begin(), new_moves.end(), input_moves.begin() + 4 * 3));
     }
 }
