19fff31036ed0263426aefb94578b0746e5a905c
Ben Lawrence <ben.lawrence@nanoporetech.com>
2024-10-11 14:59:43 +0000

    [INSTX-6935] Merge branch 'blawrence/cuda-benchmark-corruption-fix' into 'master'
    Guard access to m_chunk_benchmarks
    
    See merge request machine-learning/dorado!1229

--- dorado/basecall/CudaChunkBenchmarks.h
+++ dorado/basecall/CudaChunkBenchmarks.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <map>
+#include <mutex>
 #include <optional>
 #include <string>
 #include <tuple>
@@ -16,7 +17,14 @@ private:
     using ModelName = std::string;
     using GPUName = std::string;
     using ChunkSize = int;
+
+    mutable std::mutex m_chunk_benchmarks_mutex;
     std::map<std::tuple<GPUName, ModelName, ChunkSize>, ChunkTimings> m_chunk_benchmarks;
+
+    // Must be called with m_chunk_benchmarks_mutex already locked.
+    std::optional<const ChunkTimings> get_chunk_timings_internal(const GPUName& gpu_name,
+                                                                 const std::string& model_path,
+                                                                 ChunkSize chunk_size) const;
 
 public:
     static CudaChunkBenchmarks& instance() {
@@ -24,7 +32,7 @@ public:
         return chunk_benchmarks;
     }
 
-    std::optional<const ChunkTimings> get_chunk_timings(GPUName gpu_name,
+    std::optional<const ChunkTimings> get_chunk_timings(const GPUName& gpu_name,
                                                         const std::string& model_path,
                                                         ChunkSize chunk_size) const;
 
