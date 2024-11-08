77c318b7e11561e27fde1bff8211acdf7dbc542f
John Stone <john.stone@nanoporetech.com>
2024-11-08 11:27:19 +0100

    Added two helpers in dorado/polish/sample.h.

--- dorado/polish/sample.h
+++ dorado/polish/sample.h
@@ -4,6 +4,7 @@
 
 #include <cstdint>
 #include <string>
+#include <tuple>
 #include <vector>
 
 namespace dorado::polisher {
@@ -18,6 +19,15 @@ struct Sample {
     int64_t start() const { return (std::empty(positions_major) ? -1 : (positions_major.front())); }
     int64_t end() const {
         return (std::empty(positions_major) ? -1 : (positions_major.back() + 1));
+    }
+    std::pair<int64_t, int64_t> get_position(const int64_t idx) const {
+        if ((idx < 0) || (idx >= static_cast<int64_t>(std::size(positions_major)))) {
+            return {-1, -1};
+        }
+        return {positions_major[idx], positions_minor[idx]};
+    }
+    std::pair<int64_t, int64_t> get_last_position() const {
+        return get_position(static_cast<int64_t>(std::size(positions_major)) - 1);
     }
 };
 
