131c5e1c2252f31deb8f0a8305c5086ed85c30f5
Ben Lawrence <ben.lawrence@nanoporetech.com>
2024-11-13 10:42:25 +0000

    [DOR-895] Simplify some code and remove some logging

--- tests/AlignerTest.cpp
+++ tests/AlignerTest.cpp
@@ -110,16 +110,14 @@ protected:
         create_pipeline(index_file_access, bed_file_access, thread_pool,
                         dorado::utils::concurrency::TaskPriority::normal);
 
-        dorado::ReadCommon read_common{};
         auto client_info = std::make_shared<dorado::DefaultClientInfo>();
         client_info->contexts().register_context<const dorado::alignment::AlignmentInfo>(
                 client_align_info);
-        read_common.client_info = client_info;
-        read_common.read_id = std::move(read_id);
-        read_common.seq = std::move(sequence);
 
         auto read = std::make_unique<MessageType>();
-        read->read_common = std::move(read_common);
+        read->read_common.client_info = std::move(client_info);
+        read->read_common.read_id = std::move(read_id);
+        read->read_common.seq = std::move(sequence);
 
         pipeline->push_message(std::move(read));
         pipeline->terminate({});
@@ -560,17 +558,15 @@ TEST_CASE_METHOD(AlignerNodeTestFixture,
             std::move(simplex_read->read_common.alignment_results[0].sam_string);
 
     // Do the comparison checks
-    CHECK_FALSE(sam_line_from_read_common.empty());
-
-    if (sam_line_from_read_common.at(sam_line_from_read_common.size() - 1) == '\n') {
-        sam_line_from_read_common =
-                sam_line_from_read_common.substr(0, sam_line_from_read_common.size() - 1);
+    REQUIRE_FALSE(sam_line_from_read_common.empty());
+    if (sam_line_from_read_common.back() == '\n') {
+        sam_line_from_read_common.resize(sam_line_from_read_common.size() - 1);
     }
 
     const auto bam_fields = dorado::utils::split(sam_line_from_bam_ptr, '\t');
     const auto read_common_fields = dorado::utils::split(sam_line_from_read_common, '\t');
-    CHECK(bam_fields.size() == read_common_fields.size());
-    CHECK(bam_fields.size() >= 11);
+    REQUIRE(bam_fields.size() == read_common_fields.size());
+    REQUIRE(bam_fields.size() >= 11);
     // first 11 mandatory fields should be identical
     for (std::size_t field_index{0}; field_index < 11; ++field_index) {
         CAPTURE(field_index);
