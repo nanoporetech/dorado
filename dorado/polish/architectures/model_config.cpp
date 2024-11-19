5a9e54d42ef8bbb4418376a8109198dae8d3eb34
John Stone <john.stone@nanoporetech.com>
2024-11-19 15:12:33 +0100

    WIP. Modified ModelConfig to match the new model config definition. This will break earlier tests if they use the old config.

--- dorado/polish/architectures/model_config.cpp
+++ dorado/polish/architectures/model_config.cpp
@@ -1,9 +1,5 @@
 #include "model_config.h"
 
-#include "utils/memory_utils.h"
-#include "utils/string_utils.h"
-
-#include <spdlog/spdlog.h>
 #include <toml.hpp>
 #include <toml/value.hpp>
 
@@ -11,74 +7,106 @@
 
 namespace dorado::polisher {
 
-// void print_toml(const toml::value& val, int indent = 0) {
-//     // Add indentation based on the level
-//     std::string indent_str(indent, ' ');
+inline void print_toml(const toml::value& val, int indent = 0) {
+    const std::string indent_str(indent, ' ');
 
-//     if (val.is_table()) {
-//         const auto& table = val.as_table();
-//         for (const auto& [key, value] : table) {
-//             std::cout << indent_str << key << " = ";
-//             if (value.is_table() || value.is_array()) {
-//                 std::cout << "\n";
-//             }
-//             print_toml(value, indent + 2);  // Increase indentation for nested tables/arrays
-//         }
-//     } else if (val.is_array()) {
-//         const auto& arr = val.as_array();
-//         std::cout << "[";
-//         for (size_t i = 0; i < arr.size(); ++i) {
-//             print_toml(arr[i], 0);
-//             if (i < arr.size() - 1) {
-//                 std::cout << ", ";
-//             }
-//         }
-//         std::cout << "]\n";
-//     } else {
-//         // For primitive types (strings, ints, etc.)
-//         if (val.is_string()) {
-//             std::cout << "\"" << val.as_string() << "\"\n";
-//         } else if (val.is_integer()) {
-//             std::cout << val.as_integer() << "\n";
-//         } else if (val.is_floating()) {
-//             std::cout << val.as_floating() << "\n";
-//         } else if (val.is_boolean()) {
-//             std::cout << (val.as_boolean() ? "true" : "false") << "\n";
-//             // } else if (val.is_none()) {
-//             //     std::cout << "null\n";
-//         }
-//     }
-// }
+    if (val.is_table()) {
+        const auto& table = val.as_table();
+        for (const auto& [key, value] : table) {
+            std::cout << indent_str << key << " = ";
+            if (value.is_table() || value.is_array()) {
+                std::cout << "\n";
+            }
+            print_toml(value, indent + 2);
+        }
+    } else if (val.is_array()) {
+        const auto& arr = val.as_array();
+        std::cout << "[";
+        for (size_t i = 0; i < arr.size(); ++i) {
+            print_toml(arr[i], 0);
+            if (i < arr.size() - 1) {
+                std::cout << ", ";
+            }
+        }
+        std::cout << "]\n";
+    } else {
+        // For POD types.
+        if (val.is_string()) {
+            std::cout << "\"" << val.as_string() << "\"\n";
+        } else if (val.is_integer()) {
+            std::cout << val.as_integer() << "\n";
+        } else if (val.is_floating()) {
+            std::cout << val.as_floating() << "\n";
+        } else if (val.is_boolean()) {
+            std::cout << (val.as_boolean() ? "true" : "false") << "\n";
+        } else {
+            std::cout << "(unknown type)\n";
+        }
+    }
+}
+
+std::unordered_map<std::string, std::string> parse_kwargs(const toml::value& table) {
+    std::unordered_map<std::string, std::string> kwargs;
+
+    if (!table.is_table()) {
+        throw std::invalid_argument("Expected a TOML table.");
+    }
+
+    for (const auto& [key, value] : table.as_table()) {
+        kwargs[key] = toml::format(value);
+    }
+
+    return kwargs;
+}
 
 ModelConfig parse_model_config(const std::filesystem::path& config_path) {
     const toml::value config_toml = toml::parse(config_path.string());
 
     if (!config_toml.contains("model")) {
-        throw std::runtime_error("Model config must include [model] section");
+        throw std::runtime_error("Model config must include the [model] section.");
     }
     if (!config_toml.contains("feature_encoder")) {
-        throw std::runtime_error("Model config must include [feature_encoder] section");
+        throw std::runtime_error("Model config must include the [feature_encoder] section.");
+    }
+    if (!config_toml.contains("config_version")) {
+        throw std::runtime_error("Model config must contain 'config_version' attribute.");
     }
 
     // print_toml(config_toml);
 
     ModelConfig cfg;
 
-    const auto& model = toml::find(config_toml, "model");
-    cfg.version = toml::find<int>(model, "version");
-    cfg.model_type = toml::find<std::string>(model, "type");
-    cfg.model_file = toml::find<std::string>(model, "model_file");
-    cfg.model_dir = config_path.parent_path().string();
+    // Parse the config version.
+    { cfg.version = toml::find<int>(config_toml, "config_version"); }
 
-    const auto& feature_encoder = toml::find(config_toml, "feature_encoder");
-    cfg.feature_encoder_type = toml::find<std::string>(feature_encoder, "type");
+    // Parse the model info.
+    {
+        const auto& section = toml::find(config_toml, "model");
+        cfg.model_type = toml::find<std::string>(section, "type");
+        // cfg.model_file = toml::find<std::string>(section, "model_file");
+        cfg.model_file = "model.pt";
+        cfg.model_dir = config_path.parent_path().string();
 
-    // cfg.feature_vector_length = toml::find<int32_t>(feature_encoder, "feature_vector_length");
-    // cfg.feature_encoder_normalise = toml::find<std::string>(feature_encoder, "normalise");
-    // cfg.feature_encoder_tag_keep_missing = toml::find<bool>(feature_encoder, "tag_keep_missing");
+        // Parse kwargs for the model.
+        const auto& model_kwargs = toml::find(section, "kwargs");
+        cfg.model_kwargs = parse_kwargs(model_kwargs);
+    }
 
-    // dtypes
-    // feature_indices
+    // Parse the feature encoder info.
+    {
+        const auto& section = toml::find(config_toml, "feature_encoder");
+        cfg.feature_encoder_type = toml::find<std::string>(section, "type");
+
+        // Parse kwargs for the feature extractor.
+        const auto& model_kwargs = toml::find(section, "kwargs");
+        cfg.feature_encoder_kwargs = parse_kwargs(model_kwargs);
+    }
+
+    // Parse the label scheme section.
+    {
+        const auto& section = toml::find(config_toml, "label_scheme");
+        cfg.label_scheme = toml::find<std::string>(section, "type");
+    }
 
     return cfg;
 }
--- dorado/polish/architectures/model_config.h
+++ dorado/polish/architectures/model_config.h
@@ -3,29 +3,27 @@
 #include <cstdint>
 #include <filesystem>
 #include <string>
-
-#ifdef NDEBUG
-#define LOG_TRACE(...)
-#else
-#define LOG_TRACE(...) spdlog::trace(__VA_ARGS__)
-#endif
+#include <unordered_map>
 
 namespace dorado::polisher {
 
 struct ModelConfig {
     int32_t version = 0;
+
+    // Model section.
     std::string model_type;
     std::string model_file;
     std::string model_dir;
+    std::unordered_map<std::string, std::string> model_kwargs;
 
+    // Feature encoder section.
     std::string feature_encoder_type;
-    // int32_t feature_vector_length = 0;
-    // std::string feature_encoder_normalise;
-    // bool feature_encoder_tag_keep_missing = false;
+    std::unordered_map<std::string, std::string> feature_encoder_kwargs;
+
+    // Label scheme section.
+    std::string label_scheme;
 };
 
 ModelConfig parse_model_config(const std::filesystem::path& config_path);
-
-std::string resolve_model_name(const std::string& model_name);
 
 }  // namespace dorado::polisher
