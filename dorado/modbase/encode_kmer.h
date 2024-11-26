613413d5fc77edb7c8b328eec28855d191f42f7c
Richard Harris <richard.harris@nanoporetech.com>
2024-11-26 16:19:34 +0000

    remove [[maybe_unused]] from chunk kmer encoding

--- dorado/modbase/encode_kmer.h
+++ dorado/modbase/encode_kmer.h
@@ -10,12 +10,11 @@ std::vector<int8_t> encode_kmer_context(const std::vector<int>& seq,
                                         size_t bases_after,
                                         size_t context_samples);
 
-// FIXME -- unused until DOR-849
-[[maybe_unused]] std::vector<int8_t> encode_kmer_chunk(const std::vector<int>& seq,
-                                                       const std::vector<uint64_t>& seq_mappings,
-                                                       size_t kmer_len,
-                                                       size_t context_samples,
-                                                       size_t padding_samples,
-                                                       bool kmer_centered);
+std::vector<int8_t> encode_kmer_chunk(const std::vector<int>& seq,
+                                      const std::vector<uint64_t>& seq_mappings,
+                                      size_t kmer_len,
+                                      size_t context_samples,
+                                      size_t padding_samples,
+                                      bool kmer_centered);
 
 }  // namespace dorado::modbase
