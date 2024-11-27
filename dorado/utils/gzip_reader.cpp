683cb4351fec3584d13f48d58f11aca86476bfbd
Hugh Pendry <hugh.pendry@nanoporetech.com>
2024-11-27 09:30:05 +0000

    Fix replace free with delete to match new

--- dorado/utils/gzip_reader.cpp
+++ dorado/utils/gzip_reader.cpp
@@ -9,7 +9,7 @@ void ZstreamDestructor::operator()(z_stream* zlib_stream) {
         return;
     }
     inflateEnd(zlib_stream);
-    free(zlib_stream);
+    delete zlib_stream;
 }
 
 GzipReader::GzipReader(std::string gzip_file, std::size_t buffer_size)
@@ -25,11 +25,37 @@ GzipReader::GzipReader(std::string gzip_file, std::size_t buffer_size)
     m_zlib_stream = create_zlib_stream();
 }
 
+void GzipReader::set_failure(std::string error_message) {
+    m_is_valid = false;
+    m_error_message = std::move(error_message) + " [" + m_gzip_file + "]";
+}
+
 bool GzipReader::is_valid() const { return m_is_valid; }
 
 const std::string& GzipReader::error_message() const { return m_error_message; }
 
-std::size_t GzipReader::num_bytes_read() const { return m_num_bytes_read; }
+ZstreamPtr GzipReader::create_zlib_stream() {
+    ZstreamPtr zlib_stream(new z_stream());
+    if (!try_initialise_zlib_stream(*zlib_stream)) {
+        return {};
+    }
+    return zlib_stream;
+}
+
+bool GzipReader::try_initialise_zlib_stream(z_stream& zlib_stream) {
+    zlib_stream.zalloc = Z_NULL;
+    zlib_stream.zfree = Z_NULL;
+    zlib_stream.opaque = Z_NULL;
+    zlib_stream.avail_in = 0;
+    zlib_stream.next_in = Z_NULL;
+    auto init_result = inflateInit2(&zlib_stream, MAX_WBITS + 16);  // +16 for GZip
+    if (init_result != Z_OK) {
+        set_failure("Could not initialize stream for inflating. Error: " +
+                    std::to_string(init_result));
+        return false;
+    }
+    return true;
+}
 
 bool GzipReader::read_next() {
     if (!try_prepare_for_next_decompress()) {
@@ -53,35 +79,21 @@ bool GzipReader::read_next() {
     return true;
 }
 
-std::vector<char>& GzipReader::decompressed_buffer() { return m_decompressed_buffer; }
-
-void GzipReader::set_failure(std::string error_message) {
-    m_is_valid = false;
-    m_error_message = std::move(error_message) + " [" + m_gzip_file + "]";
-}
-
-bool GzipReader::try_initialise_zlib_stream(z_stream& zlib_stream) {
-    zlib_stream.zalloc = Z_NULL;
-    zlib_stream.zfree = Z_NULL;
-    zlib_stream.opaque = Z_NULL;
-    zlib_stream.avail_in = 0;
-    zlib_stream.next_in = Z_NULL;
-    auto init_result = inflateInit2(&zlib_stream, MAX_WBITS + 16);  // +16 for GZip
-    if (init_result != Z_OK) {
-        set_failure("Could not initialize stream for inflating. Error: " +
-                    std::to_string(init_result));
+bool GzipReader::try_prepare_for_next_decompress() {
+    if (!is_valid()) {
         return false;
     }
+    if (m_zlib_stream->avail_in == 0 && !fetch_next_compressed_chunk()) {
+        return false;
+    }
+    m_zlib_stream->avail_out = static_cast<uint32_t>(m_buffer_size);
+    m_zlib_stream->next_out = reinterpret_cast<unsigned char*>(m_decompressed_buffer.data());
     return true;
 }
 
-ZstreamPtr GzipReader::create_zlib_stream() {
-    ZstreamPtr zlib_stream(new z_stream());
-    if (!try_initialise_zlib_stream(*zlib_stream)) {
-        return {};
-    }
-    return zlib_stream;
-}
+std::size_t GzipReader::num_bytes_read() const { return m_num_bytes_read; }
+
+std::vector<char>& GzipReader::decompressed_buffer() { return m_decompressed_buffer; }
 
 ZstreamPtr GzipReader::create_next_zlib_stream(const z_stream& last_zlib_stream) {
     auto next_zlib_stream = create_zlib_stream();
@@ -104,18 +116,6 @@ bool GzipReader::fetch_next_compressed_chunk() {
     m_compressed_stream.read(m_compressed_buffer.data(), m_compressed_buffer.size());
     m_zlib_stream->avail_in = static_cast<uint32_t>(m_compressed_stream.gcount());
     m_zlib_stream->next_in = reinterpret_cast<unsigned char*>(m_compressed_buffer.data());
-    return true;
-}
-
-bool GzipReader::try_prepare_for_next_decompress() {
-    if (!is_valid()) {
-        return false;
-    }
-    if (m_zlib_stream->avail_in == 0 && !fetch_next_compressed_chunk()) {
-        return false;
-    }
-    m_zlib_stream->avail_out = static_cast<uint32_t>(m_buffer_size);
-    m_zlib_stream->next_out = reinterpret_cast<unsigned char*>(m_decompressed_buffer.data());
     return true;
 }
 
