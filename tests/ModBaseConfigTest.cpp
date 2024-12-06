800276189d8e8c992d62f756f683e5ddc83decbb
Richard Harris <richard.harris@nanoporetech.com>
2024-12-06 14:20:47 +0000

    add modbase config file examples and extend tests

--- tests/ModBaseConfigTest.cpp
+++ tests/ModBaseConfigTest.cpp
@@ -19,6 +19,9 @@ const auto _5mCG_5hmCG =
         get_data_dir("model_configs/dna_r10.4.1_e8.2_260bps_sup@v4.0.0_5mCG_5hmCG@v2");
 const auto _pseU = get_data_dir("model_configs/rna004_130bps_sup@v5.0.0_pseU@v1");
 const auto _pseU_j = get_data_dir("model_configs/rna004_130bps_sup@v5.0.1_pseU@v1");
+const auto _5mCG_5hmCG_v3 =
+        get_data_dir("model_configs/dna_r10.4.1_e8.2_400bps_hac@v5.0.0_5mCG_5hmCG@v3");
+const auto _6mA_v3 = get_data_dir("model_configs/dna_r10.4.1_e8.2_400bps_sup@v5.0.0_6mA@v3");
 TEST_CASE(TEST_GROUP ": modbase model parser", TEST_GROUP) {
     using Gen = ModelGeneralParams;
     using Mod = ModificationParams;
@@ -28,38 +31,50 @@ TEST_CASE(TEST_GROUP ": modbase model parser", TEST_GROUP) {
     // For _5mCG_5hmCG kmer center index 6 to pass tests need: Log(X)/Log(4) > 7
     // -> Log(X) > 7*1.387 -> X := Exp(9.71) = 16482
     std::vector<float> refined_kmer_levels(16482, 0);
-    constexpr int stride = 3;
-    constexpr int encoding_stride = 1;
     auto [path, general, mods, context, refine] =
             // clang-format off
             GENERATE_COPY(table<fs::path, Gen, Mod, Ctx, Rmt>({
             std::make_tuple(
                 _5mCG, 
-                Gen{ModelType::CONV_V1, 64, 9, 2, stride, stride, encoding_stride}, 
+                Gen{ModelType::CONV_V1, 64, 9, 2, 3}, 
                 Mod{{"m"}, {"5mC"}, "CG", 0},
                 Ctx{50, 50, 100, 4, 4, false, false}, 
                 Rmt{}
             ),
             std::make_tuple(
                 _5mCG_5hmCG, 
-                Gen{ModelType::CONV_LSTM_V1, 256, 9, 3, stride, stride, encoding_stride},
+                Gen{ModelType::CONV_LSTM_V1, 256, 9, 3, 3},
                 Mod{{"h", "m"}, {"5hmC", "5mC"}, "CG", 0,},                
                 Ctx{50, 50, 100, 4, 4, false, false}, 
                 Rmt{7, 6, refined_kmer_levels}
             ),
             std::make_tuple(
                 _pseU,
-                Gen{ModelType::CONV_LSTM_V1, 128, 9, 2, stride, stride, encoding_stride},
+                Gen{ModelType::CONV_LSTM_V1, 128, 9, 2, 3},
                 Mod{{"17802"}, {"pseU"}, "T", 0},            
                 Ctx{150, 150, 300, 4, 4, true, false},
                 Rmt{7, 3, refined_kmer_levels}
             ),
             std::make_tuple(
                 _pseU_j,
-                Gen{ModelType::CONV_LSTM_V1, 128, 9, 2, stride, stride, encoding_stride},
+                Gen{ModelType::CONV_LSTM_V1, 128, 9, 2, 3},
                 Mod{{"17802"}, {"pseU"}, "T", 0},            
                 Ctx{150, 150, 300, 4, 4, true, true},
                 Rmt{7, 3, refined_kmer_levels}
+            ),
+            std::make_tuple(
+                _5mCG_5hmCG_v3,
+                Gen{ModelType::CONV_LSTM_V2, 256, 9, 3, 6},
+                Mod{{"h", "m"}, {"5hmC", "5mC"}, "CG", 0},            
+                Ctx{96, 96, 192, 4, 4, false, true},
+                Rmt{7, 6, refined_kmer_levels}
+            ),
+            std::make_tuple(
+                _6mA_v3,
+                Gen{ModelType::CONV_LSTM_V2, 256, 9, 2, 3},
+                Mod{{"a"}, {"6mA"}, "A", 0},            
+                Ctx{150, 150, 600, 4, 4, false, true},
+                Rmt{7, 6, refined_kmer_levels}
             ),
             }));
     // clang-format on
@@ -96,6 +111,7 @@ TEST_CASE(TEST_GROUP ": modbase model parser", TEST_GROUP) {
         CHECK(c.kmer_len == context.kmer_len);
         CHECK(c.reverse == context.reverse);
         CHECK(c.base_start_justify == context.base_start_justify);
+        CHECK(c.chunk_size == context.chunk_size);
     }
 
     SECTION("refinement parameters") {
