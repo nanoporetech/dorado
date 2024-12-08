2f81d1f5c676b66ad0f344b6f54e0974e7bba035
John Stone <john.stone@nanoporetech.com>
2024-12-08 21:00:56 +0100

    Added BamFile::parse_header() to bam_file.

--- dorado/polish/bam_file.cpp
+++ dorado/polish/bam_file.cpp
@@ -1,6 +1,11 @@
 #include "bam_file.h"
 
-BamFile::BamFile(const std::filesystem::path &in_fn)
+#include <spdlog/spdlog.h>
+
+#include <ostream>
+#include <sstream>
+
+BamFile::BamFile(const std::filesystem::path& in_fn)
         : m_fp{hts_open(in_fn.string().c_str(), "rb"), hts_close},
           m_idx{nullptr, hts_idx_destroy},
           m_hdr{nullptr, sam_hdr_destroy} {
@@ -21,4 +26,75 @@ BamFile::BamFile(const std::filesystem::path &in_fn)
     if (!m_hdr) {
         throw std::runtime_error{"Could not load header from BAM file: '" + in_fn.string() + "'!"};
     }
+}
+
+std::vector<HeaderLineData> BamFile::parse_header() const {
+    const char* hdr_text = sam_hdr_str(m_hdr.get());
+    if (!hdr_text) {
+        spdlog::warn("Could not retrieve BAM header text!");
+        return {};
+    }
+
+    std::vector<HeaderLineData> ret;
+    std::istringstream hdr_stream(hdr_text);
+    std::string line;
+
+    while (std::getline(hdr_stream, line)) {
+        if (std::size(line) < 3) {
+            // Header tag cannot fit in less than 3 chars.
+            continue;
+        }
+        if (line[0] != '@') {
+            // Non-header line found.
+            break;
+        }
+
+        // Split line by tabs.
+        std::vector<std::string> fields;
+        std::string field;
+        std::istringstream line_stream(line);
+        while (std::getline(line_stream, field, '\t')) {
+            fields.emplace_back(std::move(field));
+        }
+
+        // Malformed lines.
+        if (std::empty(fields)) {
+            continue;
+        }
+
+        // Split tags by colon.
+        std::vector<std::pair<std::string, std::string>> key_value_pairs;
+        for (size_t i = 1; i < std::size(fields); ++i) {
+            const auto pos = fields[i].find(':');
+            if (pos != std::string::npos) {
+                std::string key = fields[i].substr(0, pos);
+                std::string value = fields[i].substr(pos + 1);
+                key_value_pairs.emplace_back(std::move(key), std::move(value));
+            } else {
+                key_value_pairs.emplace_back(fields[i], std::string());
+            }
+        }
+
+        // Store in the map.
+        const std::string& header_type = fields.front();
+        ret.emplace_back(HeaderLineData{header_type, std::move(key_value_pairs)});
+    }
+
+    return ret;
+}
+
+void header_to_stream(std::ostream& os, const std::vector<HeaderLineData>& header) {
+    for (const auto& line : header) {
+        os << line.header_type;
+        for (const auto& [key, value] : line.tags) {
+            os << '\t' << key << ":" << value;
+        }
+        os << '\n';
+    }
+}
+
+std::string header_to_string(const std::vector<HeaderLineData>& header) {
+    std::ostringstream oss;
+    header_to_stream(oss, header);
+    return oss.str();
 }
--- dorado/polish/bam_file.h
+++ dorado/polish/bam_file.h
@@ -3,7 +3,15 @@
 #include <htslib/sam.h>
 
 #include <filesystem>
+#include <iosfwd>
 #include <memory>
+#include <tuple>
+#include <vector>
+
+struct HeaderLineData {
+    std::string header_type;
+    std::vector<std::pair<std::string, std::string>> tags;
+};
 
 class BamFile {
 public:
@@ -18,8 +26,14 @@ public:
     hts_idx_t* idx() { return m_idx.get(); }
     sam_hdr_t* hdr() { return m_hdr.get(); }
 
+    std::vector<HeaderLineData> parse_header() const;
+
 private:
     std::unique_ptr<htsFile, decltype(&hts_close)> m_fp;
     std::unique_ptr<hts_idx_t, decltype(&hts_idx_destroy)> m_idx;
     std::unique_ptr<sam_hdr_t, decltype(&sam_hdr_destroy)> m_hdr;
 };
+
+void header_to_stream(std::ostream& os, const std::vector<HeaderLineData>& header);
+
+std::string header_to_string(const std::vector<HeaderLineData>& header);
