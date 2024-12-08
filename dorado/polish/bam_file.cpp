2c894c4a8919e99f824820a728d17b5ca473773f
John Stone <john.stone@nanoporetech.com>
2024-12-08 22:31:30 +0100

    Updated the constructor of dorado/polish/bam_file.cpp.

--- dorado/polish/bam_file.cpp
+++ dorado/polish/bam_file.cpp
@@ -7,22 +7,16 @@
 
 BamFile::BamFile(const std::filesystem::path& in_fn)
         : m_fp{hts_open(in_fn.string().c_str(), "rb"), hts_close},
-          m_idx{nullptr, hts_idx_destroy},
-          m_hdr{nullptr, sam_hdr_destroy},
+          m_idx{sam_index_load(m_fp.get(), in_fn.string().c_str()), hts_idx_destroy},
+          m_hdr{sam_hdr_read(m_fp.get()), sam_hdr_destroy},
           m_current_record{bam_init1(), bam_destroy1} {
     if (!m_fp) {
         throw std::runtime_error{"Could not open BAM file: '" + in_fn.string() + "'!"};
     }
 
-    m_idx = std::unique_ptr<hts_idx_t, decltype(&hts_idx_destroy)>(
-            sam_index_load(m_fp.get(), in_fn.string().c_str()), hts_idx_destroy);
-
     if (!m_idx) {
         throw std::runtime_error{"Could not open index for BAM file: '" + in_fn.string() + "'!"};
     }
-
-    m_hdr = std::unique_ptr<sam_hdr_t, decltype(&sam_hdr_destroy)>(sam_hdr_read(m_fp.get()),
-                                                                   sam_hdr_destroy);
 
     if (!m_hdr) {
         throw std::runtime_error{"Could not load header from BAM file: '" + in_fn.string() + "'!"};
