ae5865db339c8fa37470b6dc1adc7f050a2f1f4c
John Stone <john.stone@nanoporetech.com>
2024-12-12 15:04:57 +0100

    Added length() to Interval.

--- dorado/polish/interval.h
+++ dorado/polish/interval.h
@@ -7,6 +7,8 @@ namespace dorado::polisher {
 struct Interval {
     int32_t start = 0;
     int32_t end = 0;
+
+    int32_t length() const { return end - start; }
 };
 
 }  // namespace dorado::polisher
