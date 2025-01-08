31a6b2be38de125c7c07580a5c8ed0f1f46eab3b
Steve Malton <stephen.malton@nanoporetech.com>
2025-01-08 11:26:42 +0000

    [DOR-958] Add method to report the number of jobs queued in a task executor

--- dorado/utils/concurrency/async_task_executor.h
+++ dorado/utils/concurrency/async_task_executor.h
@@ -26,7 +26,7 @@ namespace dorado::utils::concurrency {
 // also flush.
 class AsyncTaskExecutor {
     MultiQueueThreadPool::ThreadPoolQueue& m_thread_pool_queue;
-    std::mutex m_mutex;
+    mutable std::mutex m_mutex;
     std::condition_variable m_tasks_in_flight_cv;
     std::size_t m_num_tasks_in_flight{};
     std::unique_ptr<Latch> m_flushing_counter;
@@ -58,7 +58,11 @@ public:
                            task))]() -> decltype(auto) { return (*task_wrapper)(); });
     }
 
-    // Testability. Currently only needs to be made public for utests.
+    std::size_t num_tasks_in_flight() const {
+        std::lock_guard lock(m_mutex);
+        return m_num_tasks_in_flight;
+    }
+
     // Blocks until all queued tasks are completed.
     // After invoking no further tasks may be enqueued by this executor.
     void flush();
