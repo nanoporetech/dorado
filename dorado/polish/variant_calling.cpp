12d054c89b02d512a06222be411ff1fb09e1a4d8
John Stone <john.stone@nanoporetech.com>
2025-01-13 22:42:39 +0100

    Initial implementation of the join_samples function in dorado/polish/variant_calling.cpp.

--- dorado/polish/variant_calling.cpp
+++ dorado/polish/variant_calling.cpp
@@ -41,12 +41,65 @@ std::string extract_draft(const std::string& draft, const std::vector<int64_t>& 
     return ret;
 }
 
+VariantCallingSample slice_vc_sample(const VariantCallingSample& vc_sample,
+                                     const int64_t idx_start,
+                                     const int64_t idx_end) {
+    const int64_t num_columns = dorado::ssize(vc_sample.sample.positions_major);
+
+    // Validate lengths.
+    if (vc_sample.logits.defined() &&
+        (vc_sample.logits.size(0) != dorado::ssize(vc_sample.sample.positions_major))) {
+        throw std::runtime_error(
+                "VariantCallingSample::logits is of incorrect size. logits.size = " +
+                std::to_string(vc_sample.logits.size(0)) +
+                ", num_columns = " + std::to_string(num_columns));
+    }
+
+    // Validate idx.
+    if ((idx_start < 0) || (idx_start >= num_columns) || (idx_end >= idx_start) ||
+        (idx_end > num_columns)) {
+        throw std::out_of_range("Index is out of range in slice_vc_sample. idx_start = " +
+                                std::to_string(idx_start) +
+                                ", idx_end = " + std::to_string(idx_end) +
+                                ", num_columns = " + std::to_string(num_columns));
+    }
+
+    // Slice.
+    return VariantCallingSample{
+            slice_sample(vc_sample.sample, idx_start, idx_end),
+            vc_sample.logits.index({at::indexing::Slice(idx_start, idx_end)}).clone()};
+}
+
+std::vector<VariantCallingSample> merge_vc_samples(
+        const std::vector<VariantCallingSample>& vc_samples) {
+    if (std::empty(vc_samples)) {
+        return {};
+    }
+
+    std::vector<VariantCallingSample> ret;
+
+    ret.emplace_back(vc_samples.front());
+
+    for (int64_t i = 1; i < dorado::ssize(vc_samples); ++i) {
+        if (ret.back().sample.end() == (vc_samples[i].sample.start() + 1)) {
+            merge_adjacent_samples_in_place(ret.back().sample, vc_samples[i].sample);
+            ret.back().logits = torch::cat({std::move(ret.back().logits), vc_samples[i].logits});
+        } else {
+            ret.emplace_back(vc_samples[i]);
+        }
+    }
+
+    return ret;
+}
+
 /**
  * \brief This function restructures the neighboring samples for one draft sequence
  */
-std::vector<Sample> join_samples(const std::vector<VariantCallingSample>& vc_samples,
-                                 const std::string& draft,
-                                 const DecoderBase& decoder) {
+std::vector<VariantCallingSample> join_samples(const std::vector<VariantCallingSample>& vc_samples,
+                                               const std::string& draft,
+                                               const DecoderBase& decoder) {
+    std::vector<VariantCallingSample> ret;
+
     std::vector<VariantCallingSample> queue;
 
     for (int64_t i = 0; i < dorado::ssize(vc_samples); ++i) {
@@ -89,12 +142,15 @@ std::vector<Sample> join_samples(const std::vector<VariantCallingSample>& vc_sam
                 extract_draft_with_gaps(draft, sample.positions_major, sample.positions_minor);
         assert(std::size(call_with_gaps) == std::size(draft_with_gaps));
 
+        const auto check_is_diff = [](const char base1, const char base2) {
+            return (base1 != base2) || (base1 == '*' && base2 == '*');
+        };
+
         // Check if all positions are diffs, or if all positions are gaps in both sequences.
         {
             int64_t count = 0;
             for (int64_t j = 0; j < dorado::ssize(call_with_gaps); ++j) {
-                if ((call_with_gaps[j] != draft_with_gaps[j]) ||
-                    (call_with_gaps[j] == '*' && draft_with_gaps[j] == '*')) {
+                if (check_is_diff(call_with_gaps[j], draft_with_gaps[j])) {
                     ++count;
                 }
             }
@@ -105,14 +161,48 @@ std::vector<Sample> join_samples(const std::vector<VariantCallingSample>& vc_sam
             }
         }
 
-        // for (int64_t i =0
+        const int64_t num_positions = dorado::ssize(sample.positions_major);
 
-        // consensuses[i] = std::move(c.front());
+        // Find a location where to split the sample.
+        int64_t last_non_var_start = 0;
+        for (int64_t j = (num_positions - 1); j >= 0; --j) {
+            if ((sample.positions_minor[j] == 0) &&
+                !check_is_diff(call_with_gaps[j], draft_with_gaps[j])) {
+                last_non_var_start = j;
+                break;
+            }
+        }
 
-        // std::cout << "[i = " << i << "] sample = " << sample << "\nC: " << consensuses[i].seq << "\nD: " << draft << "\n";
+        // Split the sample.
+        VariantCallingSample left_slice = slice_vc_sample(vc_sample, 0, last_non_var_start);
+        VariantCallingSample right_slice =
+                slice_vc_sample(vc_sample, last_non_var_start, num_positions);
+
+        // Enqueue the queue if possible.
+        if (last_non_var_start > 0) {
+            queue.emplace_back(std::move(left_slice));
+        }
+
+        // Merge and insert.
+        if (!std::empty(queue)) {
+            auto new_samples = merge_vc_samples(queue);
+            ret.insert(std::end(ret), std::make_move_iterator(std::begin(new_samples)),
+                       std::make_move_iterator(std::end(new_samples)));
+        }
+
+        // Reset the queue.
+        queue = {std::move(right_slice)};
     }
 
-    return {};
+    // Merge and insert.
+    if (!std::empty(queue)) {
+        auto new_samples = merge_vc_samples(queue);
+        ret.insert(std::end(ret), std::make_move_iterator(std::begin(new_samples)),
+                   std::make_move_iterator(std::end(new_samples)));
+        queue.clear();
+    }
+
+    return ret;
 }
 
 std::vector<Sample> apply_trimming(const std::vector<const Sample*>& samples,
