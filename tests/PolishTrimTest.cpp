d8dce3b2a85154af138a10afe300db9cdafb638b
John Stone <john.stone@nanoporetech.com>
2025-01-13 10:43:16 +0100

    [DOR-1032] Added unit tests for the new trim_samples overload.

--- tests/PolishTrimTest.cpp
+++ tests/PolishTrimTest.cpp
@@ -15,8 +15,8 @@ TEST_CASE("trim_samples tests", TEST_GROUP) {
 
     // The actual Sample struct is complicated, but the only thing needed
     // for trimming are seq_id, positions_major and positions_minor.
-    // This would not be needed if C++20 was available becauase we could simply use
-    // designited initializers.
+    // This would not be needed if C++20 was available because we could simply use
+    // designated initializers.
     struct MockSample {
         int32_t seq_id = -1;
         std::vector<int64_t> positions_major;
@@ -219,7 +219,7 @@ TEST_CASE("trim_samples tests", TEST_GROUP) {
             },
         },
         TestCase{
-            "Use region, but the region is larger than what samples span. Region trimming won't influence the results.",
+            "Use a region, but the region is larger than what samples span. Region trimming won't influence the results.",
             {   // Input samples.
                 MockSample{
                     0,                                  // seq_id
@@ -246,7 +246,7 @@ TEST_CASE("trim_samples tests", TEST_GROUP) {
             },
         },
         TestCase{
-            "Trim to region which is shorter than the span of windows. Three samples overlapping, and one on a different reference. Trimming should be applied between samples, and on region start/end.",
+            "Trim to a region which is shorter than the span of windows. Three samples overlapping, and one on a different reference. Trimming should be applied between samples, and on region start/end.",
             {   // Input samples.
                 MockSample{
                     0,                                  // seq_id
@@ -279,7 +279,7 @@ TEST_CASE("trim_samples tests", TEST_GROUP) {
             },
         },
         TestCase{
-            "Use region, but region start is not valid. Should throw.",
+            "Use a region, but the start coordinate is not valid. Should throw.",
             {   // Input samples.
                 MockSample{
                     0,                                  // seq_id
@@ -293,7 +293,7 @@ TEST_CASE("trim_samples tests", TEST_GROUP) {
             },
         },
         TestCase{
-            "Use region, but region end is not valid. Should throw.",
+            "Use a region, but the end coordinate is not valid. Should throw.",
             {   // Input samples.
                 MockSample{
                     0,                                  // seq_id
@@ -307,7 +307,7 @@ TEST_CASE("trim_samples tests", TEST_GROUP) {
             },
         },
         TestCase{
-            "Use region, but region seq_id is not valid. Should throw.",
+            "Use a region, but the seq_id is not valid. Should throw.",
             {   // Input samples.
                 MockSample{
                     0,                                  // seq_id
@@ -337,6 +337,118 @@ TEST_CASE("trim_samples tests", TEST_GROUP) {
         CHECK_THROWS(trim_samples(samples, test_case.region));
     } else {
         const std::vector<TrimInfo> result = trim_samples(samples, test_case.region);
+        CHECK(test_case.expected == result);
+    }
+}
+
+/**
+ * \brief This tests the alternative overload if trim_samples which takes a vector of pointers instead
+ *          of a vector of actual contiguous data. This allows for the const data to be permuted or sorted
+ *          and still trimmed without making copies.
+ *
+ *          This test creates one const vector of mocked samples, then uses numeric IDs to create
+ *          the std::vector<const Sample*> used for input.
+ *          There is also a test for a nullptr pointer.
+ *
+ *          Since this function is essentially a backbone of the other overload, more detailed tests from the
+ *          previous fixture will also test all the other edge cases here.
+ */
+TEST_CASE("trim_samples via pointers", TEST_GROUP) {
+    using namespace dorado::polisher;
+
+    // The actual Sample struct is complicated, but the only thing needed
+    // for trimming are seq_id, positions_major and positions_minor.
+    // This would not be needed if C++20 was available because we could simply use
+    // designated initializers.
+    struct MockSample {
+        int32_t seq_id = -1;
+        std::vector<int64_t> positions_major;
+        std::vector<int64_t> positions_minor;
+    };
+
+    // Create all input samples, in a permuted order.
+    const std::vector<MockSample> all_mock_samples{
+            MockSample{
+                    0,             // seq_id
+                    {6, 6, 7, 7},  // positions_major
+                    {0, 1, 0, 1},  // positions_minor
+            },
+            MockSample{
+                    0,                                  // seq_id
+                    {0, 0, 1, 2, 2, 2, 3, 4, 4, 4, 4},  // positions_major
+                    {0, 1, 0, 0, 1, 2, 0, 0, 1, 2, 3},  // positions_minor
+            },
+            MockSample{
+                    0,                         // seq_id
+                    {4, 4, 4, 4, 5, 6, 6, 7},  // positions_major
+                    {1, 2, 3, 5, 0, 0, 1, 0},  // positions_minor
+            },
+    };
+
+    // clang-format off
+    struct TestCase {
+        std::string name;                       // Name of the test.
+        std::vector<int32_t> sample_ids;        // IDs of samples to create a permutation, -1 means nullptr.
+        bool expect_throw = false;
+        std::vector<TrimInfo> expected;         // Expected results.
+    };
+    // clang-format on
+
+    // clang-format off
+    auto [test_case] = GENERATE(table<TestCase>({
+        TestCase{
+            "Empty input == empty output",
+            {},             // sample_ids
+            false,          // expect_throw
+            {},             // expected
+        },
+        TestCase{
+            "Three samples, overlapping. Should trim.",
+            {1, 2, 0},      // sample_ids
+            false,          // expect_throw
+            {               // expected
+                TrimInfo{0, 9, false},
+                TrimInfo{1, 6, false},
+                TrimInfo{1, 4, false},
+            },
+        },
+        TestCase{
+            "One of the samples is a nullptr, this should throw.",
+            {1, -1, 0},     // sample_ids
+            true,           // expect_throw
+            {               // expected
+            },
+        },
+    }));
+    // clang-format on
+
+    // Create the actual Sample objects. This would not be needed if we had C++20 designated initializers.
+    INFO(TEST_GROUP << " Test name: " << test_case.name);
+    std::vector<Sample> samples;
+    for (const auto& mock_sample : all_mock_samples) {
+        Sample new_sample;
+        new_sample.seq_id = mock_sample.seq_id;
+        new_sample.positions_major = mock_sample.positions_major;
+        new_sample.positions_minor = mock_sample.positions_minor;
+        samples.emplace_back(std::move(new_sample));
+    }
+
+    // Create the input data as a vector of pointers.
+    std::vector<const Sample*> input;
+    for (const int32_t id : test_case.sample_ids) {
+        if (id < 0) {
+            input.emplace_back(nullptr);
+        } else if (id >= static_cast<int32_t>(std::size(samples))) {
+            throw std::runtime_error("Bad test case! Sample ID out of bounds!");
+        } else {
+            input.emplace_back(&samples[id]);
+        }
+    }
+
+    if (test_case.expect_throw) {
+        CHECK_THROWS(trim_samples(input, std::nullopt));
+    } else {
+        const std::vector<TrimInfo> result = trim_samples(input, std::nullopt);
         CHECK(test_case.expected == result);
     }
 }
