75380610fcbac48fb499ae47a8436ba76987174e
John Stone <john.stone@nanoporetech.com>
2025-01-13 13:33:56 +0100

    Added a placeholder for PolishSampleTest.cpp.

---
+++ tests/PolishSampleTest.cpp
@@ -0,0 +1,19 @@
+#include "polish/sample.h"
+
+#include <catch2/catch.hpp>
+
+#include <cstdint>
+#include <vector>
+
+namespace {
+
+#define TEST_GROUP "[PolishTrimSamples]"
+
+TEST_CASE("slice_sample", TEST_GROUP) {
+    using namespace dorado::polisher;
+
+
+
+}
+
+}  // namespace
