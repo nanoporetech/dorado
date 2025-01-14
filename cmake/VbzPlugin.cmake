2b7dd6cf6fdeec67ca5db103ae9b915bcf6c2e3c
Ben Lawrence <ben.lawrence@nanoporetech.com>
2025-01-14 17:23:56 +0000

    [INSTX-6217] Allow VbzPlugin.cmake to be reused by other projects
    Also bring over a useful comment from where this came from.

--- cmake/VbzPlugin.cmake
+++ cmake/VbzPlugin.cmake
@@ -1,10 +1,21 @@
 function(add_hdf_vbz_plugin)
+    # cmake policy CMP0077 allows options to be overridden (which is
+    # exactly what we're trying to do here) -- without it set to NEW
+    # some of these option overrides won't take effect.
+    set(CMAKE_POLICY_DEFAULT_CMP0077 NEW)
+
+    # hdf_plugins is a pretty general-purpose library and we don't
+    # want to compile it with several of its features. We can hide
+    # them in a function to avoid polluting state
     set(ENABLE_CONAN OFF)
     set(ENABLE_PERF_TESTING OFF)
     set(ENABLE_PYTHON OFF)
     set(ENABLE_PACKAGING OFF)
     set(BUILD_SHARED_LIBS OFF)
     set(BUILD_TESTING OFF)
+
+    # Grab the project root in a way that works for projects that include us.
+    set(_dorado_root ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/..)
 
     if(WIN32)
         # On windows we need to build a static lib for zstd as there's no prebuilt distro
@@ -47,7 +58,7 @@ function(add_hdf_vbz_plugin)
             COMMAND_ERROR_IS_FATAL ANY
         )
 
-        list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/dorado/3rdparty/hdf_plugins/cmake")
+        list(APPEND CMAKE_MODULE_PATH "${_dorado_root}/dorado/3rdparty/hdf_plugins/cmake")
         # Findzstd.cmake uses conan variables to determine where things are.
         set(CONAN_INCLUDE_DIRS_RELEASE ${ZSTD_INSTALL_DIR}/include)
         set(CONAN_LIB_DIRS_RELEASE ${ZSTD_INSTALL_DIR}/lib)
@@ -56,11 +67,11 @@ function(add_hdf_vbz_plugin)
     else()
         # Some platforms need the Findzstd.cmake from hdf_plugins, but append it in case we already
         # have one in the path.
-        list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/dorado/3rdparty/hdf_plugins/cmake")
+        list(APPEND CMAKE_MODULE_PATH "${_dorado_root}/dorado/3rdparty/hdf_plugins/cmake")
         # hdf_plugins looks for 1.3.1 but the libarrow in POD5 requires 1.3.6 minimum.
         find_package(zstd 1.3.6 REQUIRED)
 
     endif()
 
-    add_subdirectory(dorado/3rdparty/hdf_plugins EXCLUDE_FROM_ALL)
+    add_subdirectory(${_dorado_root}/dorado/3rdparty/hdf_plugins EXCLUDE_FROM_ALL)
 endfunction()
