a8fe5718c6d96b090aae14755ee8f2999e877f5e
John Stone <john.stone@nanoporetech.com>
2025-01-15 11:24:57 +0100

    Refactored the VCFWriter.

--- dorado/polish/vcf_writer.cpp
+++ dorado/polish/vcf_writer.cpp
@@ -1,101 +1,109 @@
 #include "vcf_writer.h"
 
+#include <htslib/vcf.h>
+
+#include <stdexcept>
+
 namespace dorado::polisher {
 
-// VCFWriter::VCFWriter() {
-
-// }
-
-VCFWriter::VCFWriter(const std::string& filename,
-                     const std::vector<std::pair<std::string, std::string>>& filters,
-                     const std::vector<std::pair<std::string, int64_t>>& contigs) {
-    // Open the file for writing
-    vcf_file_ = hts_open(filename.c_str(), "w");
-    if (!vcf_file_) {
-        throw std::runtime_error("Failed to open VCF file: " + filename);
+// RAII for the BCF header.
+void BcfHdrDestructor::operator()(bcf_hdr_t* p) {
+    if (p) {
+        bcf_hdr_destroy(p);
     }
+}
 
-    // Create a new VCF header
-    header_ = bcf_hdr_init("w");
-    if (!header_) {
+// RAII for a single BCF record.
+struct BcfRecordDestructor {
+    void operator()(bcf1_t*);
+};
+void BcfRecordDestructor::operator()(bcf1_t* p) {
+    if (p) {
+        bcf_destroy(p);
+    }
+}
+using BcfRecordPtr = std::unique_ptr<bcf1_t, BcfRecordDestructor>;
+
+VCFWriter::VCFWriter(const std::filesystem::path& in_fn,
+                     const std::vector<std::pair<std::string, std::string>>& filters,
+                     const std::vector<std::pair<std::string, int64_t>>& contigs)
+        : m_vcf_fp{hts_open(in_fn.string().c_str(), "w"), HtsFileDestructor()},
+          m_header{bcf_hdr_init("w"), BcfHdrDestructor()} {
+    if (!m_vcf_fp) {
+        throw std::runtime_error("Failed to open VCF file: " + in_fn.string());
+    }
+    if (!m_header) {
         throw std::runtime_error("Failed to create VCF header.");
     }
 
     // Add the VCF format version
-    bcf_hdr_append(header_, "##fileformat=VCFv4.1");
+    bcf_hdr_append(m_header.get(), "##fileformat=VCFv4.1");
 
     // Add contig information
     for (const auto& [name, length] : contigs) {
-        std::string contig_entry =
+        const std::string contig_entry =
                 "##contig=<ID=" + name + ",length=" + std::to_string(length) + ">";
-        bcf_hdr_append(header_, contig_entry.c_str());
+        bcf_hdr_append(m_header.get(), contig_entry.c_str());
     }
 
     // Add FILTER entries.
     for (const auto& [id, description] : filters) {
-        std::string filter_entry = "##FILTER=<ID=" + id + ",Description=\"" + description + "\">";
-        bcf_hdr_append(header_, filter_entry.c_str());
+        const std::string filter_entry =
+                "##FILTER=<ID=" + id + ",Description=\"" + description + "\">";
+        bcf_hdr_append(m_header.get(), filter_entry.c_str());
     }
 
     // Add mandatory INFO and FORMAT fields
-    bcf_hdr_append(header_, "##dorado_version=");
-    bcf_hdr_append(header_, "##INFO=<ID=DP,Number=1,Type=Integer,Description=\"Total Depth\">");
-    bcf_hdr_append(header_, "##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">");
-    bcf_hdr_append(header_,
+    bcf_hdr_append(m_header.get(), "##dorado_version=");
+    bcf_hdr_append(m_header.get(),
+                   "##INFO=<ID=DP,Number=1,Type=Integer,Description=\"Total Depth\">");
+    bcf_hdr_append(m_header.get(),
+                   "##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">");
+    bcf_hdr_append(m_header.get(),
                    "##FORMAT=<ID=GQ,Number=1,Type=Integer,Description=\"Genotype quality score\">");
 
-    if (bcf_hdr_add_sample(header_, "SAMPLE") != 0) {
+    if (bcf_hdr_add_sample(m_header.get(), "SAMPLE") != 0) {
         throw std::runtime_error("Failed to add sample: SAMPLE");
     }
 
     // Add column headers
-    bcf_hdr_append(header_, "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tSAMPLE");
+    bcf_hdr_append(m_header.get(), "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tSAMPLE");
 
     // Write the header to the file
-    if (bcf_hdr_write(vcf_file_, header_) < 0) {
+    if (bcf_hdr_write(m_vcf_fp.get(), m_header.get()) < 0) {
         throw std::runtime_error("Failed to write VCF header.");
     }
 }
 
-VCFWriter::~VCFWriter() {
-    if (header_) {
-        bcf_hdr_destroy(header_);
-    }
-    if (vcf_file_) {
-        hts_close(vcf_file_);
-    }
-}
-
 void VCFWriter::write_variant(const Variant& variant) {
-    // Create a new VCF record
-    bcf1_t* record = bcf_init();
+    BcfRecordPtr record{bcf_init(), BcfRecordDestructor()};
+
     if (!record) {
         throw std::runtime_error("Failed to create VCF record.");
     }
 
-    // Set the record fields
+    // Set the record fields.
     record->rid = variant.seq_id;
     record->pos = variant.pos;
-    bcf_update_id(header_, record, ".");
-    bcf_update_alleles_str(header_, record, (variant.ref + "," + variant.alt).c_str());
-
+    bcf_update_id(m_header.get(), record.get(), ".");
+    bcf_update_alleles_str(m_header.get(), record.get(), (variant.ref + "," + variant.alt).c_str());
     record->qual = variant.qual;
 
     // Look up the FILTER ID in the header
-    if (!variant.filter.empty()) {
-        int32_t filter_id = bcf_hdr_id2int(header_, BCF_DT_ID, variant.filter.c_str());
+    if (!std::empty(variant.filter)) {
+        int32_t filter_id = bcf_hdr_id2int(m_header.get(), BCF_DT_ID, variant.filter.c_str());
         if (filter_id < 0) {
-            throw std::runtime_error("FILTER ID '" + variant.filter + "' not found in header.");
+            throw std::runtime_error("VCF filter ID '" + variant.filter + "' not found in header.");
         }
-        bcf_update_filter(header_, record, &filter_id, 1);
+        bcf_update_filter(m_header.get(), record.get(), &filter_id, 1);
     }
 
-    // Add INFO fields
+    // Add INFO fields.
     for (const auto& [key, value] : variant.info) {
-        bcf_update_info_string(header_, record, key.c_str(), value.c_str());
+        bcf_update_info_string(m_header.get(), record.get(), key.c_str(), value.c_str());
     }
 
-    // Process genotype to ensure GT comes first
+    // Genotype.
     {
         std::vector<std::string> format_keys;
         std::vector<int32_t> format_values;
@@ -115,20 +123,17 @@ void VCFWriter::write_variant(const Variant& variant) {
         if (std::empty(format_keys) || (format_keys.front() != "GT")) {
             throw std::runtime_error("Genotype key GT not found in variant.genotype!");
         }
-        bcf_update_genotypes(header_, record, &format_values[0], 1);
+        bcf_update_genotypes(m_header.get(), record.get(), &format_values[0], 1);
         for (size_t i = 1; i < std::size(format_keys); ++i) {
-            bcf_update_format_int32(header_, record, format_keys[i].c_str(), &format_values[i], 1);
+            bcf_update_format_int32(m_header.get(), record.get(), format_keys[i].c_str(),
+                                    &format_values[i], 1);
         }
     }
 
     // Write the record.
-    if (bcf_write(vcf_file_, header_, record) < 0) {
-        bcf_destroy(record);
+    if (bcf_write(m_vcf_fp.get(), m_header.get(), record.get()) < 0) {
         throw std::runtime_error("Failed to write VCF record.");
     }
-
-    // Free the record
-    bcf_destroy(record);
 }
 
 }  // namespace dorado::polisher
--- dorado/polish/vcf_writer.h
+++ dorado/polish/vcf_writer.h
@@ -1,36 +1,40 @@
 #pragma once
 
+#include "utils/types.h"
 #include "variant.h"
 
-#include <htslib/vcf.h>
-
-#include <iostream>
-#include <stdexcept>
+#include <filesystem>
 #include <string>
-#include <unordered_map>
+#include <vector>
+
+struct bcf_hdr_t;
 
 namespace dorado::polisher {
 
-// class VCFWriter {
-// public:
-//     VCFWriter();
-// };
+// RAII for the BCF header.
+struct BcfHdrDestructor {
+    void operator()(bcf_hdr_t*);
+};
+using BcfHdrPtr = std::unique_ptr<bcf_hdr_t, BcfHdrDestructor>;
 
 class VCFWriter {
-private:
-    htsFile* vcf_file_ = nullptr;
-    bcf_hdr_t* header_ = nullptr;
-
 public:
-    VCFWriter(const std::string& filename,
+    /**
+     * \brief Opens a VCF file for writing.
+     * \param filters A vector of all possible filters which can appear in this VCF. Required, or Htslib will fail to produce a record. Pair is: <filter_name, description>. The description is a free-text description of the filter.
+     * \param contigs A vector of header/length pairs for every contig which may appear in this VCF.
+     */
+    VCFWriter(const std::filesystem::path& filename,
               const std::vector<std::pair<std::string, std::string>>& filters,
               const std::vector<std::pair<std::string, int64_t>>& contigs);
 
-    ~VCFWriter();
+    ~VCFWriter() = default;
 
     void write_variant(const Variant& variant);
-};
 
-// void write_vcf(const std::string& filename, const std::vector<std::pair<std::string, int64_t>>& contigs
+private:
+    HtsFilePtr m_vcf_fp;
+    BcfHdrPtr m_header;
+};
 
 }  // namespace dorado::polisher
