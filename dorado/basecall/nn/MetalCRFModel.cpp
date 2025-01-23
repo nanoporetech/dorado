f9b5fb421b23603e95715dd93c6763acd87cc890
Mark Bicknell <mark.bicknell@nanoporetech.com>
2025-01-23 17:08:46 +0000

    Reinstated a note that we might want to use 32 simd groups for M4 devices

--- dorado/basecall/nn/MetalCRFModel.cpp
+++ dorado/basecall/nn/MetalCRFModel.cpp
@@ -269,7 +269,7 @@ MetalBlockImpl::MetalBlockImpl(int chunk_size_,
         kernel_simd_groups = 32;
         break;
     case 384:
-        kernel_simd_groups = 24;
+        kernel_simd_groups = 24;  // Note - we may want to set this to 32 for M4
         break;
     case 512:
         kernel_simd_groups = 32;
