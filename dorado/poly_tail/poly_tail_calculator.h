f9f868d5cf93ba89eef180f96582ee0f2d8a8081
Steve Malton <stephen.malton@nanoporetech.com>
2025-02-18 13:38:53 +0000

    [DOR-1071] Allow poly tail calculators to override the signal search range

--- dorado/poly_tail/poly_tail_calculator.h
+++ dorado/poly_tail/poly_tail_calculator.h
@@ -61,10 +61,10 @@ protected:
                                              float samples_per_base) const = 0;
 
     // Determine the outer boundary of the signal space to consider based on the anchor.
-    std::pair<int, int> signal_range(int signal_anchor,
-                                     int signal_len,
-                                     float samples_per_base,
-                                     bool fwd) const;
+    virtual std::pair<int, int> signal_range(int signal_anchor,
+                                             int signal_len,
+                                             float samples_per_base,
+                                             bool fwd) const;
 
     std::pair<float, float> estimate_samples_per_base(const dorado::SimplexRead& read) const;
 
