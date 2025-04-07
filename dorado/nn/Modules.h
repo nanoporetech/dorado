93a81fbc932e39b062bceb588bce1fb1cd67fe89
Richard Harris <richard.harris@nanoporetech.com>
2025-04-07 13:13:26 +0100

    DOR-1138 create new module dorado/nn moving all custom modules from the LSTM models other than the root model itself into Modules for re-use in modbase models later. Split out the WorkingMemory class into it's own file so that it can be used by the root model implementations.

---
+++ dorado/nn/Modules.h
@@ -0,0 +1,87 @@
+#pragma once
+
+#include "config/common.h"
+#include "nn/WorkingMemory.h"
+
+#include <torch/nn.h>
+
+namespace dorado::nn {
+
+struct ConvStackImpl : torch::nn::Module {
+    explicit ConvStackImpl(const std::vector<config::ConvParams> &layer_params);
+#if DORADO_CUDA_BUILD
+    void reserve_working_memory(WorkingMemory &wm);
+    void run_koi(WorkingMemory &wm);
+#endif  // if DORADO_CUDA_BUILD
+
+    at::Tensor forward(at::Tensor x);
+
+    struct ConvLayer {
+        explicit ConvLayer(const config::ConvParams &params);
+        const config::ConvParams params;
+        torch::nn::Conv1d conv{nullptr};
+#if DORADO_CUDA_BUILD
+        TensorLayout output_layout{TensorLayout::NTC};
+        bool cutlass_conv{false};
+        int output_T_padding{0};
+        at::Tensor w_device;
+        at::Tensor b_device;
+
+        void reserve_working_memory(WorkingMemory &wm);
+        void run_koi(WorkingMemory &wm);
+#endif  // if DORADO_CUDA_BUILD
+    };
+
+    std::vector<ConvLayer> layers;
+};
+
+struct LinearCRFImpl : torch::nn::Module {
+    LinearCRFImpl(int insize, int outsize, bool bias_, bool tanh_and_scale);
+    at::Tensor forward(const at::Tensor &x);
+#if DORADO_CUDA_BUILD
+    void reserve_working_memory(WorkingMemory &wm);
+    void run_koi(WorkingMemory &wm);
+    at::Tensor w_device;
+    at::Tensor weight_scale;
+#endif  // if DORADO_CUDA_BUILD
+    bool bias;
+    static constexpr int scale = 5;
+    torch::nn::Linear linear{nullptr};
+    torch::nn::Tanh activation{nullptr};
+};
+
+struct LSTMStackImpl : torch::nn::Module {
+    LSTMStackImpl(int num_layers, int size);
+    at::Tensor forward(at::Tensor x);
+#if DORADO_CUDA_BUILD
+    void reserve_working_memory(WorkingMemory &wm);
+    void run_koi(WorkingMemory &wm);
+
+private:
+    void forward_cublas(WorkingMemory &wm);
+    void forward_cutlass(WorkingMemory &wm);
+    void forward_quantized(WorkingMemory &wm);
+
+    std::vector<at::Tensor> device_weights;
+    std::vector<at::Tensor> device_w_ih;
+    std::vector<at::Tensor> device_w_hh;
+    std::vector<at::Tensor> device_bias;
+    std::vector<at::Tensor> device_scale;
+#endif  // if DORADO_CUDA_BUILD
+    int layer_size;
+    std::vector<torch::nn::LSTM> rnns;
+};
+
+struct ClampImpl : torch::nn::Module {
+    ClampImpl(float _min, float _max, bool _active);
+    at::Tensor forward(at::Tensor x);
+    bool active;
+    float min, max;
+};
+
+TORCH_MODULE(LSTMStack);
+TORCH_MODULE(LinearCRF);
+TORCH_MODULE(ConvStack);
+TORCH_MODULE(Clamp);
+
+}  // namespace dorado::nn
---
+++ dorado/nn/WorkingMemory.cpp
@@ -0,0 +1,104 @@
+#include "WorkingMemory.h"
+
+#include "config/common.h"
+#include "utils/math_utils.h"
+
+#if DORADO_CUDA_BUILD
+#include <ATen/cuda/CUDAContext.h>
+#include <c10/cuda/CUDAGuard.h>
+
+extern "C" {
+#include "koi.h"
+}
+#endif
+
+#include <torch/torch.h>
+
+namespace dorado::nn {
+
+#if DORADO_CUDA_BUILD
+
+int64_t WorkingMemory::tensor_bytes(torch::IntArrayRef sizes, torch::Dtype dtype) {
+    auto elems = c10::multiply_integers(sizes);
+    return utils::pad_to<int64_t>(elems * torch::elementSize(dtype), ALIGNMENT);
+}
+
+at::Tensor WorkingMemory::next(torch::IntArrayRef sizes, torch::Dtype dtype, bool make_current) {
+    auto new_bytes = tensor_bytes(sizes, dtype);
+    at::Tensor new_tensor;
+    if (!backing_tensor.defined()) {
+        // If no backing tensor is allocated yet we're still in the reservation phase
+        reservation_bytes = std::max(reservation_bytes, current_bytes + new_bytes);
+    } else {
+        if (current_bytes + new_bytes > reservation_bytes) {
+            throw std::runtime_error("WorkingMemory: overlap detected.");
+        }
+
+        bool current_is_front =
+                current.defined() && current.data_ptr() == backing_tensor.data_ptr();
+        auto elems = c10::multiply_integers(sizes);
+        auto bt_dtype = backing_tensor.view(dtype);
+        auto start_pos = current_is_front
+                                 ? (reservation_bytes - new_bytes) / torch::elementSize(dtype)
+                                 : int64_t(0);
+        new_tensor = bt_dtype.narrow(0, start_pos, elems).view(sizes);
+    }
+    if (make_current) {
+        current_bytes = new_bytes;
+        current = new_tensor;
+    }
+    return new_tensor;
+}
+
+at::Tensor WorkingMemory::get_current_NTC_view() {
+    switch (layout) {
+    case TensorLayout::NTC:
+        return current;
+    case TensorLayout::TNC:
+        return current.transpose(0, 1);
+    case TensorLayout::CUTLASS_TNC_F16:
+    case TensorLayout::CUTLASS_TNC_I8:
+        return current.narrow(0, is_input_to_rev_lstm ? 1 : 2, T).transpose(1, 0);
+    case TensorLayout::CUBLAS_TN2C:
+        return current.narrow(0, is_input_to_rev_lstm ? 1 : 0, T)
+                .transpose(1, 0)
+                .select(2, is_input_to_rev_lstm ? 1 : 0);
+    default:
+        throw std::logic_error("Unhandled TensorLayout");
+    }
+}
+
+at::Tensor WorkingMemory::next_TC(int T_, int C_, TensorLayout layout_) {
+    T = T_;
+    C = C_;
+    layout = layout_;
+    if (layout == TensorLayout::NTC) {
+        return next({N, T, C}, torch::kF16, true);
+    } else if (layout == TensorLayout::TNC) {
+        return next({T, N, C}, torch::kF16, true);
+    } else if (layout == TensorLayout::CUTLASS_TNC_F16) {
+        return next({T + 3, N, C}, torch::kF16, true);
+    } else if (layout == TensorLayout::CUTLASS_TNC_I8) {
+        return next({T + 3, N, C}, torch::kI8, true);
+    } else if (layout == TensorLayout::CUBLAS_TN2C) {
+        return next({T + 1, N, 2, C}, torch::kF16, true);
+    } else {
+        throw std::logic_error("Unhandled TensorLayout");
+    }
+}
+
+at::Tensor WorkingMemory::temp(torch::IntArrayRef sizes, torch::Dtype dtype) {
+    return next(sizes, dtype, false);
+}
+
+void WorkingMemory::allocate_backing_tensor(torch::Device dev) {
+    // Using kF16 here because the libtorch version on TX2 doesn't support `Tensor::view()`
+    // with a dtype of a different size, and all buffers are kF16 on TX2.
+    backing_tensor = torch::empty({reservation_bytes / 2},
+                                  at::TensorOptions().device(dev).dtype(torch::kF16));
+    current_bytes = 0;
+}
+
+#endif  // if DORADO_CUDA_BUILD
+
+}  // namespace dorado::nn
