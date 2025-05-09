a1b141cc6c7026c8c0e1116b89cc51696ec417c6
Xiaowen Feng <xiaowen.feng@nanoporetech.com>
2025-05-09 13:45:43 +0200

    [DOR-1130] Added the Kadayashi (local_haplotagging) library to dorado/3rdparty/kadayashi, and added unit tests to tests/KadayashiTest.cpp.

---
+++ dorado/3rdparty/kadayashi/src/haplotag_lib/ksort.h
@@ -0,0 +1,294 @@
+/* The MIT License
+
+   Copyright (c) 2008, 2011 Attractive Chaos <attractor@live.co.uk>
+
+   Permission is hereby granted, free of charge, to any person obtaining
+   a copy of this software and associated documentation files (the
+   "Software"), to deal in the Software without restriction, including
+   without limitation the rights to use, copy, modify, merge, publish,
+   distribute, sublicense, and/or sell copies of the Software, and to
+   permit persons to whom the Software is furnished to do so, subject to
+   the following conditions:
+
+   The above copyright notice and this permission notice shall be
+   included in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+*/
+
+/*
+  2011-04-10 (0.1.6):
+
+  	* Added sample
+
+  2011-03 (0.1.5):
+
+	* Added shuffle/permutation
+
+  2008-11-16 (0.1.4):
+
+    * Fixed a bug in introsort() that happens in rare cases.
+
+  2008-11-05 (0.1.3):
+
+    * Fixed a bug in introsort() for complex comparisons.
+
+	* Fixed a bug in mergesort(). The previous version is not stable.
+
+  2008-09-15 (0.1.2):
+
+	* Accelerated introsort. On my Mac (not on another Linux machine),
+	  my implementation is as fast as std::sort on random input.
+
+	* Added combsort and in introsort, switch to combsort if the
+	  recursion is too deep.
+
+  2008-09-13 (0.1.1):
+
+	* Added k-small algorithm
+
+  2008-09-05 (0.1.0):
+
+	* Initial version
+
+*/
+
+#ifndef AC_KSORT_H
+#define AC_KSORT_H
+
+#include <stdlib.h>
+#include <string.h>
+
+namespace kadayashi {
+
+typedef struct {
+    void *left, *right;
+    int depth;
+} ks_isort_stack_t;
+
+#define KSORT_SWAP(type_t, a, b) \
+    {                            \
+        type_t t = (a);          \
+        (a) = (b);               \
+        (b) = t;                 \
+    }
+
+#define KSORT_INIT(name, type_t, __sort_lt)                                          \
+    void ks_mergesort_##name(size_t n, type_t array[], type_t temp[]) {              \
+        type_t *a2[2], *a, *b;                                                       \
+        int curr, shift;                                                             \
+                                                                                     \
+        a2[0] = array;                                                               \
+        a2[1] = temp ? temp : (type_t *)malloc(sizeof(type_t) * n);                  \
+        for (curr = 0, shift = 0; (1ul << shift) < n; ++shift) {                     \
+            a = a2[curr];                                                            \
+            b = a2[1 - curr];                                                        \
+            if (shift == 0) {                                                        \
+                type_t *p = b, *i, *eb = a + n;                                      \
+                for (i = a; i < eb; i += 2) {                                        \
+                    if (i == eb - 1)                                                 \
+                        *p++ = *i;                                                   \
+                    else {                                                           \
+                        if (__sort_lt(*(i + 1), *i)) {                               \
+                            *p++ = *(i + 1);                                         \
+                            *p++ = *i;                                               \
+                        } else {                                                     \
+                            *p++ = *i;                                               \
+                            *p++ = *(i + 1);                                         \
+                        }                                                            \
+                    }                                                                \
+                }                                                                    \
+            } else {                                                                 \
+                size_t i, step = 1ul << shift;                                       \
+                for (i = 0; i < n; i += step << 1) {                                 \
+                    type_t *p, *j, *k, *ea, *eb;                                     \
+                    if (n < i + step) {                                              \
+                        ea = a + n;                                                  \
+                        eb = a;                                                      \
+                    } else {                                                         \
+                        ea = a + i + step;                                           \
+                        eb = a + (n < i + (step << 1) ? n : i + (step << 1));        \
+                    }                                                                \
+                    j = a + i;                                                       \
+                    k = a + i + step;                                                \
+                    p = b + i;                                                       \
+                    while (j < ea && k < eb) {                                       \
+                        if (__sort_lt(*k, *j))                                       \
+                            *p++ = *k++;                                             \
+                        else                                                         \
+                            *p++ = *j++;                                             \
+                    }                                                                \
+                    while (j < ea)                                                   \
+                        *p++ = *j++;                                                 \
+                    while (k < eb)                                                   \
+                        *p++ = *k++;                                                 \
+                }                                                                    \
+            }                                                                        \
+            curr = 1 - curr;                                                         \
+        }                                                                            \
+        if (curr == 1) {                                                             \
+            type_t *p = a2[0], *i = a2[1], *eb = array + n;                          \
+            for (; p < eb; ++i)                                                      \
+                *p++ = *i;                                                           \
+        }                                                                            \
+        if (temp == 0)                                                               \
+            free(a2[1]);                                                             \
+    }                                                                                \
+    /* This function is adapted from: http://ndevilla.free.fr/median/ */             \
+    /* 0 <= kk < n */                                                                \
+    type_t ks_ksmall_##name(size_t n, type_t arr[], size_t kk) {                     \
+        type_t *low, *high, *k, *ll, *hh, *mid;                                      \
+        low = arr;                                                                   \
+        high = arr + n - 1;                                                          \
+        k = arr + kk;                                                                \
+        for (;;) {                                                                   \
+            if (high <= low)                                                         \
+                return *k;                                                           \
+            if (high == low + 1) {                                                   \
+                if (__sort_lt(*high, *low))                                          \
+                    KSORT_SWAP(type_t, *low, *high);                                 \
+                return *k;                                                           \
+            }                                                                        \
+            mid = low + (high - low) / 2;                                            \
+            if (__sort_lt(*high, *mid))                                              \
+                KSORT_SWAP(type_t, *mid, *high);                                     \
+            if (__sort_lt(*high, *low))                                              \
+                KSORT_SWAP(type_t, *low, *high);                                     \
+            if (__sort_lt(*low, *mid))                                               \
+                KSORT_SWAP(type_t, *mid, *low);                                      \
+            KSORT_SWAP(type_t, *mid, *(low + 1));                                    \
+            ll = low + 1;                                                            \
+            hh = high;                                                               \
+            for (;;) {                                                               \
+                do                                                                   \
+                    ++ll;                                                            \
+                while (__sort_lt(*ll, *low));                                        \
+                do                                                                   \
+                    --hh;                                                            \
+                while (__sort_lt(*low, *hh));                                        \
+                if (hh < ll)                                                         \
+                    break;                                                           \
+                KSORT_SWAP(type_t, *ll, *hh);                                        \
+            }                                                                        \
+            KSORT_SWAP(type_t, *low, *hh);                                           \
+            if (hh <= k)                                                             \
+                low = ll;                                                            \
+            if (hh >= k)                                                             \
+                high = hh - 1;                                                       \
+        }                                                                            \
+    }                                                                                \
+    void ks_shuffle_##name(size_t n, type_t a[]) {                                   \
+        int i, j;                                                                    \
+        for (i = n; i > 1; --i) {                                                    \
+            type_t tmp;                                                              \
+            j = (int)(drand48() * i);                                                \
+            tmp = a[j];                                                              \
+            a[j] = a[i - 1];                                                         \
+            a[i - 1] = tmp;                                                          \
+        }                                                                            \
+    }                                                                                \
+    void ks_sample_##name(size_t n, size_t r, type_t a[]) /* FIXME: NOT TESTED!!! */ \
+    { /* reference: http://code.activestate.com/recipes/272884/ */                   \
+        int i;                                                                       \
+        size_t k, pop = n;                                                           \
+        for (i = (int)r, k = 0; i >= 0; --i) {                                       \
+            double z = 1., x = drand48();                                            \
+            type_t tmp;                                                              \
+            while (x < z)                                                            \
+                z -= z * i / (pop--);                                                \
+            if (k != n - pop - 1)                                                    \
+                tmp = a[k], a[k] = a[n - pop - 1], a[n - pop - 1] = tmp;             \
+            ++k;                                                                     \
+        }                                                                            \
+    }
+
+#define ks_mergesort(name, n, a, t) ks_mergesort_##name(n, a, t)
+#define ks_introsort(name, n, a) ks_introsort_##name(n, a)
+#define ks_combsort(name, n, a) ks_combsort_##name(n, a)
+#define ks_heapsort(name, n, a) ks_heapsort_##name(n, a)
+#define ks_heapmake(name, n, a) ks_heapmake_##name(n, a)
+#define ks_heapadjust(name, i, n, a) ks_heapadjust_##name(i, n, a)
+#define ks_ksmall(name, n, a, k) ks_ksmall_##name(n, a, k)
+#define ks_shuffle(name, n, a) ks_shuffle_##name(n, a)
+
+#define ks_lt_generic(a, b) ((a) < (b))
+#define ks_lt_str(a, b) (strcmp((a), (b)) < 0)
+
+typedef const char *ksstr_t;
+
+#define KSORT_INIT_GENERIC(type_t) KSORT_INIT(type_t, type_t, ks_lt_generic)
+#define KSORT_INIT_STR KSORT_INIT(str, ksstr_t, ks_lt_str)
+
+#define RS_MIN_SIZE 64
+#define RS_MAX_BITS 8
+
+#define KRADIX_SORT_INIT(name, rstype_t, rskey, sizeof_key)                        \
+    typedef struct {                                                               \
+        rstype_t *b, *e;                                                           \
+    } rsbucket_##name##_t;                                                         \
+    void rs_insertsort_##name(rstype_t *beg, rstype_t *end) {                      \
+        rstype_t *i;                                                               \
+        for (i = beg + 1; i < end; ++i)                                            \
+            if (rskey(*i) < rskey(*(i - 1))) {                                     \
+                rstype_t *j, tmp = *i;                                             \
+                for (j = i; j > beg && rskey(tmp) < rskey(*(j - 1)); --j)          \
+                    *j = *(j - 1);                                                 \
+                *j = tmp;                                                          \
+            }                                                                      \
+    }                                                                              \
+    void rs_sort_##name(rstype_t *beg, rstype_t *end, int n_bits, int s) {         \
+        rstype_t *i;                                                               \
+        int size = 1 << n_bits, m = size - 1;                                      \
+        rsbucket_##name##_t *k, b[1 << RS_MAX_BITS], *be = b + size;               \
+        assert(n_bits <= RS_MAX_BITS);                                             \
+        for (k = b; k != be; ++k)                                                  \
+            k->b = k->e = beg;                                                     \
+        for (i = beg; i != end; ++i)                                               \
+            ++b[rskey(*i) >> s & m].e;                                             \
+        for (k = b + 1; k != be; ++k)                                              \
+            k->e += (k - 1)->e - beg, k->b = (k - 1)->e;                           \
+        for (k = b; k != be;) {                                                    \
+            if (k->b != k->e) {                                                    \
+                rsbucket_##name##_t *l;                                            \
+                if ((l = b + (rskey(*k->b) >> s & m)) != k) {                      \
+                    rstype_t tmp = *k->b, swap;                                    \
+                    do {                                                           \
+                        swap = tmp;                                                \
+                        tmp = *l->b;                                               \
+                        *l->b++ = swap;                                            \
+                        l = b + (rskey(tmp) >> s & m);                             \
+                    } while (l != k);                                              \
+                    *k->b++ = tmp;                                                 \
+                } else                                                             \
+                    ++k->b;                                                        \
+            } else                                                                 \
+                ++k;                                                               \
+        }                                                                          \
+        for (b->b = beg, k = b + 1; k != be; ++k)                                  \
+            k->b = (k - 1)->e;                                                     \
+        if (s) {                                                                   \
+            s = s > n_bits ? s - n_bits : 0;                                       \
+            for (k = b; k != be; ++k)                                              \
+                if (k->e - k->b > RS_MIN_SIZE)                                     \
+                    rs_sort_##name(k->b, k->e, n_bits, s);                         \
+                else if (k->e - k->b > 1)                                          \
+                    rs_insertsort_##name(k->b, k->e);                              \
+        }                                                                          \
+    }                                                                              \
+    void radix_sort_##name(rstype_t *beg, rstype_t *end) {                         \
+        if (end - beg <= RS_MIN_SIZE)                                              \
+            rs_insertsort_##name(beg, end);                                        \
+        else                                                                       \
+            rs_sort_##name(beg, end, RS_MAX_BITS, (sizeof_key - 1) * RS_MAX_BITS); \
+    }
+
+}  // namespace kadayashi
+
+#endif
---
+++ tests/KadayashiTest.cpp
@@ -0,0 +1,140 @@
+#include "TestUtils.h"
+#include "hts_io/FastxRandomReader.h"
+#include "local_haplotagging.h"
+#include "secondary/bam_file.h"
+#include "types.h"
+
+#include <catch2/catch_test_macros.hpp>
+#include <htslib/faidx.h>
+#include <htslib/khash.h>
+#include <htslib/khash_str2int.h>
+#include <htslib/sam.h>
+#include <stdint.h>
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+
+#define TEST_GROUP "[KadayashiInterfaceTest]"
+
+namespace kadayashi::tests {
+
+CATCH_TEST_CASE("kadayashi_natoi", TEST_GROUP) {
+    CATCH_CHECK(natoi("4215M", 2) == 42);
+    CATCH_CHECK(natoi("4215M", 4) == 4215);
+    CATCH_CHECK(natoi("-3M", 2) == -1);
+    CATCH_CHECK(natoi("1.5M", 3) == -1);
+    CATCH_CHECK(natoi("111\0M", 4) == -1);
+    CATCH_CHECK(natoi("2147483647M", 10) == 2147483647);
+    CATCH_CHECK(natoi("2147483648M", 10) == -1);
+    CATCH_CHECK(natoi("2147483649M", 10) == -1);
+    CATCH_CHECK(natoi("11147483649M", 11) == -1);
+}
+
+CATCH_TEST_CASE("kadayashi_dvr_1 normal case", TEST_GROUP) {
+    // Input data.
+    const std::filesystem::path test_data_dir = get_data_dir("variant") / "test-02-supertiny";
+    const std::filesystem::path fn_bam = test_data_dir / "in.aln.bam";
+    const std::filesystem::path fn_ref = test_data_dir / "in.ref.fasta.gz";
+
+    const std::unordered_map<std::string, int32_t> expected{
+            {"1e70cda3-c41f-4d19-9c14-94d8d64e619c", 2},
+            {"61ab09d6-072f-4ab2-b14b-b0a1e38a3419", 2},
+            {"563ecca1-30dd-4dd9-991a-d417d827c803", 1},
+            {"4fd81aa2-cb77-4994-a8a5-70e6228f255e", 1},
+            {"a27cad27-2297-40d4-8666-40a4742eb2ed", 2},
+            {"e0af6c87-8655-4603-97b7-0ad5ba860df2", 1},
+            {"7d23577c-5c93-4d41-83bd-b652e687deee", 1},
+            {"627ea9e1-5204-4a2c-ae54-1e1be8bbbbe6", 2},
+            {"ac863a7d-932e-42fa-91c1-7814d7f810f9", 2},
+            {"b4139858-e420-4780-94e6-375542c2d2e8", 1},
+            {"dbe9785a-fa25-454c-9960-fd65fb99a040", 2},
+            {"3fdc1b9b-7186-411e-af92-e93a1086754c", 2},
+            {"7b2095d4-08f7-448d-aa9d-55c9568fb49d", 2},
+            {"c488f4c5-1639-4be1-92f6-948f29b7d822", 2},
+            {"02551418-20c9-4b4b-9d1b-9bee36342895", 2},
+            {"de45db56-e704-4524-af88-06a2f98c270e", 2},
+            {"49b05d0d-97ac-449e-804b-35b35e05ce28", 1},
+            {"e7e27cb5-1144-49dd-8ec4-09a75937a091", 1},
+            {"3d7a9813-67be-4b84-b66a-0269aa108340", 2},
+            {"d5560893-59c8-417c-a929-d62b4d19a1ca", 2},
+    };
+
+    // Open the input files.
+    dorado::secondary::BamFile bam_reader(fn_bam);
+    dorado::hts_io::FastxRandomReader fastx_reader(fn_ref);
+
+    CATCH_REQUIRE(bam_reader.fp());
+    CATCH_REQUIRE(bam_reader.idx());
+    CATCH_REQUIRE(bam_reader.hdr());
+    CATCH_REQUIRE(fastx_reader.get_raw_faidx_ptr());
+
+    // UUT.
+    const std::unordered_map<std::string, int32_t> result =
+            kadayashi::kadayashi_dvr_single_region_wrapper(
+                    bam_reader.fp(), bam_reader.idx(), bam_reader.hdr(),
+                    fastx_reader.get_raw_faidx_ptr(), "chr20", 1, 10000, 0, 5, 5, 0.2f, 10, 100000,
+                    1 << 27);
+
+    CATCH_CHECK(result == expected);
+}
+
+CATCH_TEST_CASE("kadayashi_dvr_1 empty region", TEST_GROUP) {
+    // Input data.
+    const std::filesystem::path test_data_dir = get_data_dir("variant") / "test-02-supertiny";
+    const std::filesystem::path fn_bam = test_data_dir / "in.aln.bam";
+    const std::filesystem::path fn_ref = test_data_dir / "in.ref.fasta.gz";
+
+    const std::unordered_map<std::string, int32_t> expected{};
+
+    // Open the input files.
+    dorado::secondary::BamFile bam_reader(fn_bam);
+    dorado::hts_io::FastxRandomReader fastx_reader(fn_ref);
+
+    CATCH_REQUIRE(bam_reader.fp());
+    CATCH_REQUIRE(bam_reader.idx());
+    CATCH_REQUIRE(bam_reader.hdr());
+    CATCH_REQUIRE(fastx_reader.get_raw_faidx_ptr());
+
+    // UUT.
+    const std::unordered_map<std::string, int32_t> result =
+            kadayashi::kadayashi_dvr_single_region_wrapper(
+                    bam_reader.fp(), bam_reader.idx(), bam_reader.hdr(),
+                    fastx_reader.get_raw_faidx_ptr(),
+
+                    "chr20", 200000, 200001, 0, 5, 5, 0.2f, 10, 100000, 1 << 27);
+
+    CATCH_CHECK(result == expected);
+}
+
+CATCH_TEST_CASE("kadayashi_dvr_1 nonexistent chromosome", TEST_GROUP) {
+    // Input data.
+    const std::filesystem::path test_data_dir = get_data_dir("variant") / "test-02-supertiny";
+    const std::filesystem::path fn_bam = test_data_dir / "in.aln.bam";
+    const std::filesystem::path fn_ref = test_data_dir / "in.ref.fasta.gz";
+
+    const std::unordered_map<std::string, int32_t> expected{};
+
+    // Open the input files.
+    dorado::secondary::BamFile bam_reader(fn_bam);
+    dorado::hts_io::FastxRandomReader fastx_reader(fn_ref);
+
+    CATCH_REQUIRE(bam_reader.fp());
+    CATCH_REQUIRE(bam_reader.idx());
+    CATCH_REQUIRE(bam_reader.hdr());
+    CATCH_REQUIRE(fastx_reader.get_raw_faidx_ptr());
+
+    // UUT.
+    const std::unordered_map<std::string, int32_t> result =
+            kadayashi::kadayashi_dvr_single_region_wrapper(
+                    bam_reader.fp(), bam_reader.idx(), bam_reader.hdr(),
+                    fastx_reader.get_raw_faidx_ptr(), "Nonexistent", 200000, 200001, 0, 5, 5, 0.2f,
+                    10, 100000, 1 << 27);
+
+    CATCH_CHECK(result == expected);
+}
+
+}  // namespace kadayashi::tests
