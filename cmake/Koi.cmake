0eb5d11db3a0302fa7e8894903cf2b0407364360
Enric Balaguer <enric.balaguerrodon@nanoporetech.com>
2025-07-04 17:20:48 +0100

    msg

--- cmake/Koi.cmake
+++ cmake/Koi.cmake
@@ -2,7 +2,7 @@ OPTION(BUILD_KOI_FROM_SOURCE OFF)
 
 function(get_best_compatible_koi_version KOI_CUDA)
     if (${CMAKE_SYSTEM_PROCESSOR} STREQUAL "aarch64")
-        # Koi provides binaries for these cuda versions when targeting aarch64
+#Koi provides binaries for these cuda versions when targeting aarch64
         set(SUPPORTED_VERSIONS 12.6 11.4 10.2)
     else()
         set(SUPPORTED_VERSIONS 12.8 12.4 12.0 11.8)
@@ -19,25 +19,20 @@ function(get_best_compatible_koi_version KOI_CUDA)
 endfunction()
 
 function(get_koi_download_hash KOI_DIR KOI_HASH)
-    # List of valid hashes.
-    set(hash__libkoi__0_6_0__Linux__aarch64__cuda__10_2 "e7e9410dda40d84b332de06f306d0b28cdceddf5f3db72434c8bafb7eb299d69")
-    set(hash__libkoi__0_6_0__Linux__aarch64__cuda__12_6 "abcf2f32baa7fac1333851a82087989d6c67f84b46bfbb497d23d1b7cc968fbd")
-    set(hash__libkoi__0_6_0__Linux__x86_64__cuda__11_8 "017664b061ad9f919ac93444b3f939abedf1cdd557f08605b67fe0a624793a40")
-    set(hash__libkoi__0_6_0__Linux__x86_64__cuda__12_0 "476b58ea0ba73bac0c3ecbb0e853d9b173ba022b2efeca87800c084638d043ab")
-    set(hash__libkoi__0_6_0__Linux__x86_64__cuda__12_4 "f0c6cfcbe29f9001ef27da12ec3621b89911033aaf899efb8c94353375f2145f")
-    set(hash__libkoi__0_6_0__Linux__x86_64__cuda__12_8 "b1a32428dcc663902f17ca8a7ae437536966d4d674ca45e34d727428fb82022e")
-    set(hash__libkoi__0_6_0__Windows__AMD64__cuda__11_8 "fd0da158e6630861c3410e61d3f7925cf8fc84c8f75162350dc604a391d217bf")
-    set(hash__libkoi__0_6_0__Windows__AMD64__cuda__12_4 "c745f33fddfd20884804a91b13a6fe63e26485db0acb924e579a5a4a371f1f17")
-    set(hash__libkoi__0_6_0__Windows__AMD64__cuda__12_8 "8037916ae99968321e32ad9b5cc5ea4390c3bc4abf2520971572c0d0776091af")
+#List of valid hashes.
+    set(hash__libkoi__0_6_0__Linux__aarch64__cuda__12_6 "9bb4a0531270a0196d08a3cd6b44ddc5e81b4d50af038e0dedcef51d34b3e54a")
+    set(hash__libkoi__0_6_0__Linux__x86_64__cuda__12_8 "bb11a8e56e8c0bf409e53293598e86a4d6cdf72190a081dd943564806de73458")
+    set(hash__libkoi__0_6_0__Windows__AMD64__cuda__12_8 "9a064fc5c509c8a2241247a89dbb3e3bf0af0e9e2c1bc08e7e1e3eb42871b1c8")
 
-    # Do the lookup.
+#Do the lookup.
     string(REPLACE "." "_" hash_key ${KOI_DIR})
     string(REPLACE "-" "__" hash_key ${hash_key})
     set(hash_key "hash__${hash_key}")
     if (NOT DEFINED ${hash_key})
         message(FATAL_ERROR "Missing hash for ${KOI_DIR}")
     endif()
-    set(${KOI_HASH} ${${hash_key}} PARENT_SCOPE)
+    set(${KOI_HASH} ${
+    $ { hash_key }} PARENT_SCOPE)
 endfunction()
 
 if(CMAKE_SYSTEM_NAME STREQUAL "Linux" OR WIN32)
@@ -62,10 +57,10 @@ if(CMAKE_SYSTEM_NAME STREQUAL "Linux" OR WIN32)
                 COMMAND
                     git clone
                         -b v${KOI_VERSION}
-                        # TODO: once we drop centos support we can use these instead of a separate submodule update
-                        #--depth 1
-                        #--recurse-submodules
-                        #--shallow-submodules
+#TODO : once we drop centos support we can use these instead of a separate submodule update
+#-- depth 1
+#-- recurse - submodules
+#-- shallow - submodules
                         ${KOI_REPO}
                         ${KOI_DIR}
                 COMMAND_ERROR_IS_FATAL ANY
