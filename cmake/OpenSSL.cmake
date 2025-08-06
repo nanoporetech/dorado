767530e497a9b0757fa1d6c7300bbe4d73d875e5
Steve Malton <stephen.malton@nanoporetech.com>
2025-08-06 14:58:47 +0100

    DOR-1314: Update openssl to 3.5.1 cdn packages

--- cmake/OpenSSL.cmake
+++ cmake/OpenSSL.cmake
@@ -5,38 +5,36 @@ else()
 endif()
 
 if(NOT DEFINED OPENSSL_ROOT_DIR)
+    set(OPENSSL_VERSION 3.5.1)
     if(APPLE)
-        set(OPENSSL_ROOT_DIR "/opt/homebrew/opt/openssl@3")
+        download_and_extract(
+            ${DORADO_CDN_URL}/openssl-${OPENSSL_VERSION}-macos-aarch64.zip
+            openssl-${OPENSSL_VERSION}-macos-aarch64
+            "d3a7c9dd9eaf09e6667c5ecfd213133fe3235b10a82c36246387e4d38b29ecd3"
+        )
+        set(OPENSSL_ROOT_DIR ${DORADO_3RD_PARTY_DOWNLOAD}/openssl-${OPENSSL_VERSION}-macos-aarch64)
     elseif(WIN32)
         download_and_extract(
-            ${DORADO_CDN_URL}/openssl3-win.zip
-            openssl3-win
-            "2b4dcaff48250428b48e28eb12f32138843af82eba5542fc8c9cbbd364a9451c"
+            ${DORADO_CDN_URL}/openssl-${OPENSSL_VERSION}-win.zip
+            openssl-${OPENSSL_VERSION}-win
+            "eeb4300919d6a44c896b30cf9e3afb851980b61b7a9b655c5cf1da9cc78aeb73"
         )
-        set(OPENSSL_ROOT_DIR ${DORADO_3RD_PARTY_DOWNLOAD}/openssl3-win)
+        set(OPENSSL_ROOT_DIR ${DORADO_3RD_PARTY_DOWNLOAD}/openssl-${OPENSSL_VERSION}-win)
     elseif(CMAKE_SYSTEM_NAME STREQUAL "Linux")
         if (CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")
             download_and_extract(
-                ${DORADO_CDN_URL}/openssl3-linux-x86_64.zip
-                openssl3-Linux-x86_64
-                "cc5790f3b58437e81cf0b523665de0cf1fb34e67c2a23181c11d1a051c18a995"
+                ${DORADO_CDN_URL}/openssl-${OPENSSL_VERSION}-linux-x86_64.zip
+                openssl-${OPENSSL_VERSION}-Linux-x86_64
+                "1f5e232fd4fb6dc441a0ce51eec631125a8585447454bc1e6a4d25ff32869b3a"
             )
-            set(OPENSSL_ROOT_DIR ${DORADO_3RD_PARTY_DOWNLOAD}/openssl3-Linux-x86_64)
+            set(OPENSSL_ROOT_DIR ${DORADO_3RD_PARTY_DOWNLOAD}/openssl-${OPENSSL_VERSION}-Linux-x86_64)
         elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "^aarch64*|^arm*")
-            if(${CUDAToolkit_VERSION} VERSION_LESS 11.0)
-                download_and_extract(
-                    ${DORADO_CDN_URL}/openssl3-linux-aarch64-gcc7.zip
-                    openssl3-Linux-aarch64
-                    "d9788e098b830612678780c623071fc1ffde7944639693970a295cef7a56b9b1"
-                )
-            else()
-                download_and_extract(
-                    ${DORADO_CDN_URL}/openssl3-linux-aarch64.zip
-                    openssl3-Linux-aarch64
-                    "f69511b0f1702e425fa0ec2f915c848a7ce451a30eff6492bdfa12bdbd860a93"
-                )
-            endif()
-            set(OPENSSL_ROOT_DIR ${DORADO_3RD_PARTY_DOWNLOAD}/openssl3-Linux-aarch64)
+            download_and_extract(
+                ${DORADO_CDN_URL}/openssl-${OPENSSL_VERSION}-linux-aarch64.zip
+                openssl-${OPENSSL_VERSION}-Linux-aarch64
+                "38111138e7c22f74d59760ac4144f998ad62ed7e6671a792f952349c3ace13fd"
+            )
+            set(OPENSSL_ROOT_DIR ${DORADO_3RD_PARTY_DOWNLOAD}/openssl-${OPENSSL_VERSION}-Linux-aarch64)
         endif()
     endif()
 else()
