130f4b93ee516e476c0788d62e6508eb04000185
Steve Malton <stephen.malton@nanoporetech.com>
2025-08-12 15:23:45 +0100

    DOR-738: Add argparse overloads for basecall args

--- dorado/cli/cli_lib/basecall_output_args.cpp
+++ dorado/cli/cli_lib/basecall_output_args.cpp
@@ -24,6 +24,18 @@ void add_emit_summary(utils::arg_parse::ArgParser& parser) {
             .flag();
 }
 
+void add_emit_types(argparse::ArgumentParser& parser) {
+    parser.add_argument(EMIT_FASTQ_ARG).help("Output in fastq format.").flag();
+    parser.add_argument(EMIT_SAM_ARG).help("Output in SAM format.").flag();
+}
+
+void add_emit_summary(argparse::ArgumentParser& parser) {
+    parser.add_argument(EMIT_SUMMARY_ARG)
+            .help("If specified, a summary file containing the details of the primary "
+                  "alignments for each read will be emitted to the root of the output folder.")
+            .flag();
+}
+
 }  // namespace
 
 void add_basecaller_output_arguments(utils::arg_parse::ArgParser& parser) {
@@ -55,6 +67,35 @@ bool get_emit_summary(const utils::arg_parse::ArgParser& parser) {
 
 std::optional<std::string> get_output_dir(const utils::arg_parse::ArgParser& parser) {
     return parser.visible.present<std::string>(OUTPUT_DIR_ARG);
+}
+
+void add_basecaller_output_arguments(argparse::ArgumentParser& parser) {
+    add_emit_types(parser);
+    parser.add_argument("-o", OUTPUT_DIR_ARG)
+            .help("Optional output folder which becomes the root of the nested output folder "
+                  "structure.");
+}
+
+void add_demux_output_arguments(argparse::ArgumentParser& parser) {
+    parser.add_argument("-o", OUTPUT_DIR_ARG)
+            .help("Output folder which is the root of the nested output folder structure.")
+            .required();
+    add_emit_types(parser);
+    add_emit_summary(parser);
+}
+
+bool get_emit_fastq(const argparse::ArgumentParser& parser) {
+    return parser.get<bool>(EMIT_FASTQ_ARG);
+}
+
+bool get_emit_sam(const argparse::ArgumentParser& parser) { return parser.get<bool>(EMIT_SAM_ARG); }
+
+bool get_emit_summary(const argparse::ArgumentParser& parser) {
+    return parser.get<bool>(EMIT_SUMMARY_ARG);
+}
+
+std::optional<std::string> get_output_dir(const argparse::ArgumentParser& parser) {
+    return parser.present<std::string>(OUTPUT_DIR_ARG);
 }
 
 }  // namespace dorado::cli
