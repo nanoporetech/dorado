63c0eebc05f373c8e3679ccfe0631aa46c6e7d1c
Steve Malton <stephen.malton@nanoporetech.com>
2025-08-12 16:08:48 +0100

    DOR-738: Remove unused overloads

--- dorado/cli/cli_lib/basecall_output_args.cpp
+++ dorado/cli/cli_lib/basecall_output_args.cpp
@@ -1,5 +1,7 @@
 #include "basecall_output_args.h"
 
+#include <argparse/argparse.hpp>
+
 #include <ctime>
 #include <optional>
 
@@ -11,18 +13,6 @@ constexpr std::string_view OUTPUT_DIR_ARG{"--output-dir"};
 constexpr std::string_view EMIT_FASTQ_ARG{"--emit-fastq"};
 constexpr std::string_view EMIT_SAM_ARG{"--emit-sam"};
 constexpr std::string_view EMIT_SUMMARY_ARG{"--emit-summary"};
-
-void add_emit_types(utils::arg_parse::ArgParser& parser) {
-    parser.visible.add_argument(EMIT_FASTQ_ARG).help("Output in fastq format.").flag();
-    parser.visible.add_argument(EMIT_SAM_ARG).help("Output in SAM format.").flag();
-}
-
-void add_emit_summary(utils::arg_parse::ArgParser& parser) {
-    parser.visible.add_argument(EMIT_SUMMARY_ARG)
-            .help("If specified, a summary file containing the details of the primary "
-                  "alignments for each read will be emitted to the root of the output folder.")
-            .flag();
-}
 
 void add_emit_types(argparse::ArgumentParser& parser) {
     parser.add_argument(EMIT_FASTQ_ARG).help("Output in fastq format.").flag();
@@ -37,37 +27,6 @@ void add_emit_summary(argparse::ArgumentParser& parser) {
 }
 
 }  // namespace
-
-void add_basecaller_output_arguments(utils::arg_parse::ArgParser& parser) {
-    add_emit_types(parser);
-    parser.visible.add_argument("-o", OUTPUT_DIR_ARG)
-            .help("Optional output folder which becomes the root of the nested output folder "
-                  "structure.");
-}
-
-void add_demux_output_arguments(utils::arg_parse::ArgParser& parser) {
-    parser.visible.add_argument("-o", OUTPUT_DIR_ARG)
-            .help("Output folder which is the root of the nested output folder structure.")
-            .required();
-    add_emit_types(parser);
-    add_emit_summary(parser);
-}
-
-bool get_emit_fastq(const utils::arg_parse::ArgParser& parser) {
-    return parser.visible.get<bool>(EMIT_FASTQ_ARG);
-}
-
-bool get_emit_sam(const utils::arg_parse::ArgParser& parser) {
-    return parser.visible.get<bool>(EMIT_SAM_ARG);
-}
-
-bool get_emit_summary(const utils::arg_parse::ArgParser& parser) {
-    return parser.visible.get<bool>(EMIT_SUMMARY_ARG);
-}
-
-std::optional<std::string> get_output_dir(const utils::arg_parse::ArgParser& parser) {
-    return parser.visible.present<std::string>(OUTPUT_DIR_ARG);
-}
 
 void add_basecaller_output_arguments(argparse::ArgumentParser& parser) {
     add_emit_types(parser);
--- dorado/cli/cli_lib/basecall_output_args.h
+++ dorado/cli/cli_lib/basecall_output_args.h
@@ -1,18 +1,13 @@
 #pragma once
 
-#include "utils/arg_parse_ext.h"
+#include <optional>
+#include <string>
 
-#include <argparse/argparse.hpp>
+namespace argparse {
+class ArgumentParser;
+}
+
 namespace dorado::cli {
-
-void add_basecaller_output_arguments(utils::arg_parse::ArgParser& parser);
-void add_demux_output_arguments(utils::arg_parse::ArgParser& parser);
-
-bool get_emit_fastq(const utils::arg_parse::ArgParser& parser);
-bool get_emit_sam(const utils::arg_parse::ArgParser& parser);
-bool get_emit_summary(const utils::arg_parse::ArgParser& parser);
-
-std::optional<std::string> get_output_dir(const utils::arg_parse::ArgParser& parser);
 
 void add_basecaller_output_arguments(argparse::ArgumentParser& parser);
 void add_demux_output_arguments(argparse::ArgumentParser& parser);
