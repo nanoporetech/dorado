0bd02973425aa98620364d18613bb04fab6145f6
Ben Lawrence <ben.lawrence@nanoporetech.com>
2025-09-02 17:45:38 +0100

    [DOR-1354] Fix indentation in this file

--- cmake/Pod5.cmake
+++ cmake/Pod5.cmake
@@ -10,50 +10,50 @@ set(POD5_REPO "https://github.com/nanoporetech/pod5-file-format")
 # implementation being inlined with different instrumentation settings.
 # See https://github.com/google/sanitizers/wiki/AddressSanitizerContainerOverflow#false-positives
 if(ECM_ENABLE_SANITIZERS MATCHES "address")
-  set(POD5_STATIC FALSE)
+    set(POD5_STATIC FALSE)
 else()
-  set(POD5_STATIC TRUE)
+    set(POD5_STATIC TRUE)
 endif()
 
 if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
     set(LIB_DIR "lib64")
     if(CMAKE_SYSTEM_PROCESSOR MATCHES "^aarch64*|^arm*")
-      set(POD5_URL "${POD5_REPO}/releases/download/${POD5_VERSION}/lib_pod5-${POD5_VERSION}-linux-arm64.tar.gz")
-      set(POD5_HASH "2b997ed4991e65515669fc9c78cc1749c133a078dea365d7a61019a2a28e85a6")
+        set(POD5_URL "${POD5_REPO}/releases/download/${POD5_VERSION}/lib_pod5-${POD5_VERSION}-linux-arm64.tar.gz")
+        set(POD5_HASH "2b997ed4991e65515669fc9c78cc1749c133a078dea365d7a61019a2a28e85a6")
     else()
-      set(POD5_URL "${POD5_REPO}/releases/download/${POD5_VERSION}/lib_pod5-${POD5_VERSION}-linux-x64.tar.gz")
-      set(POD5_HASH "3a179c703c180fe8dbab38504a56aaf1264088f072b1febbef54517910fe9ba0")
+        set(POD5_URL "${POD5_REPO}/releases/download/${POD5_VERSION}/lib_pod5-${POD5_VERSION}-linux-x64.tar.gz")
+        set(POD5_HASH "3a179c703c180fe8dbab38504a56aaf1264088f072b1febbef54517910fe9ba0")
     endif()
     if(POD5_STATIC)
-      set(POD5_LIBRARIES
-        ${DORADO_3RD_PARTY_DOWNLOAD}/${POD5_DIR}/${LIB_DIR}/libpod5_format.a
-        ${DORADO_3RD_PARTY_DOWNLOAD}/${POD5_DIR}/${LIB_DIR}/libarrow.a
-        ${DORADO_3RD_PARTY_DOWNLOAD}/${POD5_DIR}/${LIB_DIR}/libjemalloc_pic.a
-        ${DORADO_3RD_PARTY_DOWNLOAD}/${POD5_DIR}/${LIB_DIR}/libzstd.a
-      )
+        set(POD5_LIBRARIES
+            ${DORADO_3RD_PARTY_DOWNLOAD}/${POD5_DIR}/${LIB_DIR}/libpod5_format.a
+            ${DORADO_3RD_PARTY_DOWNLOAD}/${POD5_DIR}/${LIB_DIR}/libarrow.a
+            ${DORADO_3RD_PARTY_DOWNLOAD}/${POD5_DIR}/${LIB_DIR}/libjemalloc_pic.a
+            ${DORADO_3RD_PARTY_DOWNLOAD}/${POD5_DIR}/${LIB_DIR}/libzstd.a
+        )
     else()
-      set(POD5_LIBRARIES ${DORADO_3RD_PARTY_DOWNLOAD}/${POD5_DIR}/lib/libpod5_format.so)
+        set(POD5_LIBRARIES ${DORADO_3RD_PARTY_DOWNLOAD}/${POD5_DIR}/lib/libpod5_format.so)
     endif()
 elseif(APPLE)
     set(POD5_URL "${POD5_REPO}/releases/download/${POD5_VERSION}/lib_pod5-${POD5_VERSION}-osx-11.0-arm64.tar.gz")
     set(POD5_HASH "e6155bcf6415c84e1c37ec214034fd9f5b81cbac2a8d2f7746e2af24d7078fe3")
     if(POD5_STATIC)
-      set(POD5_LIBRARIES
-        ${DORADO_3RD_PARTY_DOWNLOAD}/${POD5_DIR}/lib/libpod5_format.a
-        ${DORADO_3RD_PARTY_DOWNLOAD}/${POD5_DIR}/lib/libarrow.a
-        ${DORADO_3RD_PARTY_DOWNLOAD}/${POD5_DIR}/lib/libzstd.a
-      )
+        set(POD5_LIBRARIES
+            ${DORADO_3RD_PARTY_DOWNLOAD}/${POD5_DIR}/lib/libpod5_format.a
+            ${DORADO_3RD_PARTY_DOWNLOAD}/${POD5_DIR}/lib/libarrow.a
+            ${DORADO_3RD_PARTY_DOWNLOAD}/${POD5_DIR}/lib/libzstd.a
+        )
     else()
-      set(POD5_LIBRARIES ${DORADO_3RD_PARTY_DOWNLOAD}/${POD5_DIR}/lib/libpod5_format.dylib)
+        set(POD5_LIBRARIES ${DORADO_3RD_PARTY_DOWNLOAD}/${POD5_DIR}/lib/libpod5_format.dylib)
     endif()
 elseif(WIN32)
     set(POD5_URL "${POD5_REPO}/releases/download/${POD5_VERSION}/lib_pod5-${POD5_VERSION}-win-x64.tar.gz")
     set(POD5_HASH "29099ab7d86689919202bc8b906103d8cf6208356a7565a7c8f1a2edf514f95e")
     set(POD5_LIBRARIES
-      ${DORADO_3RD_PARTY_DOWNLOAD}/${POD5_DIR}/lib/pod5_format.lib
-      ${DORADO_3RD_PARTY_DOWNLOAD}/${POD5_DIR}/lib/arrow_static.lib
-      ${DORADO_3RD_PARTY_DOWNLOAD}/${POD5_DIR}/lib/zstd_static.lib
-      bcrypt.lib
+        ${DORADO_3RD_PARTY_DOWNLOAD}/${POD5_DIR}/lib/pod5_format.lib
+        ${DORADO_3RD_PARTY_DOWNLOAD}/${POD5_DIR}/lib/arrow_static.lib
+        ${DORADO_3RD_PARTY_DOWNLOAD}/${POD5_DIR}/lib/zstd_static.lib
+        bcrypt.lib
     )
 endif()
 
