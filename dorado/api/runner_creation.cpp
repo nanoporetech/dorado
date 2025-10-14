b259e345d1abc7d393af3b1fb78e44ffdc5dd5ee
Alice White <svc-awhite@nanoporetech.com>
2025-10-14 11:50:11 +0100

    [DOR-1409] Disable variable chunks on Orin

--- dorado/api/runner_creation.cpp
+++ dorado/api/runner_creation.cpp
@@ -24,7 +24,7 @@ namespace dorado::api {
 bool check_variable_chunk_sizes_supported(
         [[maybe_unused]] const config::BasecallModelConfig& model_config,
         [[maybe_unused]] const std::span<const int> device_ids) {
-#if DORADO_CUDA_BUILD
+#if DORADO_CUDA_BUILD && !DORADO_ORIN
     if (!model_config.is_lstm_model() || (model_config.lstm_size <= 128) ||
         (model_config.lstm_size > 1024) || ((model_config.lstm_size % 128) != 0)) {
         return false;
