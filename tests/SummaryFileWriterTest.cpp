10bfd448e257f735d5cffbdce393904c8fafbbde
Steve Malton <stephen.malton@nanoporetech.com>
2025-11-07 16:15:13 +0000

    DOR-1434: More unit tests

--- tests/SummaryFileWriterTest.cpp
+++ tests/SummaryFileWriterTest.cpp
@@ -1,5 +1,7 @@
 #include "hts_writer/SummaryFileWriter.h"
 
+#include "utils/string_utils.h"
+
 #include <catch2/catch_message.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/generators/catch_generators.hpp>
@@ -13,4 +15,18 @@ using namespace dorado;
 CATCH_TEST_CASE(TEST_GROUP " constructor no throw", TEST_GROUP) {
     std::ostringstream stream;
     CATCH_CHECK_NOTHROW(hts_writer::SummaryFileWriter(stream, 0));
+}
+
+CATCH_TEST_CASE(TEST_GROUP " column headers by flags", TEST_GROUP) {
+    using sfw = hts_writer::SummaryFileWriter;
+    auto [flags, expected_column_count] = GENERATE(table<sfw::FieldFlags, size_t>({
+            {0, 10},
+            {sfw::BASECALLING_FIELDS | sfw::EXPERIMENT_FIELDS, 20},
+            {sfw::BARCODING_FIELDS | sfw::POLYA_FIELDS | sfw::DUPLEX_FIELDS, 29},
+            {sfw::BARCODING_FIELDS | sfw::ALIGNMENT_FIELDS | sfw::BASECALLING_FIELDS, 47},
+    }));
+    std::ostringstream stream;
+    hts_writer::SummaryFileWriter writer(stream, flags);
+    auto columns = utils::split(stream.str(), '\t');
+    CATCH_CHECK(columns.size() == expected_column_count);
 }
