5d3d2b11bb47a605f564b6f574bfcad15316690d
Steve Malton <stephen.malton@nanoporetech.com>
2025-11-07 16:15:13 +0000

    DOR-1434: Start of unit tests for SummaryFileWriter

---
+++ tests/SummaryFileWriterTest.cpp
@@ -0,0 +1,16 @@
+#include "hts_writer/SummaryFileWriter.h"
+
+#include <catch2/catch_message.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+
+#include <sstream>
+
+#define TEST_GROUP "[SummaryFileWriter]"
+
+using namespace dorado;
+
+CATCH_TEST_CASE(TEST_GROUP " constructor no throw", TEST_GROUP) {
+    std::ostringstream stream;
+    CATCH_CHECK_NOTHROW(hts_writer::SummaryFileWriter(stream));
+}
