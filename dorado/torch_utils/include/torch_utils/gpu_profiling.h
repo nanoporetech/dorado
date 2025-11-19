27ce945951a2cef62b056eddb52fcbe1e571e27c
Ben Lawrence <ben.lawrence@nanoporetech.com>
2025-11-19 10:49:04 +0000

    [INSTX-12518] Teach ScopedProfileRange how to profile on macOS

--- dorado/torch_utils/include/torch_utils/gpu_profiling.h
+++ dorado/torch_utils/include/torch_utils/gpu_profiling.h
@@ -1,19 +1,32 @@
 #pragma once
 
+#include "utils/dev_utils.h"
+
+#include <nvtx3/nvtx3.hpp>
+
 // Set this to >0 to enable output of GPU profiling information to stderr
 // or use `dorado [basecaller|duplex] ... --devopts cuda_profile_level=<X> ...`
 #define CUDA_PROFILE_LEVEL_DEFAULT 0
 
 #if DORADO_CUDA_BUILD
 #include "cuda_utils.h"
-#include "utils/dev_utils.h"
 
 #include <ATen/cuda/CUDAContext.h>
 #include <cuda_runtime.h>
-#include <nvtx3/nvtx3.hpp>
+
+#elif DORADO_METAL_BUILD
+#include "metal_utils.h"
+
+namespace dorado::utils::detail {
+inline os_log_t s_scoped_os_log =
+        os_log_create("scoped_profile", OS_LOG_CATEGORY_POINTS_OF_INTEREST);
+}  // namespace dorado::utils::detail
+
+#endif
 
 namespace dorado::utils {
-// If `detail_level <= CUDA_PROFILE_TO_CERR_LEVEL`, this times a range and prints it to stderr so
+
+// If `detail_level <= CUDA_PROFILE_LEVEL_DEFAULT`, this times a range and prints it to stderr so
 // you don't have to generate a QDREP file to perform basic profiling.
 // Also is a nvtx3::scoped_range which means `label` will be shown in NSight tools.
 class ScopedProfileRange : public nvtx3::scoped_range {
@@ -24,20 +37,39 @@ public:
               m_detail_level(detail_level),
               m_active(m_detail_level <=
                        get_dev_opt<int>("cuda_profile_level", CUDA_PROFILE_LEVEL_DEFAULT)) {
-        if (m_active) {
-            m_stream = at::cuda::getCurrentCUDAStream().stream();
-            handle_cuda_result(cudaEventCreate(&m_start));
-            handle_cuda_result(cudaEventRecord(m_start, m_stream));
-        }
+        activate();
     }
 
     ~ScopedProfileRange() { finish(); }
 
 private:
+    ScopedProfileRange(const ScopedProfileRange &) = delete;
+    ScopedProfileRange(ScopedProfileRange &&) = delete;
+    ScopedProfileRange &operator=(const ScopedProfileRange &) = delete;
+    ScopedProfileRange &operator=(ScopedProfileRange &&) = delete;
+
+    void activate() {
+        if (!m_active) {
+            return;
+        }
+#if DORADO_CUDA_BUILD
+        m_stream = at::cuda::getCurrentCUDAStream().stream();
+        handle_cuda_result(cudaEventCreate(&m_start));
+        handle_cuda_result(cudaEventRecord(m_start, m_stream));
+#elif DORADO_METAL_BUILD
+        m_signpost_id = os_signpost_id_generate(detail::s_scoped_os_log);
+        os_signpost_interval_begin(detail::s_scoped_os_log, m_signpost_id, "ScopedProfileRange",
+                                   "[%i]%s", m_detail_level, m_label);
+#endif
+    }
+
     void finish() {
         if (!m_active) {
             return;
         }
+        m_active = false;
+
+#if DORADO_CUDA_BUILD
         cudaEvent_t stop;
         handle_cuda_result(cudaEventCreate(&stop));
         handle_cuda_result(cudaEventRecord(stop, m_stream));
@@ -48,24 +80,22 @@ private:
         handle_cuda_result(cudaEventDestroy(stop));
         std::cerr << std::string(m_detail_level - 1, '\t') << "[" << m_label << " " << timeMs
                   << " ms]\n";
-        m_active = false;
+#elif DORADO_METAL_BUILD
+        os_signpost_interval_end(detail::s_scoped_os_log, m_signpost_id, "ScopedProfileRange",
+                                 "[%i]%s", m_detail_level, m_label);
+#endif
     }
 
     const char *m_label;
-    cudaStream_t m_stream;
-    cudaEvent_t m_start;
     int m_detail_level;
     bool m_active;
-};
 
-}  // namespace dorado::utils
-
-#else
-namespace dorado::utils {
-// Do nothing on Apple platforms
-struct ScopedProfileRange {
-    explicit ScopedProfileRange(const char *, int) {}
-};
-}  // namespace dorado::utils
-
+#if DORADO_CUDA_BUILD
+    cudaStream_t m_stream;
+    cudaEvent_t m_start;
+#elif DORADO_METAL_BUILD
+    os_signpost_id_t m_signpost_id = OS_SIGNPOST_ID_NULL;
 #endif
+};
+
+}  // namespace dorado::utils
